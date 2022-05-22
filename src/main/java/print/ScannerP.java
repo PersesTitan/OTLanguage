12f81865aebc2e15a46ec79ddc58c22bb0b51f3e
PersesTitan <torampersestitan@gmail.com>
2022-05-23 07:41:34 +0900

    commit

--- src/main/java/print/ScannerP.java
+++ src/main/java/print/ScannerP.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 
 public class ScannerP implements Check {
     private static final String SPECIFIED = "ㅅㅇㅅ";
-    private final String patternText = "\\bㅅㅇㅅ\\b";
+    private final String patternText = "\\sㅅㅇㅅ\\s";
     private final Pattern pattern = Pattern.compile(patternText);
 
     /**
