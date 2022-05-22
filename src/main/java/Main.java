4e5065ef8603cf5136d556d92e30bd1c46be72df
PersesTitan <torampersestitan@gmail.com>
2022-05-23 00:02:06 +0900

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,10 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Locale;
+import java.util.StringTokenizer;
 
 
 public class Main extends Setting implements ActivityItem {
