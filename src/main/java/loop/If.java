99161e883718ce5610f31475b0beba609d353fff
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-07 00:01:54 +0900

    루프 변경

--- src/main/java/loop/If.java
+++ src/main/java/loop/If.java
@@ -2,11 +2,11 @@ package loop;
 
 import item.Check;
 import item.Setting;
-import item.VarCheck;
-import item.kind.LoopType;
+import item.work.LoopPosition;
 import item.work.LoopWork;
 
 import java.util.StringTokenizer;
+import java.util.UUID;
 import java.util.regex.Pattern;
 
 public class If extends Setting implements Check, LoopWork {
@@ -14,33 +14,23 @@ public class If extends Setting implements Check, LoopWork {
     private final String patternText = "\\n\\s*\\?ㅅ\\?\\s|^\\s*\\?ㅅ\\?\\s";
     private final Pattern pattern = Pattern.compile(patternText);
 
-    @Override
-    public boolean check(String total) {
-        return pattern.matcher(total).find();
-    }
+//    @Override
+//    public boolean check(String total) {
+//        return pattern.matcher(total).find();
+//    }
 
-    private int lnCountBlank(String total) {
-        int position = 0;
-        StringBuilder subTotal = new StringBuilder();
-        final int blank = countBlank(total) + 1;
-        for (String line : total.split("\\n")) {
-            if (countBlank(line) >= blank) subTotal.append(line);
-            else break;
-        }
-        total = subTotal.toString();
-
-        // total 안에 loop 가 있으면 검사하기
-//        LoopType loopType = loopCheck());
-//        if (loopType.equals())
-
-        return position;
-    }
-
-    private void cut(String total) {
-        int start = total.indexOf(SPECIFIED);
-        if (start > 0) do {start -= 1;} while (total.charAt(start) != ' ');
-
-    }
+//    public void getLoop(String total) {
+//        while (check(total)) {
+//            LoopPosition loopPosition = countPattern(total);
+//            String start = total.substring(loopPosition.getStart());
+//            int end = endPattern.matcher(start).end();
+//            String random = " ".repeat(Math.max(0, loopPosition.getBlank())) +
+//                    UUID.randomUUID();
+//            String cut = total.substring(loopPosition.getStart(), end);
+//            total = total.replace(cut, random);
+//            getLoop(total);
+//        }
+//    }
 
     @Override
     public void start(String line) throws Exception {
@@ -57,5 +47,10 @@ public class If extends Setting implements Check, LoopWork {
         } else {
             //false 일때의 동작
         }
+    }
+
+    @Override
+    public boolean check(String line) {
+        return false;
     }
 }
