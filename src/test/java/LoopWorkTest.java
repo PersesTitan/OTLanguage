7f7c5684a6c0ef1205d09c31ae06056dfa073869
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-16 08:23:33 +0900

    create Test

--- src/test/java/LoopWorkTest.java
+++ src/test/java/LoopWorkTest.java
@@ -1,16 +1,32 @@
 import item.work.LoopWork;
+import loop.Bracket;
 import org.junit.jupiter.api.Test;
 
+import java.util.StringTokenizer;
 import java.util.regex.Matcher;
 
 public class LoopWorkTest implements LoopWork {
 
-    public static void main(String[] args) {
-        String testTotal = "?ㅅ? \n ?ㅅ? \n ^^ \n^^";
-        Matcher matcher = pattern.matcher(testTotal);
-        while (matcher.find()) {
-            String str = String.format("%s %d %d", matcher.group(), matcher.start(), matcher.end());
-            System.out.println(str);
+    public static void main(String[] args) throws Exception {
+        String testTotal = "adsaf\n" +
+                "{asfs{afsfs\n" +
+                "}\n" +
+                "asfs{sf{f\n" +
+                "  saf\n" +
+                "}af}af}a\n" +
+                "fs{af}s\n" +
+                "" +
+                "" +
+                "f{sfsf}af\n";
+        Bracket bracket = new Bracket();
+        String pr = bracket.bracket(testTotal);
+
+        System.out.println(pr);
+
+        String[] str = pr.split("\n");
+        for (String s : str) {
+            System.out.println(s);
+            System.out.println(bracket.check(s));
         }
     }
 
