c711f6b438e3571afc4ee697b09f914db5a9c938
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-22 10:59:00 +0900

    파일이 존재하지 않을때 파일 찾는 창 띄우기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 import item.ActivityItem;
 import item.Setting;
 
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -11,30 +13,42 @@ public class Main extends Setting implements ActivityItem {
 
     public static void main(String[] args) throws Exception {
 
-//        args = new String[1];
-//        args[0] = "./hello.otl";
+        args = new String[1];
+        args[0] = "./hello.otl";
         new Main(args);
     }
 
-    Main(String[] args) throws Exception {
-        if (args.length <= 0) throw new Exception("파일이 존재하지 않습니다.");
-        if (!new File(args[0]).canRead()) throw new Exception("파일을 읽을 수 없습니다.");
-        if (!args[0].toLowerCase(Locale.ROOT).endsWith(".otl")) throw new Exception("확장자를 확인해주세요.");
+    private Main(String[] args) throws Exception {
+        String path = args.length <= 0 ? showGUI() : args[0];
+        if (!new File(path).canRead()) throw new Exception("파일을 읽을 수 없습니다.");
+        if (!path.toLowerCase(Locale.ROOT).endsWith(".otl")) throw new Exception("확장자를 확인해주세요.");
         int count = 0;
         varClear();
         String text;
         System.out.println("================출력================");
-        try (BufferedReader reader = new BufferedReader(new FileReader(args[0], StandardCharsets.UTF_8))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(path, StandardCharsets.UTF_8))) {
             while ((text = reader.readLine()) != null) {
                 idLine.put(count, text);
                 total.append(text);
                 start(text);
                 count++;
             }
-
-
         }
         pause();
+    }
+
+    private String showGUI() throws Exception {
+        final JFrame frame = new JFrame();
+        final String[] extensions = {"otl"};
+        final JFileChooser chooser = new JFileChooser();
+        final FileNameExtensionFilter filter = new FileNameExtensionFilter(".otl", extensions);
+
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        chooser.setFileFilter(filter);
+        int open = chooser.showOpenDialog(frame.getParent());
+        if (open == JFileChooser.OPEN_DIALOG)
+            return chooser.getSelectedFile().getPath();
+        else throw new Exception("파일이 존재하지 않습니다.");
     }
 
     private void pause() {
