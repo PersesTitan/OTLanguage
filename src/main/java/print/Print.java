3e1b52841caac7d68c894906f04f37a8c4cd3666
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-22 11:52:36 +0900

    글짜 수 줄이기

--- src/main/java/print/Print.java
+++ src/main/java/print/Print.java
@@ -8,7 +8,7 @@ import java.util.regex.Pattern;
 
 public class Print extends Setting implements Check, PrintWork {
     private static final String SPECIFIED = "ㅅㅁㅅ";
-    private final String patternText = "\\n\\s*ㅅㅁㅅ\\s|^\\s*ㅅㅁㅅ\\s";
+    private final String patternText = "(\\n|^)\\s*ㅅㅁㅅ\\s";
     private final Pattern pattern = Pattern.compile(patternText);
 
     /**
