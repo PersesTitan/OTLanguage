0d6b03648b28774df7015fbee07cd19ed95b2d63
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-24 07:39:07 +0900

    괄호 테스트 코드

--- src/test/java/LoopWorkTest.java
+++ src/test/java/LoopWorkTest.java
@@ -4,17 +4,17 @@ import loop.Bracket;
 public class LoopWorkTest implements LoopWork {
 
     public static void main(String[] args) throws Exception {
-        String testTotal = "dd";
+        String testTotal = "{ }{  } {   }\n{  } {         {}}";
 
         Bracket bracket = new Bracket();
         String pr = bracket.bracket(testTotal);
 
-        System.out.println(pr);
+//        System.out.println(pr);
 
         String[] str = pr.split("\n");
         for (String s : str) {
             System.out.println(s);
-            System.out.println(bracket.check(s));
+//            System.out.println(bracket.check(s));
         }
     }
 
