39e1d449104ce90702522e983a9690fffbd97bf9
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-27 12:49:36 +0900

    숫자 + 기호 + 숫자

--- src/main/java/comparison/Small.java
+++ src/main/java/comparison/Small.java
@@ -1,9 +1,16 @@
 package comparison;
 
+import item.Check;
+
 import java.util.regex.Pattern;
 
-public class Small {
+public class Small implements Check {
     private static final String SPECIFIED = "ㅇ>ㅇ";
-    private final String patternText = "\\s*ㅇ>ㅇ\\s";
+    private final String patternText = "[\\S+.-]\\s*ㅇ>ㅇ\\s*[\\S+.-]";
     private final Pattern pattern = Pattern.compile(patternText);
+
+    @Override
+    public boolean check(String line) throws Exception {
+        return pattern.matcher(line).find();
+    }
 }
