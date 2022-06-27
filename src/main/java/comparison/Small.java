f7d21cd798f798c34ef0496f61bf6db4faf2265e
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-28 00:58:52 +0900

    체크 메소드 추가

--- src/main/java/comparison/Small.java
+++ src/main/java/comparison/Small.java
@@ -1,15 +1,12 @@
 package comparison;
 
-import item.Check;
-
 import java.util.regex.Pattern;
 
-public class Small implements Check {
+public class Small {
     private static final String SPECIFIED = "ㅇ>ㅇ";
     private final String patternText = "[\\S+.-]\\s*ㅇ>ㅇ\\s*[\\S+.-]";
     private final Pattern pattern = Pattern.compile(patternText);
 
-    @Override
     public boolean check(String line) throws Exception {
         return pattern.matcher(line).find();
     }
