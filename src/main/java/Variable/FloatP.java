cd5794a1bfef0cdc8a2cfc460359181b2534c4ff
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-28 00:49:56 +0900

    체크 삭제

--- src/main/java/Variable/FloatP.java
+++ src/main/java/Variable/FloatP.java
@@ -1,6 +1,5 @@
 package Variable;
 
-import item.Check;
 import item.KeyValueItem;
 import item.Setting;
 import item.kind.VarType;
@@ -8,7 +7,7 @@ import item.work.VariableWork;
 
 import java.util.regex.Pattern;
 
-public class FloatP extends Setting implements Check, VariableWork {
+public class FloatP extends Setting implements VariableWork {
     public static final String SPECIFIED = "ㅇㅅㅇ";
     public static final String patternText = "(\\n|^)\\s*ㅇㅅㅇ\\s";
     private final Pattern pattern = Pattern.compile(patternText);
