a5636fdd2dc86a4462b02f43c12da3f74a2cdb0b
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-29 17:29:14 +0900

    통과 조건 변경

--- src/main/java/comparison/Small.java
+++ src/main/java/comparison/Small.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public class Small {
     private static final String SPECIFIED = "ㅇ>ㅇ";
-    private final String patternText = "[\\S+.-]\\s*ㅇ>ㅇ\\s*[\\S+.-]";
+    private final String patternText = "-?\\d+\\.?\\d*\\s*ㅇ>ㅇ\\s*-?\\d+\\.?\\d*";
     private final Pattern pattern = Pattern.compile(patternText);
 
     public boolean check(String line) throws Exception {
