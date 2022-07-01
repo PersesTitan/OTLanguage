e5c86b92cabf7feecd547f568c6670625f842b77
PersesTitan <ToramPersesTitan@gmail.com>
2022-07-01 13:28:36 +0900

    사용하지 않는 코드 제거

--- src/main/java/comparison/Big.java
+++ src/main/java/comparison/Big.java
@@ -1,87 +1,40 @@
 package comparison;
 
 import item.ActivityItem;
-import item.KeyValueItem;
-import item.kind.VarType;
 import item.work.ComparisonWork;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.regex.Pattern;
 
 public class Big implements ComparisonWork, ActivityItem {
     private static final String SPECIFIED = "ㅇ<ㅇ";
     private final String patternText = "-?\\d+\\.?\\d*\\s*ㅇ<ㅇ\\s*-?\\d+\\.?\\d*";
+    private final String singText = "ㅇ<ㅇ";
+    private final String numberText = "-?\\d+\\.?\\d*";
     private final Pattern pattern = Pattern.compile(patternText);
     private final static char left = '(';
     private final static char right = ')';
 
     @Override
     public boolean check(String line) throws Exception {
-        return pattern.matcher(line).find() && checkLine(checkLine(line));
+        return pattern.matcher(line).find();
     }
 
     @Override
-    public boolean start(String line) throws Exception {
-        int startSave;
-        int endSave;
-
-        // key = start
-        // value = end
-        var lines = line.split(" ");
-        List<String> lists = new ArrayList<>(Arrays.asList(lines));
-        lists.removeAll(Collections.singletonList(" "));
-        lists.removeAll(Collections.singletonList(null));
-        int position = lists.indexOf(SPECIFIED);
-        if (position-1 < 0) throw new Exception(SPECIFIED + " 위치가 잘못 되었습니다.");
-        String start = lists.get(position-1);
-        String end = lists.get(position+1);
-
-        boolean s_bool1 = varCheck.check(start, VarType.Integer);
-        boolean s_bool2 = varCheck.check(start, VarType.Long);
-        boolean s_bool4 = varCheck.check(start, VarType.Float);
-        boolean s_bool3 = varCheck.check(start, VarType.Double);
-
-        boolean e_bool1 = varCheck.check(end, VarType.Integer);
-        boolean e_bool2 = varCheck.check(end, VarType.Long);
-        boolean e_bool3 = varCheck.check(end, VarType.Double);
-        boolean e_bool4 = varCheck.check(end, VarType.Float);
-
-        var value = checkLine(line);
-        return false;
+    public String start(String line) throws Exception {
+        return null;
     }
-
-    private KeyValueItem checkLine(String line) throws Exception {
-        // key = start
-        // value = end
-        var lines = line.split(" ");
-        List<String> lists = new ArrayList<>(Arrays.asList(lines));
-        lists.removeAll(Collections.singletonList(" "));
-        lists.removeAll(Collections.singletonList(null));
-        int position = lists.indexOf(SPECIFIED);
-        if (position-1 < 0) throw new Exception(SPECIFIED + " 위치가 잘못 되었습니다.");
-        String start = lists.get(position-1);
-        String end = lists.get(position+1);
-        return new KeyValueItem(start, end);
-    }
-
-    private boolean checkLine(KeyValueItem line) {
-        String start = line.getKey();
-        String end = line.getValue();
-        boolean s_bool1 = varCheck.check(start, VarType.Integer);
-        boolean s_bool2 = varCheck.check(start, VarType.Long);
-        boolean s_bool4 = varCheck.check(start, VarType.Float);
-        boolean s_bool3 = varCheck.check(start, VarType.Double);
-
-        boolean e_bool1 = varCheck.check(end, VarType.Integer);
-        boolean e_bool2 = varCheck.check(end, VarType.Long);
-        boolean e_bool3 = varCheck.check(end, VarType.Double);
-        boolean e_bool4 = varCheck.check(end, VarType.Float);
-
-        boolean bool1 = s_bool1 || s_bool2 || s_bool3 || s_bool4;
-        boolean bool2 = e_bool1 || e_bool2 || e_bool3 || e_bool4;
-        return bool1 && bool2;
-    }
+//
+//    private void stack(String line) throws Exception {
+//        Stack<Integer> stack = new Stack<>();
+//        for (int i = 0; i<line.length(); i++) {
+//            if (line.charAt(i) == left) stack.add(i);
+//            else if (line.charAt(i) == right) {
+//                if (stack.isEmpty()) throw new Exception("괄호 짝이 일치하지 않습니다.");
+//                int start = stack.pop();
+//                //(10>25)
+//                String numbers = line.substring(start, i+1);
+//                line = line.replaceFirst(numbers, );
+//            }
+//        }
+//    }
 }
