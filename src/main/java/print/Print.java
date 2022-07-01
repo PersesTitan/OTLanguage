9130916a65b5e72be75454f6737a87ad5f9f8c26
PersesTitan <ToramPersesTitan@gmail.com>
2022-07-02 00:02:01 +0900

    조건 추가

--- src/main/java/print/Print.java
+++ src/main/java/print/Print.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 
 public class Print extends Setting implements PrintWork {
     private static final String SPECIFIED = "ㅅㅁㅅ";
-    private final String patternText = "(\\n|^)\\s*ㅅㅁㅅ\\s";
+    private final String patternText = "(\\n|^)\\s*ㅅㅁㅅ($|\\s)";
     private final Pattern pattern = Pattern.compile(patternText);
 
     /**
