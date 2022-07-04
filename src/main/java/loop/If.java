6e5f1ef8a29618bfd066b502d113186a67e0c298
PersesTitan <ToramPersesTitan@gmail.com>
2022-07-04 19:22:39 +0900

    if 동작 처리

--- src/main/java/loop/If.java
+++ src/main/java/loop/If.java
@@ -4,6 +4,7 @@ import item.Setting;
 import item.work.LoopWork;
 
 import java.util.StringTokenizer;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class If extends Setting implements LoopWork {
@@ -14,18 +15,15 @@ public class If extends Setting implements LoopWork {
 
     @Override
     public void start(String line) throws Exception {
-        //?ㅅ? 제거 작업
-        line = line.strip().substring(SPECIFIED.length());
+
         StringTokenizer tokenizer = new StringTokenizer(line);
-        while(tokenizer.hasMoreTokens()) {
-            
-        }
-        line = line.replace("ㅇㅇ", "true");
-        line = line.replace("ㄴㄴ", "false");
-        if (changeBool(line)) {
-            //동작 적는 곳
-        } else {
-            //false 일때의 동작
+        tokenizer.nextToken();
+        String bool = tokenizer.nextToken();
+        String token = tokenizer.nextToken();
+
+        if (bool.equals("ㅇㅇ")) {
+            for (String lines : uuidMap.get(token).split("\\n"))
+                super.start(lines);
         }
     }
 
