1a8a3082547b1663b82259b653566e5e9dfff7fd
PersesTitan <ToramPersesTitan@gmail.com>
2022-07-05 14:20:24 +0900

    Class 클래스 생성

--- src/main/java/extend/Class.java
+++ src/main/java/extend/Class.java
@@ -1,5 +1,17 @@
 package extend;
 
-public class Class {
+import java.util.regex.Pattern;
 
+public class Class implements ClassWork {
+    private final Pattern pattern = Pattern.compile("^\\s*ㅋㅅㅋ \\S");
+
+    @Override
+    public boolean check(String line) {
+        return pattern.matcher(line).find();
+    }
+
+    @Override
+    public void start(String line) {
+
+    }
 }
