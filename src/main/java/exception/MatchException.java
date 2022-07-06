8e68653a0215aa6255ca091de5b791fb5519dd49
PersesTitan <ToramPersesTitan@gmail.com>
2022-07-06 20:59:21 +0900

    사용하지 않는 로직 제거

--- src/main/java/exception/MatchException.java
+++ src/main/java/exception/MatchException.java
@@ -17,8 +17,4 @@ public class MatchException extends RuntimeException {
     public MatchException(Throwable cause) {
         super(cause);
     }
-
-    protected MatchException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
 }
--- src/main/java/exception/VariableTypeDifferentException.java
+++ src/main/java/exception/VariableTypeDifferentException.java
@@ -17,8 +17,4 @@ public class VariableTypeDifferentException extends RuntimeException {
     public VariableTypeDifferentException(Throwable cause) {
         super(cause);
     }
-
-    protected VariableTypeDifferentException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
 }
