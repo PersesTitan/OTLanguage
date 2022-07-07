0d01e23cedd66b43ab2536c3628a28ed99fdea52
PersesTitan <ToramPersesTitan@gmail.com>
2022-07-07 19:07:54 +0900

    강제 출력 생성

--- src/main/java/print/PriorityPrintln.java
+++ src/main/java/print/PriorityPrintln.java
@@ -1,4 +1,20 @@
 package print;
 
-public class PriorityPrintln {
+import item.work.PrintWork;
+
+import java.util.regex.Pattern;
+
+public class PriorityPrintln implements PrintWork {
+    private final String PATTERN = "^\\s*!ㅆㅁㅆ!(\\s|$)";
+    private final Pattern pattern = Pattern.compile(PATTERN);
+
+    @Override
+    public void start(String line) {
+        System.out.println(line.replaceFirst(PATTERN, ""));
+    }
+
+    @Override
+    public boolean check(String line) throws Exception {
+        return pattern.matcher(line).find();
+    }
 }
