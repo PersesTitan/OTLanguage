5679cba67217cf23fc867b53135bdbf8703182d4
PersesTitan <ToramPersesTitan@gmail.com>
2022-07-07 19:03:54 +0900

    강제 출력 추가

--- src/main/java/item/ActivityItem.java
+++ src/main/java/item/ActivityItem.java
@@ -2,6 +2,8 @@ package item;
 
 import calculation.Account;
 import calculation.Calculation;
+import item.work.PrintWork;
+import print.*;
 import variable.*;
 import comparison.Comparison;
 import comparison.ComparisonBool;
@@ -12,9 +14,6 @@ import item.work.ComparisonWork;
 import loop.Bracket;
 import loop.For;
 import loop.If;
-import print.Print;
-import print.Println;
-import print.ScannerP;
 
 public interface ActivityItem {
     String typeErrorMessage = "타입 오류가 발생하였습니다.";
@@ -23,6 +22,9 @@ public interface ActivityItem {
     ScannerP scannerP = new ScannerP();
     Print print = new Print();
     Println println = new Println();
+    PriorityPrint priorityPrint = new PriorityPrint();
+    PriorityPrintln priorityPrintln = new PriorityPrintln();
+
     BooleanP booleanP = new BooleanP();
     CharacterP characterP = new CharacterP();
     DoubleP doubleP = new DoubleP();
