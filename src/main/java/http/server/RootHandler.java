4b139d566089eb7c9f3bd08af1ae2ae94c65410c
PersesTitan <ToramPersesTitan@gmail.com>
2022-07-09 19:40:44 +0900

    Post, GET 데이터 받아오기

--- src/main/java/http/server/RootHandler.java
+++ src/main/java/http/server/RootHandler.java
@@ -3,15 +3,16 @@ package http.server;
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-import http.items.Color;
+import http.handler.HttpGetHandler;
+import http.handler.HttpPostHandler;
 import http.items.Temporary;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 public class RootHandler implements HttpHandler, Temporary {
 
@@ -31,16 +32,18 @@ public class RootHandler implements HttpHandler, Temporary {
                 headers.add("Content-Type", "text/html;charset=UTF-8");
                 headers.add("Content-Length", String.valueOf(contentLen));
 
-                //[2022-01-01 01:01:12][GET] [PATH] /test | [QueryString] username=Test
-                String dateFormat = "yyyy-MM-dd H:mm:ss";
+                String method = exchange.getRequestMethod();
                 String query = exchange.getRequestURI().getQuery();
-                System.out.printf("[%s]", new SimpleDateFormat(dateFormat).format(new Date()));
-                System.out.printf("%s[%s]%s", Color.cyan, exchange.getRequestMethod(), Color.exit);
-                System.out.printf(" %s[PATH]%s %s ", Color.blue, Color.exit, path == null ? "" : path);
-                System.out.printf("| %s[QueryString]%s %s\n", Color.blue, Color.exit, query == null ? "" : query);
+
+                String value;
+                if (method.equals("POST")) value = new HttpPostHandler().handle(exchange);
+                else value = new HttpGetHandler().handle(exchange);
+
+                printLog(method, path, value);
 
                 exchange.sendResponseHeaders(200, contentLen);
                 responsive.write(content);
+
 
             } else {
                 if (!path.equals("/favicon.ico")) System.out.println(path + "가 정의되어 있지 않습니다.");
