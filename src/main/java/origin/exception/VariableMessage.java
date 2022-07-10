1116c44e79493460d01198eee03d052aaf4ef99e
PersesTitan <ToramPersesTitan@gmail.com>
2022-07-10 22:50:36 +0900

    에러 메세지 생성

---
+++ src/main/java/origin/exception/VariableMessage.java
@@ -0,0 +1,9 @@
+package origin.exception;
+
+public interface VariableMessage {
+    String typeMatchError = "변수 타입이 일치하지 않습니다.";
+    String sameVariable = "은(는) 이미 존재하는 변수명 입니다.";
+    String doNotFind = "은(는) 정의되지 않은 변수 입니다.";
+    String doNotDefine = "은(는) 정의 되지 않은 타입입니다.";
+    String noHaveInitial = "초기값이 존재하지 않습니다.";
+}
