d992083e652c5c0505b1624f8142253376b840e0
PersesTitan <ToramPersesTitan@gmail.com>
2022-07-10 22:44:51 +0900

    Handler 인식 변경

--- src/main/java/http/handler/HttpPostHandler.java
+++ src/main/java/http/handler/HttpPostHandler.java
@@ -13,16 +13,15 @@ import java.util.Map;
 public class HttpPostHandler {
     public String handle(HttpExchange exchange) throws IOException {
         Map<String, Object> parameters = new HashMap<>();
-        InputStreamReader isr = new InputStreamReader(exchange.getRequestBody(), StandardCharsets.UTF_8);
-        BufferedReader br = new BufferedReader(isr);
-        String query = br.readLine();
-        UriParser uriParse = new UriParser();
+        var isr = new InputStreamReader(exchange.getRequestBody(), StandardCharsets.UTF_8);
+        var br = new BufferedReader(isr);
+        var query = br.readLine();
+        var uriParse = new UriParser();
         uriParse.parsesQuery(query, parameters);
         Temporary.POST.putAll(parameters);
 
-        StringBuilder response = new StringBuilder();
-        parameters.forEach((key, value) -> response.append(key).append("=").append(value).append("&"));
-        int responseSize = response.toString().length();
-        return responseSize<=0 ? "" : response.substring(0, responseSize-1).strip();
+        var response = new StringBuilder();
+        parameters.forEach((key, value) -> response.append(key).append("=").append(value).append(" "));
+        return response.toString();
     }
 }
