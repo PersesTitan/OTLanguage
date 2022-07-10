b3768277dbcc0f61f55b799eb4531091839b6879
PersesTitan <ToramPersesTitan@gmail.com>
2022-07-10 22:48:10 +0900

    서버 정의 변경

--- src/main/java/http/server/Server.java
+++ src/main/java/http/server/Server.java
@@ -4,30 +4,17 @@ import http.items.Color;
 import http.items.Temporary;
 
 public class Server implements Temporary {
+    public static HttpServerManager httpServerManager;
 
     public Server() {
-        HttpServerManager httpServerManager = new HttpServerManager();
-        try {
-            httpServerManager.start();
-            if (pathMap.isEmpty()) System.out.printf("%s경로가 존재하지 않습니다.%s\n", Color.red, Color.exit);
-            else {
-                while (true) {}
-            }
-        } finally {
-            httpServerManager.stop();
-        }
+        httpServerManager = new HttpServerManager();
+        httpServerManager.start();
+        if (pathMap.isEmpty()) System.out.printf("%s경로가 존재하지 않습니다.%s\n", Color.RED, Color.RESET);
     }
 
     public Server(int port) {
-        HttpServerManager httpServerManager = new HttpServerManager(port);
-        try {
-            httpServerManager.start();
-            if (pathMap.isEmpty()) System.out.printf("%s경로가 존재하지 않습니다.%s\n", Color.red, Color.exit);
-            else {
-                while (true) {}
-            }
-        } finally {
-            httpServerManager.stop();
-        }
+        httpServerManager = new HttpServerManager(port);
+        httpServerManager.start();
+        if (pathMap.isEmpty()) System.out.printf("%s경로가 존재하지 않습니다.%s\n", Color.RED, Color.RESET);
     }
 }
