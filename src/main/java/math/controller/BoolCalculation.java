452a8d690bdd9bb68eee56d198c4db4ccec62f83
PersesTitan <torampersesTitan@gmail.com>
2022-07-15 22:01:43 +0900

    예러 코드 변경

--- src/main/java/math/controller/BoolCalculation.java
+++ src/main/java/math/controller/BoolCalculation.java
@@ -1,32 +1,34 @@
-package origin.comparison;
+package math.controller;
 
-import origin.item.work.ComparisonWork;
+import math.model.CalculationWork;
+import origin.exception.MatchException;
+import origin.exception.MatchMessage;
 
 import java.util.Stack;
 import java.util.StringTokenizer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-//ㅇㅇ ㄸ ㄴㄴ ㅇㄴ 와 같은 연산자 변환
-public class ComparisonBool implements ComparisonWork {
+public class BoolCalculation implements CalculationWork {
     private final String notPattern = "ㅇㄴ\\s*(ㅇㅇ|ㄴㄴ)";
     private final String PATTERN = "(ㅇㅇ|ㄴㄴ)\\s*[ㄸㄲ]\\s*(ㅇㅇ|ㄴㄴ)";
-    private final String bool = "ㅇㅇ|ㄴㄴ";
-    private final String sing = "[ㄸㄲ]";
     private final Pattern not = Pattern.compile(notPattern);
     private final Pattern pattern = Pattern.compile(PATTERN);
-    private final Pattern boolPattern = Pattern.compile(bool);
-    private final Pattern singPattern = Pattern.compile(sing);
     private final static char left = '(';
     private final static char right = ')';
 
     @Override
-    public String start(String line) throws Exception {
+    public String start(String line) {
         return checkRun(line);
     }
 
+    @Override
+    public boolean check(String line) {
+        return not.matcher(line).find() || pattern.matcher(line).find();
+    }
+
     //괄호 있는지 확인 하고 계산
-    private String checkRun(String line) throws Exception {
+    private String checkRun(String line) {
         line = notChange(line);
         if (line.contains("(") || line.contains(")")) {
             line = stack(line);
@@ -35,9 +37,8 @@ public class ComparisonBool implements ComparisonWork {
         return cut(line);
     }
 
-
     //ㅇㅇ ㄸ ㄴㄴ => ㅇㅇ
-    private String cut(String line) throws Exception {
+    private String cut(String line) {
         Matcher matcher = pattern.matcher(line);
         while (matcher.find()) {
             String value = matcher.group();
@@ -51,19 +52,19 @@ public class ComparisonBool implements ComparisonWork {
                 boolean b = sing.equals("ㄸ") ? (b1 || b2) : (b1 && b2);
                 line = line.replaceFirst(value, b ? "ㅇㅇ" : "ㄴㄴ");
             } catch (Exception ignored) {
-                throw new Exception("문법 오류 발생");
+                throw new MatchException(MatchMessage.boolError);
             }
         }
         return line;
     }
 
     //====================================================================================
-    private String stack(String line) throws Exception {
+    private String stack(String line) {
         Stack<Integer> stack = new Stack<>();
         for (int i = 0; i<line.length(); i++) {
             if (line.charAt(i) == left) stack.add(i);
             else if (line.charAt(i) == right) {
-                if (stack.isEmpty()) throw new Exception("괄호 짝이 일치하지 않습니다.");
+                if (stack.isEmpty()) throw new MatchException(MatchMessage.matchError);
                 int start = stack.pop();
                 String bool = line.substring(start, i+1);
                 line = line.replace(bool, cut(bool.substring(1, bool.length()-1)));
@@ -82,10 +83,5 @@ public class ComparisonBool implements ComparisonWork {
             else line = line.replaceFirst(value, "ㅇㅇ");
         }
         return line;
-    }
-
-    @Override
-    public boolean check(String line) throws Exception {
-        return not.matcher(line).find() || pattern.matcher(line).find();
     }
 }
