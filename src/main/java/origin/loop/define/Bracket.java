f949a6acd81ddfebeecccf0f5fe7847fb5118450
PersesTitan <torampersesTitan@gmail.com>
2022-07-15 22:02:27 +0900

    동작 방식 변경

--- src/main/java/origin/loop/define/Bracket.java
+++ src/main/java/origin/loop/define/Bracket.java
@@ -1,21 +1,28 @@
-package origin.loop;
+package origin.loop.define;
 
-import origin.item.Setting;
-import origin.item.work.Check;
+import origin.exception.MatchException;
+import origin.exception.MatchMessage;
+import origin.loop.model.LoopWork;
+import origin.variable.model.Repository;
 
+import java.util.Stack;
 import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Bracket extends Setting implements Check {
+public class Bracket implements Repository {
 
     private final static char left = '{';
     private final static char right = '}';
 
-    //괄호가 존재하면 아이디로 대체
-    public String bracket(String total) throws Exception {
-        stack.clear();
+    private String deleteEnter(String total) {
+        for (LoopWork loop : loopWorks) total = setMatcher(total, loop.getPattern());
+        return total;
+    }
 
+    //괄호가 존재하면 아이디로 대체
+    public String bracket(String total) {
+        Stack<Integer> stack = new Stack<>();
         total = deleteEnter(total);
 
         for (int i = 0; i < total.length(); i++) {
@@ -24,7 +31,7 @@ public class Bracket extends Setting implements Check {
             }
 
             if (total.charAt(i) == right) {
-                if (stack.empty()) throw new Exception("괄호 짝이 맞지 않습니다.");
+                if (stack.empty()) throw new MatchException(MatchMessage.matchError);
                 String uuid = UUID.randomUUID().toString();
                 int start = stack.pop();
                 String bl = total.substring(start, i+1);
@@ -34,13 +41,6 @@ public class Bracket extends Setting implements Check {
                 uuidMap.put(uuid, bl);
             }
         }
-        return total;
-    }
-
-    private String deleteEnter(String total) {
-        total = setMatcher(total, For.patternText);
-        total = setMatcher(total, If.patternText);
-
         return total;
     }
 
@@ -54,15 +54,16 @@ public class Bracket extends Setting implements Check {
         while (matcher.find()) {
             String group = matcher.group();
             Matcher m = p.matcher(group);
-            if (m.find())
-                total = total.replace(group, m.group().strip().concat(Character.toString(left)));
+            if (m.find()) {
+                String change = m.group().strip().concat(Character.toString(left)) + "\n";
+                total = total.replace(group, change);
+            }
         }
 
         return total;
     }
 
     //uuid 가 존재하는지 체크함
-    @Override
     public boolean check(String line) {
         if (uuidMap.isEmpty()) return false;
         return uuidMap.entrySet()
