680d25b62154f821a8d8b1a148778200fe2935ab
PersesTitan <torampersesTitan@gmail.com>
2022-07-18 18:33:14 +0900

    create DBError Setting

---
+++ src/main/java/origin/exception/DBException.java
@@ -0,0 +1,15 @@
+package origin.exception;
+
+public class DBException extends RuntimeException {
+    public DBException() {
+        super();
+    }
+
+    public DBException(String message) {
+        super(message);
+    }
+
+    public DBException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/origin/exception/DBMessage.java
@@ -0,0 +1,5 @@
+package origin.exception;
+
+public interface DBMessage {
+    String error = "디비생성에 실패하였습니다.";
+}
