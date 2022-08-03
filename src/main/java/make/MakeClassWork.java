e903adaaded49ac3de0cba3fe1aea775e093f8bb
PersesTitan <torampersesTitan@gmail.com>
2022-08-03 13:51:36 +0900

    create MakeClassWork

---
+++ src/main/java/make/MakeClassWork.java
@@ -0,0 +1,9 @@
+package make;
+
+import make.abs.AbsString;
+import make.abs.AbsVoid;
+
+public interface MakeClassWork {
+    void addVoid(AbsVoid absVoid);
+    void addString(AbsString absString);
+}
