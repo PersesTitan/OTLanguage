851b9e791fafb2a4017784c4d6d3ac4a1f4640dc
PersesTitan <torampersesTitan@gmail.com>
2022-08-03 13:03:40 +0900

    make DDL SQLException

--- src/main/java/http/controller/db/controller/sql/DDL.java
+++ src/main/java/http/controller/db/controller/sql/DDL.java
@@ -7,27 +7,18 @@ import java.sql.SQLException;
 
 public class DDL {
 
-    public void create(String sql, Connection con) {
-        try (var stat = con.createStatement()) {
-            stat.execute(sql);
-        } catch (SQLException ignored) {
-            System.out.printf("%s%s에 실패하였습니다.%s\n", Color.RED, "CREATE", Color.RESET);
-        }
+    public void create(Connection con, String sql) throws SQLException {
+        var stat = con.createStatement();
+        stat.execute(sql);
     }
 
-    public void alter(String sql, Connection con) {
-        try (var stat = con.createStatement()) {
-            stat.execute(sql);
-        } catch (SQLException ignored) {
-            System.out.printf("%s%s에 실패하였습니다.%s\n", Color.RED, "ALTER", Color.RESET);
-        }
+    public void alter(Connection con, String sql) throws SQLException {
+        var stat = con.createStatement();
+        stat.execute(sql);
     }
 
-    public void drop(String sql, Connection con) {
-        try (var stat = con.createStatement()) {
-            stat.execute(sql);
-        } catch (SQLException ignored) {
-            System.out.printf("%s%s에 실패하였습니다.%s\n", Color.RED, "DROP", Color.RESET);
-        }
+    public void drop(Connection con, String sql) throws SQLException {
+        var stat = con.createStatement();
+        stat.execute(sql);
     }
 }
