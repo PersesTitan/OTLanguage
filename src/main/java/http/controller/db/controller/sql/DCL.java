1a211879b33966e80c8d172f95bb54bb535636a9
PersesTitan <torampersesTitan@gmail.com>
2022-08-03 13:03:33 +0900

    make DCL SQLException

--- src/main/java/http/controller/db/controller/sql/DCL.java
+++ src/main/java/http/controller/db/controller/sql/DCL.java
@@ -7,19 +7,11 @@ import java.sql.SQLException;
 
 public class DCL {
 
-    public void commit(Connection con) {
-        try {
-            con.commit();
-        } catch (SQLException ignored) {
-            System.out.printf("%s%s에 실패하였습니다.%s\n", Color.RED, "COMMIT", Color.RESET);
-        }
+    public void commit(Connection con) throws SQLException {
+        con.commit();
     }
 
-    public void rollback(Connection con) {
-        try {
-            con.rollback();
-        } catch (SQLException e) {
-            System.out.printf("%s%s에 실패하였습니다.%s\n", Color.RED, "ROLLBACK", Color.RESET);
-        }
+    public void rollback(Connection con) throws SQLException {
+        con.rollback();
     }
 }
