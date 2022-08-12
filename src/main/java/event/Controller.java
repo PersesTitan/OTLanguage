4f6141faa04d0846368612ccb8bd531a6ff99fe3
PersesTitan <torampersesTitan@gmail.com>
2022-08-12 22:34:35 +0900

    update Controller add finish, addCustomClassWork, customGetVariableWork, customSetVariableWork

--- src/main/java/event/Controller.java
+++ src/main/java/event/Controller.java
@@ -1,25 +1,32 @@
 package event;
 
+import custom.tool.CustomGetVariable;
+import custom.tool.CustomSetVariable;
 import event.list.ListSetting;
 import http.controller.HttpGetPost;
 import http.controller.PortVariable;
+import make.custom.variable.controller.AddCustomClass;
 import origin.consol.controller.ConsoleScanner;
 import origin.loop.define.Bracket;
 import origin.variable.controller.GetVariable;
 import origin.variable.controller.SetVariable;
-import origin.variable.controller.list.ListVariable;
-import origin.variable.model.VariableListWork;
+import system.start.Finish;
+import system.work.FinishWork;
 
 public interface Controller extends ListSetting {
     //변수
     GetVariable getVariable = new GetVariable();
     SetVariable setVariable = new SetVariable();
-    //출력, 입력
-    ConsoleScanner consoleScanner = new ConsoleScanner();
-    //괄호를 토큰으로 변환
-    Bracket bracket = new Bracket();
-    //html 전용 변수
-    PortVariable portVariable = new PortVariable();
-    //post, get 값 가져오기
-    HttpGetPost httpGetPost = new HttpGetPost();
+    ConsoleScanner consoleScanner = new ConsoleScanner(); //출력, 입력
+    Bracket bracket = new Bracket(); //괄호를 토큰으로 변환
+    PortVariable portVariable = new PortVariable(); //html 전용 변수
+    HttpGetPost httpGetPost = new HttpGetPost(); //post, get 값 가져오기
+
+    //시스템
+    FinishWork finishWork = new Finish("ㄲㅌㄲ"); //강제 종료
+
+    // 커스텀 클래스
+    AddCustomClass addCustomClassWork = new AddCustomClass("ㅋㅅㅋ");
+    CustomGetVariable customGetVariableWork = new CustomGetVariable();
+    CustomSetVariable customSetVariableWork = new CustomSetVariable();
 }
