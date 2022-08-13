c639ed4394eb981dd2e4e327666f208aedbac0f5
PersesTitan <torampersesTitan@gmail.com>
2022-08-14 00:38:51 +0900

    delete MainPattenVoid

--- src/main/java/make/method/define/MakePatternVoid.java
+++
@@ -1,9 +0,0 @@
-package make.method.define;
-
-import java.util.regex.Pattern;
-
-public interface MakePatternVoid {
-    boolean check(String line);
-    String getPatternText();
-    Pattern getPattern();
-}
