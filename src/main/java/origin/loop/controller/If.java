8f71fbb843f5a596e43ebf4656e2c7795d27a8e5
PersesTitan <torampersesTitan@gmail.com>
2022-09-11 00:10:26 +0900

    Update If.java

--- src/main/java/origin/loop/controller/If.java
+++ src/main/java/origin/loop/controller/If.java
@@ -1,15 +1,22 @@
 package origin.loop.controller;
 
 import event.Setting;
+import origin.exception.MatchException;
+import origin.exception.MatchMessage;
 import origin.loop.model.LoopWork;
 import origin.variable.model.Repository;
 
+import java.util.Arrays;
 import java.util.StringTokenizer;
 import java.util.regex.Pattern;
 
 public class If extends Setting implements LoopWork, Repository {
     //시작 또는 줄바꿈 [공백] ?ㅅ? [공백 1개 이상]
-    private static final String patternText = "(\\n|^)\\s*\\?ㅅ\\?\\s+";
+    private static final String uuidPattern = "[a-zA-Z0-9-]+";
+    private static final String patternText =
+            "(\\n|^)\\s*\\?ㅅ\\?\\s+(ㅇㅇ|ㄴㄴ)\\s+"+uuidPattern+ //if
+                    "(\\s+\\?ㅈ\\?\\s+(ㅇㅇ|ㄴㄴ)\\s+"+uuidPattern+")*"+   //else if
+                    "(\\?ㅉ\\?\\s+"+uuidPattern+")?"; //else
     private static final Pattern pattern = Pattern.compile(patternText);
 
     @Override
@@ -19,14 +26,34 @@ public class If extends Setting implements LoopWork, Repository {
 
     @Override
     public void start(String line) {
-        StringTokenizer tokenizer = new StringTokenizer(line);
-        tokenizer.nextToken();
-        String bool = tokenizer.nextToken();
-        String token = tokenizer.nextToken();
-
-        if (bool.equals("ㅇㅇ")) {
-            for (String lines : uuidMap.get(token).split("\\n")) super.start(lines);
+        for (String l : lineStart(line).split("\\n")) {
+            Setting.start(l);
         }
+    }
+
+    //라인을 반환함
+    public static String lineStart(String line) {
+        if (pattern.matcher(line).find()) {
+            StringTokenizer tokenizer = new StringTokenizer(line);
+            while (tokenizer.hasMoreTokens()) {
+                String sing = tokenizer.nextToken();
+                switch (sing) {
+                    case "?ㅅ?":
+                    case "?ㅈ?":
+                        if (tokenizer.nextToken().equals("ㅇㅇ"))
+                            return checkUuid(tokenizer.nextToken());
+                        break;
+                    case "?ㅉ?":
+                        return checkUuid(tokenizer.nextToken());
+                }
+            }
+        }
+        return "";
+    }
+
+    private static String checkUuid(String key) {
+        if (uuidMap.containsKey(key)) return uuidMap.get(key);
+        else throw new MatchException(MatchMessage.grammarError);
     }
 
     @Override
