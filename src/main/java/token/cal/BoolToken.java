1a7dd10b5362c93791c6eea82fae585f12cd339a
PersesTitan <torampersesTitan@gmail.com>
2022-09-12 00:03:01 +0900

    Create 3.0.0 BoolToken

---
+++ src/main/java/token/cal/BoolToken.java
@@ -0,0 +1,16 @@
+package token.cal;
+
+public interface BoolToken {
+    // BOOLEAN
+    String TRUE = "ㅇㅇ";
+    String FALSE = "ㄴㄴ";
+    String OR = "ㄸ";
+    String AND = "ㄲ";
+    String NOT = "ㅇㄴ";
+
+    String BIG = "ㅇ>ㅇ";
+    String SMALL = "ㅇ<ㅇ";
+    String SAME = "ㅇ=ㅇ";
+    String BIG_SAME = "ㅇ>=ㅇ";
+    String SMALL_SAME = "ㅇ<=ㅇ";
+}
