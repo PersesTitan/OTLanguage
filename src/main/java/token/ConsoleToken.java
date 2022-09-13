a5c3ea3fed975cab9279114494f11d54737662a7
PersesTitan <torampersesTitan@gmail.com>
2022-09-13 20:17:40 +0900

    Create ConsoleToken.java

---
+++ src/main/java/token/ConsoleToken.java
@@ -0,0 +1,16 @@
+package token;
+
+public interface ConsoleToken {
+    String PRIORITY_PRINT = "!ㅅㅁㅅ!";
+    String PRIORITY_PRINTLN = "!ㅆㅁㅆ!";
+    String PRIORITY_PRINT_SPACE = "!ㅆㅁㅅ!";
+    String PRIORITY_PRINT_TAP = "!ㅅㅁㅆ!";
+    String PRINT = "ㅅㅁㅅ";
+    String PRINTLN = "ㅆㅁㅆ";
+    String PRINT_SPACE = "ㅆㅁㅅ";
+    String PRINT_TAP = "ㅅㅁㅆ";
+
+    String SCANNER = "ㅅㅇㅅ";
+
+    String FILE = "ㅍㅅㅍ";
+}
