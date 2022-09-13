4c460713445b47a3e0c7974ffebd405b8e8d0e47
PersesTitan <torampersesTitan@gmail.com>
2022-09-14 00:56:28 +0900

    Create Token.java

---
+++ src/main/java/bin/token/Token.java
@@ -0,0 +1,36 @@
+package bin.token;
+
+public interface Token extends MergeToken {
+    String START = "^";
+    String END = "$";
+    String BLANK = "\\s*";
+    String BLANKS = "\\s+";
+
+    // SING
+    String STAR = "\\*";
+    String PIPE = "\\|";
+    String HYPHEN = "\\-";
+    String PLUS = "\\+";
+    String CARET = "\\^";
+    String DOT = "\\.";
+    String QUESTION = "\\?";
+    String SL = "\\(";
+    String SR = "\\)";
+    String ML = "\\{";
+    String MR = "\\}";
+    String BL = "\\[";
+    String BR = "\\]";
+    String BACKSLASH = "\\\\";
+
+    String NO_PATTERN = "(?<=(^|[^\\\\])(\\\\\\\\)?+)";
+    String YES_PATTERN = "(?<=(^|[^\\\\])(\\\\\\\\)?+)\\\\";
+
+    String COMMA = ",";
+    String MAP_EQUAL = NO_PATTERN + "=";
+
+    String ACCESS = "~";
+    String LESS_SIGN = "<";
+    String GREATER_SIGN = ">";
+    String EXCLAMATION = "!";
+    String AMPERSAND = "&";
+}
