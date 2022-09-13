b77d04e674422ac9fbbc7737968cfe25e8c5045f
PersesTitan <torampersesTitan@gmail.com>
2022-09-14 00:58:40 +0900

    Create PrintTap.java

---
+++ src/main/java/bin/orign/console/PrintTap.java
@@ -0,0 +1,29 @@
+package bin.orign.console;
+
+import bin.token.ConsoleToken;
+import bin.token.Token;
+import work.StartWork;
+
+import java.util.Map;
+import java.util.regex.Pattern;
+
+public class PrintTap implements
+        StartWork, ConsoleToken, Token {
+    private final String patternText;
+    private final Pattern pattern;
+
+    public PrintTap(String type) {
+        this.patternText = startMerge(type, orMerge(BLANKS, END));
+        this.pattern = Pattern.compile(this.patternText);
+    }
+
+    @Override
+    public boolean check(String line) {
+        return pattern.matcher(line).find();
+    }
+
+    @Override
+    public void start(String line, String origen, Map<String, Map<String, Object>>[] repositoryArray) {
+        System.out.print(line.replaceFirst(this.patternText, "") + "\t");
+    }
+}
