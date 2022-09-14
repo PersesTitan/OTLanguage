7f969550cae9675bc46b30f37604d2da2f78e657
PersesTitan <torampersesTitan@gmail.com>
2022-09-15 00:29:52 +0900

    Create TryCatch.java
    Co-Authored-By: LockBell <109267661+LockBell@users.noreply.github.com>

---
+++ src/main/java/bin/apply/sys/run/TryCatch.java
@@ -0,0 +1,22 @@
+package bin.apply.sys.run;
+
+import work.StartWork;
+
+import java.util.Map;
+
+public class TryCatch implements StartWork {
+
+    public TryCatch(String type) {
+
+    }
+
+    @Override
+    public boolean check(String line) {
+        return false;
+    }
+
+    @Override
+    public void start(String line, String origen, Map<String, Map<String, Object>>[] repositoryArray) {
+
+    }
+}
