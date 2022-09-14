6dbd266ac92155556f873c2e16f502d5acb457bb
PersesTitan <torampersesTitan@gmail.com>
2022-09-15 00:28:37 +0900

    Create Separator.java

---
+++ src/main/java/bin/apply/sys/item/Separator.java
@@ -0,0 +1,7 @@
+package bin.apply.sys.item;
+
+public interface Separator {
+    String SEPARATOR_FILE = System.getProperty("file.separator");   // /
+    String SEPARATOR_PATH = System.getProperty("path.separator");   // :
+    String SEPARATOR_LINE = System.getProperty("line.separator");   // \n
+}
