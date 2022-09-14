e6f043e38917252edbd8ad198a37b90441d46cf5
PersesTitan <torampersesTitan@gmail.com>
2022-09-15 00:28:17 +0900

    Create VariableType.java

---
+++ src/main/java/bin/check/VariableType.java
@@ -0,0 +1,19 @@
+package bin.check;
+
+public class VariableType {
+    public enum Origin {
+        Boolean, Character, Double, Float, Integer, Long, String
+    }
+
+    public enum List {
+        Boolean, Character, Double, Float, Integer, Long, String
+    }
+
+    public enum Map {
+        Boolean, Character, Double, Float, Integer, Long, String
+    }
+
+    public enum Set {
+        Boolean, Character, Double, Float, Integer, Long, String
+    }
+}
