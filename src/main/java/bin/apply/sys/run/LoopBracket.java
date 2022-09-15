e3e7ccb4ee58d718b70653cbea3b06ab0a34b27f
PersesTitan <torampersesTitan@gmail.com>
2022-09-16 00:32:25 +0900

    Update LoopBracket.java

--- src/main/java/bin/apply/sys/run/LoopBracket.java
+++ src/main/java/bin/apply/sys/run/LoopBracket.java
@@ -10,15 +10,15 @@ public class LoopBracket implements LoopToken, Token {
     private final String LINE = "\\n" + LINE_NUMBER + BLANK;
     private final String IfPattern = "(^|\\n)" + LINE_NUMBER + BLANK + IF + "[^\\n]+" + BRACE_STYLE;
     private final String Else_IfPattern = ELSE_IF + "[^\\n]+" + BRACE_STYLE;
-    private final String ElsePattern = ELSE + BLANK + BLANK + BRACE_STYLE;
+    private final String ElsePattern = ELSE + BLANK + BRACE_STYLE;
 
     private final String ElsePatternNo = "(" + ElsePattern + ")?";
     private final String Else_IfPatterns = "(" + Else_IfPattern + ")+" + ElsePatternNo;
 
-    private final String pattern1 = "(?<=" + LINE + Else_IfPattern + ")" + LINE + "(?=" + ElsePattern + ")";
-    private final String pattern2 = "(?<=" + LINE + Else_IfPattern + ")" + LINE + "(?=" + Else_IfPatterns + ")";
-    private final String pattern3 = "(?<=" + IfPattern + ")" + LINE + "(?=" + ElsePattern + ")";
-    private final String pattern4 = "(?<=" + IfPattern + ")" + LINE + "(?=" + Else_IfPatterns + ")";
+    private final String pattern1 = "(?!" + LINE + Else_IfPattern + ")" + LINE + "(?=" + ElsePattern + ")";
+    private final String pattern2 = "(?!" + LINE + Else_IfPattern + ")" + LINE + "(?=" + Else_IfPatterns + ")";
+    private final String pattern3 = "(?!" + IfPattern + ")" + LINE + "(?=" + ElsePattern + ")";
+    private final String pattern4 = "(?!" + IfPattern + ")" + LINE + "(?=" + Else_IfPatterns + ")";
 
     public String deleteEnter(String total) {
         return total
