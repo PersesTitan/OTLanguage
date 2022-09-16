5e33d84b348fb5308423a42b6c91adf46fc9b676
PersesTitan <torampersesTitan@gmail.com>
2022-09-17 00:26:55 +0900

    Update Controller.java

--- src/main/java/bin/apply/Controller.java
+++ src/main/java/bin/apply/Controller.java
@@ -1,14 +1,15 @@
 package bin.apply;
 
 import bin.apply.sys.make.Bracket;
-import bin.apply.sys.run.LoopBracket;
 import bin.calculator.NumberCalculator;
 import bin.calculator.bool.BoolCalculator;
 import bin.calculator.bool.CompareCalculator;
+import bin.orign.loop.tool.LoopController;
 
 public interface Controller {
     NumberCalculator numberCalculator = new NumberCalculator();
     BoolCalculator boolCalculator = new BoolCalculator();
     CompareCalculator compareCalculator = new CompareCalculator();
+    LoopController loopController = new LoopController();
     Bracket bracket = new Bracket();
 }
