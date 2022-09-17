b83851eefbe9a223f84d21138880310a308c6965
PersesTitan <torampersesTitan@gmail.com>
2022-09-18 00:35:07 +0900

    Delete FileFailMessage.java

--- src/main/java/origin/exception/FileFailMessage.java
+++
@@ -1,7 +0,0 @@
-package origin.exception;
-
-public interface FileFailMessage {
-    String doNotFindFile = "파일을 찾을 수 없습니다.";
-    String doNotReadFile = "파일을 읽을 수 없습니다.";
-    String notMatchExtension = "읽을 수 없는 확장자 입니다.";
-}
