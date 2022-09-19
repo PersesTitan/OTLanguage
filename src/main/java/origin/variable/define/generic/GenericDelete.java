7465d5379901e9787a2da1609d31168e3fb457b8
PersesTitan <torampersesTitan@gmail.com>
2022-09-20 01:20:36 +0900

    Delete GenericDelete.java

--- src/main/java/origin/variable/define/generic/GenericDelete.java
+++
@@ -1,8 +0,0 @@
-package origin.variable.define.generic;
-
-import java.util.Map;
-
-public interface GenericDelete {
-    boolean check(String line);
-    void start(String line, Map<String, Map<String, Object>> repository);
-}
