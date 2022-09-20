e30e869b186660a26f39ba7831072da159cd6a8b
PersesTitan <torampersesTitan@gmail.com>
2022-09-21 07:42:11 +0900

    Update StartWork.java

--- src/main/java/work/StartWork.java
+++ src/main/java/work/StartWork.java
@@ -6,5 +6,6 @@ import java.util.Set;
 
 public interface StartWork extends Serializable {
     boolean check(String line);
-    void start(String line, String origen, Map<String, Map<String, Object>>[] repositoryArray);
+    void start(String line, String origen,
+               Map<String, Map<String, Object>>[] repositoryArray);
 }
