d0d88195ddc481c9d4f39d5f35c8a829a4f15634
PersesTitan <torampersesTitan@gmail.com>
2022-09-21 00:06:33 +0900

    Update HttpRepository.java

--- src/main/java/cos/http/controller/HttpRepository.java
+++ src/main/java/cos/http/controller/HttpRepository.java
@@ -1,8 +1,58 @@
 package cos.http.controller;
 
-import java.util.HashMap;
-import java.util.Map;
+import bin.apply.sys.item.Color;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public interface HttpRepository {
-    Map<String, String> httpRepository = new HashMap<>();
+    String dateFormat = "yyyy-MM-dd H:mm:ss";
+    Map<HttpMethod, Map<String, String>> httpRepository = new HashMap<>(); // url, html
+
+    default void startServerPrint() {
+        System.out.printf(
+                """
+                %s╭───────────╮╭──╮╭────────╮╭──────────╮╭─────────╮%s
+                %s│  ╭─────╮  │╰──╯│  ╭─────╯│  ╭────╮  ││  ╭───╮  │%s
+                %s│  ╰─────╯  │╭──╮│  ╰─────╮│  │    │  ││  │   │  │%s
+                %s│  ┌────────╯│  │╰─────╮  ││  │    │  ││  │   │  │%s
+                %s│  │ ╭──────╮│  │╭─────╯  ││  ╰────╯  ││  │   │  │%s
+                %s│  │ │ ╭──╮ ││  │╰────────╯╰──────────╯╰──╯   ╰──╯%s
+                %s│  │ │ ╰──╯ ││  │%s        %s== OTLanguage ==%s
+                %s╰──╯ ╰──────╯╰──╯%s        %s==   Poison   ==%s
+
+                """,
+                Color.PURPLE_BRIGHT, Color.RESET,
+                Color.PURPLE_BRIGHT, Color.RESET,
+                Color.PURPLE_BRIGHT, Color.RESET,
+                Color.PURPLE_BRIGHT, Color.RESET,
+                Color.PURPLE_BRIGHT, Color.RESET,
+                Color.PURPLE_BRIGHT, Color.RESET,
+                Color.PURPLE_BRIGHT, Color.RESET,
+                Color.PURPLE, Color.RESET,
+                Color.PURPLE_BRIGHT, Color.RESET,
+                Color.PURPLE, Color.RESET);
+
+        System.out.printf("%s[%s]%s%s[Poison Server 시작]%s ",
+                Color.YELLOW,
+                new SimpleDateFormat(dateFormat).format(new Date()),
+                Color.RESET, Color.GREEN, Color.RESET);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() ->
+                System.out.printf("%s[%s]%s%s[Poison Server 종료]%s \n",
+                        Color.YELLOW,
+                        new SimpleDateFormat(dateFormat).format(new Date()),
+                        Color.RESET, Color.GREEN, Color.RESET)));
+    }
+
+    default void printLog(HttpMethod method, String path, String query) {
+        //[2022-07-12 18:58:53] GET   [경로]  /  | [값]  name=hi
+        System.out.printf("%s[%s]%s", Color.YELLOW, new SimpleDateFormat(dateFormat).format(new Date()), Color.RESET);
+        if (method.equals(HttpMethod.POST)) System.out.printf("%s %s %s", Color.POST_PRINT, method, Color.RESET);
+        else System.out.printf("%s %s  %s", Color.GET_PRINT, method, Color.RESET);
+        System.out.print(" [경로] ");
+        System.out.printf("%s %s %s",Color.QUERY_PRINT, path == null ? "" : path, Color.RESET);
+        System.out.print(" | [값] ");
+        System.out.printf("%s %s %s\n", Color.QUERY_PRINT, query == null ? "" : query, Color.RESET);
+    }
 }
