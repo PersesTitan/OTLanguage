ec8c6a74753e3b4045ef88a54ce17beeaa26f30c
PersesTitan <torampersesTitan@gmail.com>
2022-09-21 00:05:14 +0900

    Create UriParser.java

---
+++ src/main/java/cos/poison/controller/UriParser.java
@@ -0,0 +1,32 @@
+package cos.poison.controller;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class UriParser {
+    public void parsesQuery(String query, Map<String, Object> parameter) throws UnsupportedEncodingException {
+        if (query != null) {
+            for (String pair : query.split("&")) {
+                var params = pair.split("=");
+                String key = null;
+                String value = null;
+                if (params.length > 0) key = URLDecoder.decode(params[0], System.getProperty("file.encoding"));
+                if (params.length > 1) value = URLDecoder.decode(params[1], System.getProperty("file.encoding"));
+
+                if (parameter.containsKey(key)) {
+                    Object obj = parameter.get(key);
+                    if(obj instanceof List<?>) return;
+                    else if (obj instanceof String) {
+                        List<String> values = new ArrayList<>();
+                        values.add((String) obj);
+                        values.add(value);
+                        parameter.put(key, values);
+                    }
+                } else parameter.put(key, value);
+            }
+        }
+    }
+}
