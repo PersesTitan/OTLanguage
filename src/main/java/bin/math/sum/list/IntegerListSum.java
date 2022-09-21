c646a8a85bab4a588dc769fb62d914823cc2635c
PersesTitan <torampersesTitan@gmail.com>
2022-09-21 20:15:56 +0900

    Update IntegerListSum.java

--- src/main/java/bin/math/sum/list/IntegerListSum.java
+++ src/main/java/bin/math/sum/list/IntegerListSum.java
@@ -4,6 +4,7 @@ import bin.orign.variable.list.get.GetList;
 import bin.token.LoopToken;
 import work.ReturnWork;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -13,7 +14,7 @@ public class IntegerListSum implements ReturnWork, LoopToken, GetList {
     private final String type = LIST_INTEGER;
     private final String value = orMerge(VARIABLE_ACCESS, NUMBER_LIST);
     private final String patternText =
-            VARIABLE_GET_S + value + ACCESS + LIST_SUM + VARIABLE_GET_E;
+            VARIABLE_GET_S + value + LIST_SUM + VARIABLE_GET_E;
     private final Pattern pattern = Pattern.compile(patternText);
 
     @Override
@@ -28,18 +29,18 @@ public class IntegerListSum implements ReturnWork, LoopToken, GetList {
         while (matcher.find()) {
             String group = matcher.group();
             String groups = bothEndCut(group)
-                    .replaceFirst(ACCESS + LIST_SUM + END, "");
+                    .replaceFirst(LIST_SUM + END, "");
             if (groups.matches(VARIABLE_ACCESS)) {
                 var repository = repositoryArray[accessCount(groups)].get(type);
                 String variableName = groups.replaceAll(ACCESS, "");
                 if (repository.containsKey(variableName)) {
-                    List<Integer> list = (List<Integer>) repository.get(variableName);
+                    LinkedList<Integer> list = (LinkedList<Integer>) repository.get(variableName);
                     String sum = Integer.toString(list.stream().mapToInt(v -> v).sum());
                     line = line.replace(group, sum);
                 }
             } else {
                 try {
-                    List<Integer> list = getIntegerList(groups);
+                    LinkedList<Integer> list = getIntegerList(groups);
                     String sum = Integer.toString(list.stream().mapToInt(v -> v).sum());
                     line = line.replace(group, sum);
                 } catch (Exception ignored) {}
