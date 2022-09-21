291119634830cb12023a930cc7be68869bd01802
PersesTitan <torampersesTitan@gmail.com>
2022-09-22 07:39:09 +0900

    Update IntegerSetSum.java

--- src/main/java/bin/math/sum/set/IntegerSetSum.java
+++ src/main/java/bin/math/sum/set/IntegerSetSum.java
@@ -1,17 +1,19 @@
 package bin.math.sum.set;
 
 import bin.orign.variable.list.get.GetList;
+import bin.orign.variable.set.get.GetSet;
 import bin.token.LoopToken;
 import work.ReturnWork;
 
+import java.util.LinkedHashSet;
 import java.util.Map;
-import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class IntegerSetSum implements ReturnWork, LoopToken, GetList {
+public class IntegerSetSum implements ReturnWork, LoopToken, GetSet {
+    private final String value = orMerge(VARIABLE_ACCESS, NUMBER_LIST);
     private final String patternText =
-            VARIABLE_GET_S + VARIABLE_ACCESS + ACCESS + LIST_SUM + VARIABLE_GET_E;
+            VARIABLE_GET_S + value + SET_SUM + VARIABLE_GET_E;
     private final Pattern pattern = Pattern.compile(patternText);
 
     @Override
@@ -26,15 +28,21 @@ public class IntegerSetSum implements ReturnWork, LoopToken, GetList {
         while (matcher.find()) {
             String group = matcher.group();
             String groups = bothEndCut(group)
-                    .replaceFirst(ACCESS + SET_SUM + END, "");
+                    .replaceFirst(SET_SUM + END, "");
             if (groups.matches(VARIABLE_ACCESS)) {
                 var repository = repositoryArray[accessCount(groups)].get(SET_INTEGER);
                 String variableName = groups.replaceAll(ACCESS, "");
                 if (repository.containsKey(variableName)) {
-                    Set<Integer> list = (Set<Integer>) repository.get(variableName);
+                    LinkedHashSet<Integer> list = (LinkedHashSet<Integer>) repository.get(variableName);
                     String sum = Integer.toString(list.stream().mapToInt(v -> v).sum());
                     line = line.replace(group, sum);
                 }
+            } else {
+                try {
+                    LinkedHashSet<Integer> list = getIntegerSet(groups);
+                    String sum = Integer.toString(list.stream().mapToInt(v -> v).sum());
+                    line = line.replace(group, sum);
+                } catch (Exception ignored) {}
             }
         }
         return line;
