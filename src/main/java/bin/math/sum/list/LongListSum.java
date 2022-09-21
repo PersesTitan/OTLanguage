264938c31a831d5d584d15f5ab2640204156b6d5
PersesTitan <torampersesTitan@gmail.com>
2022-09-21 20:16:00 +0900

    Update LongListSum.java

--- src/main/java/bin/math/sum/list/LongListSum.java
+++ src/main/java/bin/math/sum/list/LongListSum.java
@@ -4,6 +4,7 @@ import bin.orign.variable.list.get.GetList;
 import bin.token.LoopToken;
 import work.ReturnWork;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -13,7 +14,7 @@ public class LongListSum implements ReturnWork, LoopToken, GetList {
     private final String type = LIST_LONG;
     private final String value = orMerge(VARIABLE_ACCESS, NUMBER_LIST);
     private final String patternText =
-            VARIABLE_GET_S + value + ACCESS + LIST_SUM + VARIABLE_GET_E;
+            VARIABLE_GET_S + value + LIST_SUM + VARIABLE_GET_E;
     private final Pattern pattern = Pattern.compile(patternText);
 
     @Override
@@ -28,18 +29,18 @@ public class LongListSum implements ReturnWork, LoopToken, GetList {
         while (matcher.find()) {
             String group = matcher.group();
             String groups = bothEndCut(group)
-                    .replaceFirst(ACCESS + LIST_SUM + END, "");
+                    .replaceFirst(LIST_SUM + END, "");
             if (groups.matches(VARIABLE_ACCESS)) {
                 var repository = repositoryArray[accessCount(groups)].get(type);
                 String variableName = groups.replaceAll(ACCESS, "");
                 if (repository.containsKey(variableName)) {
-                    List<Long> list = (List<Long>) repository.get(variableName);
+                    LinkedList<Long> list = (LinkedList<Long>) repository.get(variableName);
                     String sum = Long.toString(list.stream().mapToLong(v -> v).sum());
                     line = line.replace(group, sum);
                 }
             } else {
                 try {
-                    List<Long> list = getLongList(groups);
+                    LinkedList<Long> list = getLongList(groups);
                     String sum = Long.toString(list.stream().mapToLong(v -> v).sum());
                     line = line.replace(group, sum);
                 } catch (Exception ignored) {}
