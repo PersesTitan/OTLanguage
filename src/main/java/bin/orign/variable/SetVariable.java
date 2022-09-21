b812a3d5b70e4595f142bdf6fca0fa188c151e28
PersesTitan <torampersesTitan@gmail.com>
2022-09-22 07:39:27 +0900

    Update SetVariable.java

--- src/main/java/bin/orign/variable/SetVariable.java
+++ src/main/java/bin/orign/variable/SetVariable.java
@@ -4,35 +4,39 @@ import bin.token.VariableToken;
 import work.ReturnWork;
 
 import java.util.Map;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SetVariable implements ReturnWork, VariableToken {
     private final static Pattern pattern = Pattern.compile(VARIABLE);
+    private Matcher matcher;
 
     @Override
     public boolean check(String line) {
-        return pattern.matcher(line).find();
+        return (matcher = pattern.matcher(line)).find();
     }
 
     @Override
     public String start(String line,
                         Map<String, Map<String, Object>>[] repositoryArray) {
-        for (int i = 0; i<repositoryArray.length; i++) {
-            var repository = repositoryArray[i];
-            line = getValue(line, i, repository);
+        matcher.reset();
+        while (matcher.find()) {
+            String group = matcher.group();
+            int access = accessCount(group);
+            String variableName = bothEndCut(group).replaceFirst(START + ACCESS + "+", "");
+            var repository = repositoryArray[access];
+            line = getValue(group, variableName, line, repository);
         }
+
         return line;
     }
 
-    private String getValue(String line, int i,
+
+    private String getValue(String group, String variableName, String line,
                             Map<String, Map<String, Object>> repository) {
         for (var rep : repository.values()) {
-            for (Map.Entry<String, Object> entry : rep.entrySet()) {
-                String k = entry.getKey();
-                Object v = entry.getValue();
-                String key = VARIABLE_GET_S + ACCESS.repeat(i) + k + VARIABLE_GET_E;
-                line = line.replace(key, v.toString());
-            }
+            if (rep.containsKey(variableName))
+                line = line.replaceFirst(Pattern.quote(group), rep.get(variableName).toString());
         }
         return line;
     }
