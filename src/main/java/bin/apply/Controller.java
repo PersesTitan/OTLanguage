32f32883f51e143e075e1d0eaeeb0626eb6175db
PersesTitan <torampersesTitan@gmail.com>
2022-09-21 20:15:35 +0900

    Update Controller.java

--- src/main/java/bin/apply/Controller.java
+++ src/main/java/bin/apply/Controller.java
@@ -4,7 +4,10 @@ import bin.apply.sys.make.Bracket;
 import bin.calculator.NumberCalculator;
 import bin.calculator.bool.BoolCalculator;
 import bin.calculator.bool.CompareCalculator;
+import bin.exception.ConsoleException;
 import bin.orign.loop.tool.LoopController;
+
+import java.io.*;
 
 public interface Controller {
     NumberCalculator numberCalculator = new NumberCalculator();
@@ -12,4 +15,24 @@ public interface Controller {
     CompareCalculator compareCalculator = new CompareCalculator();
     LoopController loopController = new LoopController();
     Bracket bracket = new Bracket();
+    // 입력, 출력
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static String scanner() {
+        try {
+            return br.readLine();
+        } catch (IOException e) {
+            throw ConsoleException.scannerError();
+        }
+    }
+
+    static void print(String text) {
+        try {
+            bw.write(text);
+            bw.flush();
+        } catch (IOException e) {
+            throw ConsoleException.printError();
+        }
+    }
 }
