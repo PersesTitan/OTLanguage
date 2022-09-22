e48b3747e932630244d3cfa70982c315fc472d46
PersesTitan <torampersesTitan@gmail.com>
2022-09-23 08:40:01 +0900

    Update PoisonGet.java

--- src/main/java/cos/poison/method/PoisonGet.java
+++ src/main/java/cos/poison/method/PoisonGet.java
@@ -1,23 +1,34 @@
 package cos.poison.method;
 
+import bin.apply.Setting;
+import bin.exception.FileException;
 import bin.exception.MatchException;
 import bin.token.LoopToken;
 import work.StartWork;
 
+import java.io.File;
 import java.util.Map;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static bin.apply.sys.item.Separator.SEPARATOR_FILE;
 
 public class PoisonGet implements LoopToken, StartWork {
     private final String patternText;
     private final Pattern pattern;
+    private final String param;
+    private final Pattern paramPattern;
 
     public PoisonGet(String className) {
-        String or = orMerge(TOTAL_LIST) + BLANKS + VARIABLE_NAME + VARIABLE_PUT + "\\S+";
+        String or = blackMerge(BL, orMerge(TOTAL_LIST) + BLANKS + VARIABLE_NAME + VARIABLE_PUT, BR);
+        this.param = "[" + ARGUMENT + "]" + "(" + or + ")*";
         this.patternText = startEndMerge(
                 className, ACCESS, POISON_GET, BLANKS,
-                "[" + ARGUMENT + "]", blackMerge("(", or, ")*"),
+                param,
                 BLANKS, BRACE_STYLE(), BLANKS, RETURN);
+
         this.pattern = Pattern.compile(this.patternText);
+        this.paramPattern = Pattern.compile(START + param);
     }
 
     @Override
@@ -31,6 +42,23 @@ public class PoisonGet implements LoopToken, StartWork {
         // ㅍㅇㅍ~ㅍㅅㅍ, [sub/][ㅇㅅㅇ ㅁ:ㅁ] (start,1,10) => index.html
         String[] tokens = line.strip().split(BLANKS, 2);
         if (tokens.length != 2) throw MatchException.grammarError();
-        String token = tokens[1];
+        String token = tokens[1]; // [sub/][ㅇㅅㅇ ㅁ:ㅁ] (start,1,10) => index.html
+        Matcher param = this.paramPattern.matcher(token);
+        if (param.find()) {
+            String group = param.group(); // [sub/][ㅇㅅㅇ ㅁ:ㅁ]
+            String[] values = token.strip().split(BLANK + RETURN_TOKEN + BLANK, 2);
+            if (values.length != 2) throw MatchException.grammarError();
+            String[] params = bothEndCut(group).split(BR + BL);
+            String path = getPath(values[1]);
+
+            
+        }
+    }
+
+    private String getPath(String path) {
+        String p = path.replace(ACCESS, SEPARATOR_FILE);
+        String pat = Setting.path + (p.startsWith(SEPARATOR_FILE) ? p : SEPARATOR_FILE + p);
+        if (new File(pat).isFile()) return pat;
+        else throw FileException.pathNoHaveError();
     }
 }
