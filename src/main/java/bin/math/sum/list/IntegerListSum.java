685653d511f3c956b702a3767ace4925d0f590f7
PersesTitan <torampersesTitan@gmail.com>
2022-09-25 19:56:00 +0900

    Update IntegerListSum.java

--- src/main/java/bin/math/sum/list/IntegerListSum.java
+++ src/main/java/bin/math/sum/list/IntegerListSum.java
@@ -11,7 +11,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class IntegerListSum implements ReturnWork, LoopToken, GetList {
-    private final String type = LIST_INTEGER;
     private final String value = orMerge(VARIABLE_ACCESS, NUMBER_LIST);
     private final String patternText =
             VARIABLE_GET_S + value + LIST_SUM + VARIABLE_GET_E;
@@ -31,14 +30,17 @@ public class IntegerListSum implements ReturnWork, LoopToken, GetList {
             String groups = bothEndCut(group)
                     .replaceFirst(LIST_SUM + END, "");
             if (groups.matches(VARIABLE_ACCESS)) {
-                var repository = repositoryArray[accessCount(groups)].get(type);
-                String variableName = groups.replaceAll(ACCESS, "");
+                int accessCount = accessCount(groups);
+                if (repositoryArray.length <= accessCount) continue;
+                var repository = repositoryArray[accessCount].get(LIST_INTEGER);
+                String variableName = groups.substring(accessCount);
                 if (repository.containsKey(variableName)) {
                     LinkedList<Integer> list = (LinkedList<Integer>) repository.get(variableName);
                     String sum = Integer.toString(list.stream().mapToInt(v -> v).sum());
                     line = line.replace(group, sum);
                 }
             } else {
+                // 변환이 불가능하면 에러발생
                 try {
                     LinkedList<Integer> list = getIntegerList(groups);
                     String sum = Integer.toString(list.stream().mapToInt(v -> v).sum());
