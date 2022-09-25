bee13d25cbad0c11c8a7d85e377e7a24478d07b0
PersesTitan <torampersesTitan@gmail.com>
2022-09-25 19:56:13 +0900

    Update Separator.java

--- src/main/java/bin/apply/sys/item/Separator.java
+++ src/main/java/bin/apply/sys/item/Separator.java
@@ -3,5 +3,6 @@ package bin.apply.sys.item;
 public interface Separator {
     String SEPARATOR_FILE = System.getProperty("file.separator");   // /
     String SEPARATOR_PATH = System.getProperty("path.separator");   // :
-    String SEPARATOR_LINE = System.getProperty("line.separator");   // \n
+    String SEPARATOR_LINE = System.getProperty("line.separator");   // \n \r
+    String SEPARATOR_HOME = System.getProperty("user.home");        // /User/name
 }
