06d4e4c8a5dbfd09350336dacef08c2ba98f83be
PersesTitan <torampersesTitan@gmail.com>
2022-10-03 12:42:09 +0900

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,18 @@
+import bin.apply.Repository;
 import bin.apply.Setting;
 import bin.apply.sys.item.HpMap;
 import bin.apply.sys.make.StartLine;
 import bin.exception.FileException;
-import com.sun.management.OperatingSystemMXBean;
-import org.apache.commons.collections4.queue.PredicatedQueue;
-import work.setting.ReadOTLM;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.management.ManagementFactory;
+import java.lang.annotation.Repeatable;
 import java.nio.charset.StandardCharsets;
-import java.text.DecimalFormat;
-import java.util.Map;
 import java.util.regex.Pattern;
 
-import static bin.apply.Controller.br;
-import static bin.apply.Controller.bw;
+import static bin.apply.Controller.*;
 import static bin.apply.sys.item.SystemSetting.extensionCheck;
 import static bin.token.Token.*;
 import static bin.token.VariableToken.TOTAL_LIST;
@@ -44,15 +39,13 @@ public class Main extends Setting {
             new Main(args);
         } catch (FileException e) {
             FileException.printErrorMessage(e, Setting.mainPath);
-        } finally {
-            try {
-                br.close();
-                bw.close();
-            } catch (IOException ignored) {}
-        }
+        } finally {try {br.close(); bw.close();} catch (IOException ignored) {}}
     }
 
     private Main(String[] args) {
+        shell();
+        System.exit(0);
+
         if (args.length <= 0) throw FileException.noFindError();
         File file = new File(args[0]); //파일 생성
         Setting.mainPath = file.getAbsolutePath();
@@ -62,7 +55,7 @@ public class Main extends Setting {
         else if (!file.canRead()) throw FileException.noReadError();
         else if (!extensionCheck(file.getName())) throw FileException.rightExtension();
         Setting.firstStart();
-//        TOTAL_LIST.forEach(v -> COPY_REPOSITORY.put(v, new HpMap<>()));
+        TOTAL_LIST.forEach(v -> COPY_REPOSITORY.put(v, new HpMap<>()));
 //        repository.putAll((Map<String, Map<String, Object>>) COPY_REPOSITORY.clone());
 //        new ReadOTLM().readSetting("system.otls");
 
@@ -76,7 +69,17 @@ public class Main extends Setting {
         pause();
     }
 
+    private void shell() {
+        Setting.firstStart();
+        while (true) {
+            System.out.print(">>> ");
+            String line = scanner();
+            if (line.equals("끝")) break;
+            Setting.start(line, line, repository);
+        }
+    }
+
     private void pause() {
-        while (true) {}
+        for (;;) {}
     }
 }
