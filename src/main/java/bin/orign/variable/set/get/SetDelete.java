2035a841ade2bcbcdbee3a1ece56c9d0ed87f816
PersesTitan <torampersesTitan@gmail.com>
2022-10-06 01:08:33 +0900

    Update SetDelete.java

--- src/main/java/bin/orign/variable/set/get/SetDelete.java
+++ src/main/java/bin/orign/variable/set/get/SetDelete.java
@@ -8,22 +8,23 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.Map;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class SetDelete implements StartWork, LoopToken {
     private final String type;
-    private final Pattern pattern;
+    private final Matcher matcher;
 
     public SetDelete(String type) {
         this.type = type;
         String patternText = startEndMerge(VARIABLE_ACCESS, type, "\\d+");
-        this.pattern = Pattern.compile(patternText);
+        this.matcher = Pattern.compile(patternText).matcher("");
     }
 
     @Override
     public boolean check(String line) {
-        return pattern.matcher(line).find();
+        return matcher.reset(line).find();
     }
 
     @Override
