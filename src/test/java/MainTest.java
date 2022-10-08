020d7b345573a384289193ac05ac74011184fc11
PersesTitan <torampersesTitan@gmail.com>
2022-10-09 02:12:30 +0900

    Update MainTest.java

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,3 +1,5 @@
+import bin.token.LoopToken;
+
 import javax.swing.*;
 import java.awt.*;
 import java.net.MalformedURLException;
@@ -8,10 +10,38 @@ import java.util.regex.Pattern;
 
 import static bin.apply.sys.item.Separator.SEPARATOR_LINE;
 
-public class MainTest {
-    public static void main(String[] args) throws AWTException, MalformedURLException {
-        Taskbar.getTaskbar().setIconImage(Toolkit.getDefaultToolkit().getImage("/Users/persestitan/Documents/GitHub/OTLanguage/OTLanguage.png"));
+public class MainTest implements LoopToken {
+    public static void main(String[] args) {
+//        Taskbar.getTaskbar().setIconImage(Toolkit.getDefaultToolkit().getImage("/Users/persestitan/Documents/GitHub/OTLanguage/OTLanguage.png"));
 //        Robot robot = new Robot();
 //        JFrame frame = new JFrame("test");
+        new MainTest();
+    }
+
+    public MainTest() {
+        String params = orMerge(TOTAL_LIST) + BLANKS + VARIABLE_HTML;
+        String patternText = startEndMerge(
+                METHOD, BLANKS, VARIABLE_HTML,
+                "(", "(", BL, params, BR, ")+", "|", BL, BR, ")",
+                BLANKS, BRACE_STYLE(),
+                "(", BLANK, RETURN, ")?");
+
+        String[] strings = {"ㅇㅅㅇ ㅁㄴㄴ", "ㅇㅈㅇ ㅁㄴㅇㄹ", "ㅇㅈㅇ ㅁㄴㅇㄹ"};
+        System.out.println(Arrays.deepToString(getParams(strings)));
+        System.out.println(getParams(strings).length);
+        System.out.println(new String[0][0].length);
+
+        Pattern pattern = Pattern.compile(patternText);
+        System.out.println(Arrays.toString("ㅇㅅㅇ 매개변수][adf".split(BR + BL)));
+        System.out.println(pattern.matcher("ㅁㅅㅁ 메소드명[] (test,1,10)").find());
+        System.out.println(pattern.matcher("ㅁㅅㅁ 메소드명[ㅇㅈㅇ ㅁㄴㅇㄹ][ㅇㅈㅇ ㅁㄴㅇㄹ] (test,1,10)=> a").find());
+        System.out.println(pattern.matcher("ㅁㅅㅁ 메소드명[ㅇㅈㅇ] (test,1,10)").find());
+    }
+
+    private String[][] getParams(String[] params) {
+        int count = params.length;
+        String[][] param = new String[count][2];
+        for (int i = 0; i<count; i++) param[i] = matchSplitError(params[i], BLANKS, 2);
+        return param;
     }
 }
