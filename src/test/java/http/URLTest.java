df89ab682dd5c7d080a4ea65db426dbaba7b5886
PersesTitan <97427878+PersesTitan@users.noreply.github.com>
2022-10-14 17:40:01 +0900

    Create URLTest.java

---
+++ src/test/java/http/URLTest.java
@@ -0,0 +1,45 @@
+package http;
+
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpServer;
+import cos.poison.root.RootWork;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.*;
+import java.text.SimpleDateFormat;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class URLTest {
+    public static void main(String[] args) throws IOException {
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(9090), 0);
+        String newUrl ="/a";
+        server.createContext("/", t -> {
+            String response = "This is the response";
+//            Headers responseHeaders = t.getResponseHeaders();
+//            responseHeaders.set("Location", newUrl);
+
+            RootWork rootWork = new RootWork();
+            Headers headers = t.getResponseHeaders();
+            rootWork.setCookie(headers, "test1", "1", null, 20);
+            rootWork.setCookie(headers, "test2", "2", null, -1);
+
+            rootWork.getCookie(t.getRequestHeaders(), "test1");
+
+            t.sendResponseHeaders(200,0);
+
+
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+
+        server.start();
+    }
+
+}
