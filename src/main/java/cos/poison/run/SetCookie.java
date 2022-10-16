a1f9f5bef3d1bf938c2c993d0ffd2043444a1741
PersesTitan <torampersesTitan@gmail.com>
2022-10-17 08:04:24 +0900

    Create SetCookie.java

---
+++ src/main/java/cos/poison/run/SetCookie.java
@@ -0,0 +1,46 @@
+package cos.poison.run;
+
+import bin.exception.VariableException;
+import bin.token.LoopToken;
+import com.sun.net.httpserver.Headers;
+import cos.poison.root.RootWork;
+import cos.poison.work.PoisonStartWork;
+
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SetCookie implements RootWork, LoopToken, PoisonStartWork {
+    private final int length;
+    private final Matcher matcher;
+
+    public SetCookie(String type) {
+        this.length = type.replace("\\", "").length();
+        String patternText = startEndMerge(type, ARGUMENT.repeat(2), "(", ARGUMENT, "){0,2}");
+        this.matcher = Pattern.compile(patternText).matcher("");
+    }
+
+    @Override
+    public boolean check(String line) {
+        return matcher.reset(line).find();
+    }
+
+    @Override
+    public void start(String line, String origen, Headers responseHeader,
+                      Map<String, Map<String, Object>>[] repositoryArray) {
+        String[] tokens = matchSplitError(bothEndCut(line.strip(), length + 1, 1), BR + BL, 4);
+        switch (tokens.length) {
+            case 2 -> setCookie(responseHeader, tokens[0], tokens[1], null, -1);
+            case 3 -> {
+                if (Pattern.matches("[0-9]+", tokens[2]))
+                    setCookie(responseHeader, tokens[0], tokens[1], null, Integer.parseInt(tokens[2]));
+                else setCookie(responseHeader, tokens[0], tokens[1], tokens[2], -1);
+            }
+            case 4 -> {
+                if (Pattern.matches("[0-9]+", tokens[3]))
+                    setCookie(responseHeader, tokens[0], tokens[1], tokens[2], Integer.parseInt(tokens[3]));
+                else throw VariableException.typeMatch();
+            }
+        }
+    }
+}
