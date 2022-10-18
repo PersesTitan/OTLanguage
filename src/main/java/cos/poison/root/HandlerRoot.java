b9224403679bdc2b7a755c6606621b9c2830dd3d
PersesTitan <97427878+PersesTitan@users.noreply.github.com>
2022-10-18 15:03:39 +0900

    Update HandlerRoot.java

--- src/main/java/cos/poison/root/HandlerRoot.java
+++ src/main/java/cos/poison/root/HandlerRoot.java
@@ -9,22 +9,28 @@ import com.sun.net.httpserver.HttpHandler;
 import cos.http.controller.HttpMethod;
 import cos.http.controller.HttpRepository;
 import cos.poison.controller.HandlerItem;
-import cos.poison.handler.HandlerDao;
+import cos.poison.method.PoisonTools;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Map;
 
-import static bin.apply.Setting.COPY_REPOSITORY;
-import static bin.apply.Setting.warringMessage;
+import static bin.apply.Setting.*;
+import static bin.apply.sys.make.StartLine.errorCount;
+import static bin.apply.sys.make.StartLine.errorLine;
 import static cos.poison.Poison.variableHTML;
 import static cos.poison.controller.HttpServerManager.*;
 
-public class HandlerRoot implements HttpHandler, HttpRepository, SetVariableValue {
+public class HandlerRoot implements HttpHandler, HttpRepository, SetVariableValue, PoisonTools {
     private final Map<String, Map<String, Object>> repository = (Map<String, Map<String, Object>>) COPY_REPOSITORY.clone();
+    private final String defaultHtml;
+    public HandlerRoot(String defaultHtml) {
+        this.defaultHtml = defaultHtml;
+    }
 
     // 동작
     @Override
@@ -37,7 +43,14 @@ public class HandlerRoot implements HttpHandler, HttpRepository, SetVariableValu
             Headers responseHeader = exchange.getResponseHeaders(); // 응답(send)
             Headers requestHeader = exchange.getRequestHeaders();   // 요청(get)
             HandlerItem handlerItem = checkHandlerItem(method, path);
-            if (handlerItem != null) {
+            if (defaultHtml != null) {
+                exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK,0);
+                responseBody.write(defaultHtml.getBytes());
+            }
+
+            if (defaultHtml == null && handlerItem != null) {
+                if (handlerItem.contentType() != null)
+                    responseHeader.add("Content-Type", handlerItem.contentType());
                 // 정보 가져오기
                 var handlerDao = getHttpHandlerInf(method).handle(exchange);
                 Map<String, Object> parameters = handlerDao.parameters();
@@ -46,11 +59,8 @@ public class HandlerRoot implements HttpHandler, HttpRepository, SetVariableValu
                 printLog(method, path, handlerDao.value());
                 // 동작
                 serverStart(repository, handlerItem.startFinalTotal(), handlerItem.fileName());
-
                 exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK,0);
-                String responseValue = handlerItem.responseValue();
-                if (responseValue.endsWith(".html")) responseBody.write(html(responseValue));
-                else responseBody.write(responseValue.getBytes());
+                responseBody.write(getBody(handlerItem.responseValue()));
             } else {
                 // ==== 에러 동작 ====
                 if (!path.equals("/favicon.ico"))
@@ -61,59 +71,34 @@ public class HandlerRoot implements HttpHandler, HttpRepository, SetVariableValu
             try {exchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_METHOD, 0);}
             catch (IOException ignored) {}
         } catch (IOException ignored) {}
-
-//        String path = exchange.getRequestURI().getPath();
-//        String methodType = exchange.getRequestMethod();
-
-//        var map = methodType.equals(POST.name()) ? postMap : getMap;
-//        var method = methodType.equals(POST.name()) ? POST : GET;
-//        if (map.containsKey(path)) {
-//            var handlerItem = map.get(path);
-//            try (exchange; OutputStream responsive = exchange.getResponseBody()) {
-//                // 정보 받아오기
-//                var handlerDao = getHttpHandlerInf(method).handle(exchange);
-//                // 저장공간 생성
-//                Map<String, Object> parameters = handlerDao.parameters();
-//                setParameters(parameters, handlerItem.params());
-//
-//                printLog(method, path, handlerDao.value());
-//                serverStart(repository, handlerItem.startFinalTotal(), handlerItem.fileName());
-//
-//                String htmlTotal = Files.readString(Path.of(handlerItem.html()));
-//                ByteBuffer byteBuffer = StandardCharsets.UTF_8.encode(variableHTML.replace(htmlTotal));
-//
-//                int contentLen = byteBuffer.limit();
-//                byte[] content = new byte[contentLen];
-//                byteBuffer.get(content, 0, contentLen);
-//
-//                Headers headers = exchange.getResponseHeaders();
-//                headers.add("Content-Type", handlerItem.contentType());
-//                headers.add("Content-Length", String.valueOf(contentLen));
-//
-//                exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, contentLen);
-//                responsive.write(content);
-//            } catch (IOException e) {throw ServerException.serverReadError();}
-//        } else {
-//            if (!path.equals("/favicon.ico")) warringMessage("[" + method + "][" + path + "]가 정의되어 있지 않습니다.");
-//        }
     }
 
     // Body Value
-    private byte[] html(String responseValue) throws IOException {
-        String htmlTotal = Files.readString(Path.of(responseValue));
-        return variableHTML.replace(htmlTotal).getBytes();
+    private byte[] getBody(String responseValue) throws IOException {
+        if (responseValue.endsWith(".html"))
+            return variableHTML.replace(Files.readString(Path.of(getHtml(responseValue)))).getBytes();
+        else return responseValue.getBytes(StandardCharsets.UTF_16);
     }
 
     private void setParameters(Map<String, Object> parameters, String[][] params) {
         repository.values().forEach(Map::clear);
-        for (var param : params)
-            set(param[0], param[1], parameters.getOrDefault(param[2], DEFAULT.get(param[0])).toString(), repository);
+        for (var param : params) {
+            try {
+                if (parameters.containsKey(param[2]))
+                    set(param[0], param[1], parameters.get(param[2]).toString(), repository);
+            } catch (Exception ignored) {}
+        }
     }
 
     private void serverStart(Map<String, Map<String, Object>> repository,
                              String startFinalTotal, String fileName) {
         variableHTML.reset();
-        StartLine.startPoison(startFinalTotal, fileName, repository, Repository.repository);
+        try {
+            StartLine.startPoison(startFinalTotal, fileName, repository, Repository.repository);
+        } catch (Exception e) {
+            String error = String.format("Error Line %d (%s)", errorCount.get(), errorLine.get());
+            errorMessage(error);
+        }
     }
 
     private HandlerItem checkHandlerItem(HttpMethod method, String path) {
