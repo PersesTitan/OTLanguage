02c4f1c093d6dc436d4b4dd93d56be8b4c520079
PersesTitan <torampersesTitan@gmail.com>
2022-10-20 00:34:52 +0900

    Update CreateDoubleSet.java

--- src/main/java/bin/orign/variable/set/create/CreateDoubleSet.java
+++ src/main/java/bin/orign/variable/set/create/CreateDoubleSet.java
@@ -20,8 +20,7 @@ public class CreateDoubleSet implements
     private final Matcher matcher;
     private final String type;
 
-    public CreateDoubleSet(String type, Map<String, Map<String, Object>> repository) {
-        repository.put(type, new HpMap<>());
+    public CreateDoubleSet(String type) {
         this.patternText = startMerge(type, BLANKS, VARIABLE_NAME);
         this.matcher = Pattern.compile(patternText).matcher("");
         this.type = type;
