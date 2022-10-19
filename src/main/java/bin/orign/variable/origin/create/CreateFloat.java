49d457f829cbe398953083e08aa9e2d1ae7e9d5c
PersesTitan <torampersesTitan@gmail.com>
2022-10-20 00:35:13 +0900

    Update CreateFloat.java

--- src/main/java/bin/orign/variable/origin/create/CreateFloat.java
+++ src/main/java/bin/orign/variable/origin/create/CreateFloat.java
@@ -18,9 +18,8 @@ public class CreateFloat implements StartWork, VariableToken, Controller {
     private final Matcher matcher;
     private final String type;
 
-    public CreateFloat(String type, Map<String, Map<String, Object>> repository) {
+    public CreateFloat(String type) {
         String patternText = startMerge(type, BLANKS, VARIABLE_NAME, VARIABLE_PUT);
-        repository.put(type, new HpMap<>());
         this.matcher = Pattern.compile(patternText).matcher("");
         this.type = type;
     }
