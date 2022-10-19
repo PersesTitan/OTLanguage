7114adef2a32cb05f4d774b123b1c66126067ce4
PersesTitan <torampersesTitan@gmail.com>
2022-10-20 00:35:06 +0900

    Update CreateString.java

--- src/main/java/bin/orign/variable/origin/create/CreateString.java
+++ src/main/java/bin/orign/variable/origin/create/CreateString.java
@@ -16,9 +16,8 @@ public class CreateString implements StartWork, VariableToken, Controller {
     private final Matcher matcher;
     private final String type;
 
-    public CreateString(String type, Map<String, Map<String, Object>> repository) {
+    public CreateString(String type) {
         String patternText = startMerge(type, BLANKS, VARIABLE_NAME, VARIABLE_PUT);
-        repository.put(type, new HpMap<>());
         this.matcher = Pattern.compile(patternText).matcher("");
         this.type = type;
     }
