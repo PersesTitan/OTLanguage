052e3ff7efc8c1d44ef098bcf4c9b254f3a05a10
PersesTitan <torampersesTitan@gmail.com>
2022-10-20 00:37:26 +0900

    Update CreateFloatMap.java

--- src/main/java/bin/orign/variable/map/create/CreateFloatMap.java
+++ src/main/java/bin/orign/variable/map/create/CreateFloatMap.java
@@ -19,8 +19,7 @@ public class CreateFloatMap implements
     private final Matcher matcher;
     private final String type;
 
-    public CreateFloatMap(String type, Map<String, Map<String, Object>> repository) {
-        repository.put(type, new HpMap<>());
+    public CreateFloatMap(String type) {
         this.patternText = startMerge(type, BLANKS, VARIABLE_NAME);
         this.matcher = Pattern.compile(patternText).matcher("");
         this.type = type;
