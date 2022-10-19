857c39e77e01e46e7fd744a4ebd34d2b50b2f5ee
PersesTitan <torampersesTitan@gmail.com>
2022-10-20 00:34:38 +0900

    Update CreateLongSet.java

--- src/main/java/bin/orign/variable/set/create/CreateLongSet.java
+++ src/main/java/bin/orign/variable/set/create/CreateLongSet.java
@@ -20,8 +20,7 @@ public class CreateLongSet implements
     private final Matcher matcher;
     private final String type;
 
-    public CreateLongSet(String type, Map<String, Map<String, Object>> repository) {
-        repository.put(type, new HpMap<>());
+    public CreateLongSet(String type) {
         this.patternText = startMerge(type, BLANKS, VARIABLE_NAME);
         this.matcher = Pattern.compile(patternText).matcher("");
         this.type = type;
