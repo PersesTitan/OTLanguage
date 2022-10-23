e459bea2636e54b3b3122b14f268ba54d38a2a8b
PersesTitan <torampersesTitan@gmail.com>
2022-10-23 12:22:15 +0900

    Create CreateFileTest.java

---
+++ src/test/java/test/CreateFileTest.java
@@ -0,0 +1,30 @@
+package test;
+
+import org.apache.commons.codec.binary.Base64;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class CreateFileTest {
+    public static void main(String[] args) throws Exception {
+//        String encodstring = encodeFileToBase64Binary(new File("image/icon.png"));
+        String encodstring = encodeFileToBase64Binary(new File("image/icon.png"));
+        System.out.println(encodstring);
+        new File("image/icon.png");
+
+        Process process = Runtime.getRuntime().exec("");
+//        Image image = ImageIO.read();
+
+//        System.out.println(image.toString());
+//        Taskbar.getTaskbar().setIconImage(image);
+    }
+    private static String encodeFileToBase64Binary(File file) throws Exception{
+        FileInputStream fileInputStreamReader = new FileInputStream(file);
+        byte[] bytes = new byte[(int)file.length()];
+        fileInputStreamReader.read(bytes);
+        return new String(Base64.encodeBase64(bytes), "UTF-8");
+    }
+}
