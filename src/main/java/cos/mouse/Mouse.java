b996db8c7ba5c535c0feb19464eb04ab14dfc480
PersesTitan <torampersesTitan@gmail.com>
2022-10-23 12:22:40 +0900

    Update Mouse.java

--- src/main/java/cos/mouse/Mouse.java
+++ src/main/java/cos/mouse/Mouse.java
@@ -1,5 +1,10 @@
 package cos.mouse;
 
+import java.awt.*;
+
 public class Mouse {
+    static {
+        Taskbar.getTaskbar().setIconImage(Toolkit.getDefaultToolkit().getImage("/Users/persestitan/Documents/GitHub/OTLanguage/OTLanguage.png"));
+    }
 
 }
