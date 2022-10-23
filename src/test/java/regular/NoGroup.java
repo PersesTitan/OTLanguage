85fcd31b15a111595f5dee5778f811daa331433b
PersesTitan <torampersesTitan@gmail.com>
2022-10-23 12:22:18 +0900

    Create NoGroup.java

---
+++ src/test/java/regular/NoGroup.java
@@ -0,0 +1,13 @@
+package regular;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class NoGroup {
+    public static void main(String[] args) {
+        Matcher matcher = Pattern.compile("a(?=b)").matcher("ab");
+        if (matcher.find()) System.out.println(matcher.group());
+        matcher = Pattern.compile("(?<=a)b").matcher("ab");
+        if (matcher.find()) System.out.println(matcher.group());
+    }
+}
