dcd1e3929c3b880ae9938699165f96d33b534f9b
PersesTitan <torampersesTitan@gmail.com>
2022-10-23 23:45:35 +0900

    Update SetClear.java

--- src/main/java/bin/orign/variable/set/get/SetClear.java
+++ src/main/java/bin/orign/variable/set/get/SetClear.java
@@ -30,9 +30,9 @@ public class SetClear implements VariableToken, StartWork {
     public void start(String line, String origen,
                       Map<String, Map<String, Object>>[] repositoryArray) {
         line = line.strip();
-        int count = accessCount(line);
+        int count = accessCount(line, repositoryArray.length);
+        if (count == -1) throw VariableException.localNoVariable();
         String variableName = bothEndCut(line, count, type.length());
-        if (count > repositoryArray.length) throw VariableException.localNoVariable();
         getSet(count, variableName, repositoryArray).clear();
     }
 
