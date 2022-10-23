a89464b185627877a38140a7f18cf7d308bd660b
PersesTitan <torampersesTitan@gmail.com>
2022-10-23 12:22:25 +0900

    Create CalculatorTest.java

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,17 @@
+package calculator;
+
+import java.math.BigInteger;
+import java.util.HashSet;
+import java.util.Set;
+
+public class CalculatorTest {
+
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+
+    }
+
+    private void sum() {
+//        BigInteger bigInteger = new BigInteger();
+    }
+}
