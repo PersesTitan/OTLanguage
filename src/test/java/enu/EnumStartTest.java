7864298d940c09c6f91ec2158b1e13619ff2109c
PersesTitan <torampersesTitan@gmail.com>
2022-10-23 12:22:23 +0900

    Create EnumStartTest.java

---
+++ src/test/java/enu/EnumStartTest.java
@@ -0,0 +1,15 @@
+package enu;
+
+public class EnumStartTest {
+
+    public static void main(String[] args) {
+        TestEnum testEnum1 = TestEnum.INFO;
+        TestEnum testEnum2 = TestEnum.WARRING;
+        System.out.println(testEnum1.ordinal());
+        System.out.println(testEnum2.ordinal());
+    }
+
+    public enum TestEnum {
+        INFO, WARRING, ERROR
+    }
+}
