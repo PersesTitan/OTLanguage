540df3aa0c712cae7721b8d7276286f7e5798343
PersesTitan <torampersesTitan@gmail.com>
2022-10-23 12:23:19 +0900

    Update BoolCalculator.java

--- src/main/java/bin/calculator/bool/BoolCalculator.java
+++ src/main/java/bin/calculator/bool/BoolCalculator.java
@@ -6,58 +6,48 @@ import bin.token.Token;
 import bin.token.cal.BoolToken;
 import bin.token.cal.NumberToken;
 
+import java.util.StringTokenizer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class BoolCalculator implements
-        BoolToken, NumberToken, MergeToken, Token {
-    private final String sings = orMerge(AND, OR); // (ㄸ|ㄲ)
-    private final String bool = orMerge(TRUE, FALSE);
-    private final String bools =    // (ㅇㅇㄸㅇㅇ)
-            blackMerge(SL, orMerge(blackMerge(bool, sings, bool, "(", sings, bool, ")*"), bool), SR);
-
+public class BoolCalculator implements BoolToken, NumberToken, MergeToken, Token {
     public String start(String line) {
-        line = Controller.compareCalculator.start(line);
-        return blank(line);
+        return orAnd(Controller.compareCalculator.start(line));
     }
 
-    private String blank(String line) {
-        Matcher matcher = Pattern.compile(this.bools).matcher(line);
-        while (matcher.find()) {
-            String group = matcher.group(); // (계산식)
-            String bools = group.substring(1, group.length()-1); // 계산식
-            line = line.replaceFirst(this.bools, calculator(bools));
-            matcher.reset(line);
+    private final String BOOL = orMerge(TRUE, FALSE);
+    private final String orAnd = orMerge(AND, OR);
+    private final String orAndPatternText = BOOL + BLANK + orAnd + BLANK + BOOL;
+
+    private final Matcher matcherOrAnd = Pattern.compile(orAndPatternText).matcher("");
+    public String orAnd(String line) {
+        line = not(line);
+        while (matcherOrAnd.reset(line).find()) {
+            StringTokenizer tokenizer = new StringTokenizer(matcherOrAnd.group(), OR + AND, true);
+            boolean bool1 = tokenizer.nextToken().strip().equals(TRUE);
+            String sing = tokenizer.nextToken();
+            boolean bool2 = tokenizer.nextToken().strip().equals(TRUE);
+            boolean bool = sing.equals(OR) ? (bool1 || bool2) : (bool1 && bool2);
+            line = line.replaceFirst(orAndPatternText, bool ? TRUE : FALSE);
         }
-        return calculator(not(line));
+        return matcherOrAnd.reset((line = not(blank(line)))).find() ? orAnd(line) : line;
     }
 
-    private String calculator(String line) {
-        String patternText = blackMerge(this.bool, this.sings, this.bool);
-        Matcher matcher = Pattern.compile(patternText).matcher(line);
-        while (matcher.find()) {
-            String group = matcher.group().replaceAll(BLANKS, "");
-            String[] or = group.split(OR);
-            String[] and = group.split(AND);
-            if (or.length == 2)
-                line = line.replaceFirst(patternText, calculator(or[0], or[1], OR));
-            else if (and.length == 2)
-                line = line.replaceFirst(patternText, calculator(and[0], and[1], AND));
-            matcher.reset(line);
-        }
-        return line;
+    private final Matcher matcherNo = Pattern.compile(NOT + BLANK + BOOL).matcher("");
+    private String not(String value) {
+        return matcherNo.reset(value).find()
+                ? value
+                .replaceAll(NOT + BLANK + TRUE, FALSE)
+                .replaceAll(NOT + BLANK + FALSE, TRUE)
+                : value;
     }
 
-    private String not(String line) {
-        return line
-                .replaceAll(blackMerge(NOT, TRUE), FALSE)
-                .replaceAll(blackMerge(NOT, FALSE), TRUE);
-    }
-
-    private String calculator(String bool1, String bool2, String sing) {
-        boolean b1 = bool1.equals(TRUE);
-        boolean b2 = bool2.equals(TRUE);
-        if (sing.equals(OR)) return b1 || b2 ? TRUE : FALSE;
-        else return b1 && b2 ? TRUE : FALSE;
+    private final Matcher matcherBlank = Pattern.compile(SL + BLANK + BOOL + BLANK + SR).matcher("");
+    private String blank(String value) {
+        return matcherBlank.reset(value).find()
+                ? value
+                .replaceAll(SL + BLANK + TRUE + BLANK + SR, TRUE)
+                .replaceAll(SL + BLANK + FALSE + BLANK + SR, FALSE)
+                : value;
     }
 }
