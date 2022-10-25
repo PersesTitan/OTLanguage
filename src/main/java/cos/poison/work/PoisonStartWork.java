72bd0003900d0854aca27ca3dcaa2840355f2259
PersesTitan <97427878+PersesTitan@users.noreply.github.com>
2022-10-25 15:12:00 +0900

    Update PoisonStartWork.java

--- src/main/java/cos/poison/work/PoisonStartWork.java
+++ src/main/java/cos/poison/work/PoisonStartWork.java
@@ -1,16 +1,13 @@
 package cos.poison.work;
 
-import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import work.StartWork;
 
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
 public interface PoisonStartWork extends StartWork {
     void setExchange(HttpExchange exchange);
-    void setResponseHeader(Headers responseHeader);
     void setStatCode(AtomicInteger statCode);
     void setNowPath(AtomicReference<String> nowPath);
 }
