babaca1d3b710e53ac6dff06b2a997129bb80d37
PersesTitan <97427878+PersesTitan@users.noreply.github.com>
2022-10-25 15:11:42 +0900

    Create KlassMapTest.java

---
+++ src/test/java/klass/KlassMapTest.java
@@ -0,0 +1,10 @@
+package klass;
+
+import work.StartWork;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class KlassMapTest extends HashMap<String, StartWork> implements Map<String, StartWork> {
+
+}
