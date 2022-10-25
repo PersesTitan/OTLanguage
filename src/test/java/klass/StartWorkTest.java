4cbb67909bde5f25006daa5788c2f664e3e6571a
PersesTitan <torampersesTitan@gmail.com>
2022-10-26 00:35:17 +0900

    Update StartWorkTest.java

--- src/test/java/klass/StartWorkTest.java
+++ src/test/java/klass/StartWorkTest.java
@@ -1,8 +1,22 @@
 package klass;
 
+import bin.exception.MatchException;
+
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.stream.IntStream;
 
-public interface StartWorkTest {
-    void start(String line, String[] params, LinkedList<Map<String, Map<String, Object>>> repositoryArray);
+public abstract class StartWorkTest {
+    private final int[] counts;
+
+    protected StartWorkTest(int...counts) {
+        this.counts = counts.length == 0 ? null : counts;
+    }
+
+    abstract void start(String line, String[] params, LinkedList<Map<String, Map<String, Object>>> repositoryArray);
+    public StartWorkTest paramsCheck(int size) {
+        if (!(counts == null || Arrays.stream(counts).anyMatch(v -> v == size))) throw MatchException.grammarError();
+        return this;
+    }
 }
