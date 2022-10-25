e0f1a51ada7f57dfd7b5bb8223418fdaf1574a85
PersesTitan <torampersesTitan@gmail.com>
2022-10-26 00:35:22 +0900

    Update ReturnWorkTest.java

--- src/test/java/klass/ReturnWorkTest.java
+++ src/test/java/klass/ReturnWorkTest.java
@@ -1,7 +1,19 @@
 package klass;
 
+import java.util.Arrays;
 import java.util.Map;
+import java.util.stream.IntStream;
 
-public interface ReturnWorkTest {
-    String start(String line, String[] params, Map<String, Map<String, Object>>[] repositoryArray);
+public abstract class ReturnWorkTest {
+    private final IntStream counts;
+
+    protected ReturnWorkTest(int...counts) {
+        this.counts = IntStream.of(counts);
+    }
+
+    public abstract String start(String line, String[] params, Map<String, Map<String, Object>>[] repositoryArray);
+    public ReturnWorkTest paramsCheck(int size) {
+        counts.anyMatch(v -> v == size);
+        return this;
+    }
 }
