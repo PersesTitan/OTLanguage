453abafc484907e0393ebec3b984b6035f50898d
PersesTitan <97427878+PersesTitan@users.noreply.github.com>
2022-10-26 17:27:06 +0900

    Create ReturnWorkV3.java

---
+++ src/main/java/work/v3/ReturnWorkV3.java
@@ -0,0 +1,25 @@
+package work.v3;
+
+import bin.exception.MatchException;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Map;
+
+public abstract class ReturnWorkV3 implements Serializable {
+    private final int[] counts;
+
+    public ReturnWorkV3(int...counts) {
+        this.counts = counts.length == 0 ? null : counts;
+    }
+
+    public abstract String start(String line, String[] params, LinkedList<Map<String, Map<String, Object>>> repositoryArray);
+
+    public ReturnWorkV3 paramsCheck(int size, String params) {
+        if (!(counts == null
+                || (params != null && counts.length == 1 && counts[0] == 0 && params.isEmpty())
+                || Arrays.stream(counts).anyMatch(v -> v == size))) throw MatchException.grammarError();
+        return this;
+    }
+}
