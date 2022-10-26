149471b818340b7ad6a566bbc106e847954248fe
PersesTitan <97427878+PersesTitan@users.noreply.github.com>
2022-10-26 17:27:41 +0900

    Update ReturnWorkTest.java

--- src/test/java/klass/ReturnWorkTest.java
+++ src/test/java/klass/ReturnWorkTest.java
@@ -1,19 +1,26 @@
 package klass;
 
+import bin.exception.MatchException;
+
+import java.io.Serializable;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.stream.IntStream;
 
-public abstract class ReturnWorkTest {
-    private final IntStream counts;
+public abstract class ReturnWorkTest implements Serializable {
+    private final int[] counts;
 
     protected ReturnWorkTest(int...counts) {
-        this.counts = IntStream.of(counts);
+        this.counts = counts.length == 0 ? null : counts;
     }
 
-    public abstract String start(String line, String[] params, Map<String, Map<String, Object>>[] repositoryArray);
-    public ReturnWorkTest paramsCheck(int size) {
-        counts.anyMatch(v -> v == size);
+    public abstract String start(String line, String[] params, LinkedList<Map<String, Map<String, Object>>> repositoryArray);
+
+    public ReturnWorkTest paramsCheck(int size, String params) {
+        if (!(counts == null
+                || (params != null && counts.length == 1 && counts[0] == 0 && params.isEmpty())
+                || Arrays.stream(counts).anyMatch(v -> v == size))) throw MatchException.grammarError();
         return this;
     }
 }
