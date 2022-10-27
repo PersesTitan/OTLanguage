d0a28cc3a4980cbce958af3fcfe85f265cdc0baa
PersesTitan <torampersesTitan@gmail.com>
2022-10-27 09:50:53 +0900

    Update ReturnWork.java

--- src/main/java/work/ReturnWork.java
+++ src/main/java/work/ReturnWork.java
@@ -1,10 +1,11 @@
 package work;
 
 import java.io.Serializable;
+import java.util.LinkedList;
 import java.util.Map;
 
 public interface ReturnWork extends Serializable {
     boolean check(String line);
-    String start(String line, Map<String, Map<String, Object>>[] repositoryArray);
+    String start(String line, LinkedList<Map<String, Map<String, Object>>> repositoryArray);
     ReturnWork first();
 }
