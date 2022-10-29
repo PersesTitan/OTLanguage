c6a1954d59b3579159047e0cf91189983ad7767f
PersesTitan <torampersesTitan@gmail.com>
2022-10-30 03:01:51 +0900

    Create TryCatch.java

---
+++ src/main/java/bin/v3/sys/TryCatch.java
@@ -0,0 +1,27 @@
+package bin.v3.sys;
+
+import bin.token.MergeToken;
+import work.v3.StartWorkV3;
+
+import java.util.LinkedList;
+import java.util.Map;
+
+import static bin.apply.sys.make.StartLine.getFinalTotal;
+import static bin.apply.sys.make.StartLine.startStartLine;
+
+public class TryCatch extends StartWorkV3 implements MergeToken {
+    // 1
+    public TryCatch(int... counts) {
+        super(counts);
+    }
+
+    @Override
+    public void start(String line, String[] params,
+                      LinkedList<Map<String, Map<String, Object>>> repositoryArray) {
+        String[] loopTotals = getLoopTotal(params[0].strip()); // test, total
+        try {
+            String finalTotal = getFinalTotal(false, loopTotals[1], loopTotals[0]);
+            startStartLine(finalTotal, loopTotals[0], repositoryArray);
+        } catch (Exception ignored) {}
+    }
+}
