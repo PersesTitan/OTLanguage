c386475ea644287c4587ee27299395f13da5b42f
PersesTitan <torampersesTitan@gmail.com>
2022-10-30 22:29:18 +0900

    Create ForceQuit.java

---
+++ src/main/java/bin/apply/sys/run/ForceQuit.java
@@ -0,0 +1,18 @@
+package bin.apply.sys.run;
+
+import work.v3.StartWorkV3;
+
+import java.util.LinkedList;
+import java.util.Map;
+
+public class ForceQuit extends StartWorkV3 {
+    public ForceQuit(int... counts) {
+        super(counts);
+    }
+
+    @Override
+    public void start(String line, String[] params,
+                      LinkedList<Map<String, Map<String, Object>>> repositoryArray) {
+        System.exit(0);
+    }
+}
