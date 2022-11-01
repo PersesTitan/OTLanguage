009644a30beb9479f7f1b16a962959e8f52ec372
PersesTitan <torampersesTitan@gmail.com>
2022-11-02 01:30:54 +0900

    Update GetMouse.java

--- src/main/java/cos/mouse/controller/GetMouse.java
+++ src/main/java/cos/mouse/controller/GetMouse.java
@@ -1,44 +1,22 @@
 package cos.mouse.controller;
 
 import bin.token.LoopToken;
-import work.ReturnWork;
+import work.v3.ReturnWorkV3;
 
 import java.awt.*;
 import java.util.LinkedList;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-public class GetMouse implements ReturnWork, LoopToken {
-    private final String group;
-    private final Pattern pattern;
-    private Matcher matcher;
-
-    public GetMouse(String className, String type) {
-        this.group = Pattern.quote(className + ACCESS + type);
-        String patternText = startEndMerge(VARIABLE_GET_S, className + ACCESS + type, VARIABLE_GET_E);
-        this.pattern = Pattern.compile(patternText);
+public class GetMouse extends ReturnWorkV3 implements LoopToken {
+    // 0
+    public GetMouse(int... counts) {
+        super(counts);
     }
 
     @Override
-    public boolean check(String line) {
-        this.matcher = pattern.matcher(line);
-        return matcher.find();
-    }
-
-    @Override
-    public String start(String line, LinkedList<Map<String, Map<String, Object>>> repositoryArray) {
-        matcher.reset();
-        while (matcher.find()) {
-            Point local = MouseInfo.getPointerInfo().getLocation();
-            String newWord = String.format("[%d, %d]", local.x, local.y);
-            line = line.replaceFirst(group, newWord);
-        }
-        return line;
-    }
-
-    @Override
-    public ReturnWork first() {
-        return this;
+    public String start(String line, String[] params,
+                        LinkedList<Map<String, Map<String, Object>>> repositoryArray) {
+        Point local = MouseInfo.getPointerInfo().getLocation();
+        return String.format("[%d, %d]", local.x, local.y);
     }
 }
