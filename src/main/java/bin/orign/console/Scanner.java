4ee41396a125277bdfd1616e06f0ab33ec8a69f1
PersesTitan <torampersesTitan@gmail.com>
2022-11-02 07:49:49 +0900

    Create Scanner.java

---
+++ src/main/java/bin/orign/console/Scanner.java
@@ -0,0 +1,20 @@
+package bin.orign.console;
+
+import work.v3.ReturnWorkV3;
+
+import java.util.LinkedList;
+import java.util.Map;
+
+import static bin.apply.Controller.scanner;
+
+public class Scanner extends ReturnWorkV3 {
+    public Scanner(int... counts) {
+        super(counts);
+    }
+
+    @Override
+    public String start(String line, String[] params,
+                        LinkedList<Map<String, Map<String, Object>>> repositoryArray) {
+        return scanner();
+    }
+}
