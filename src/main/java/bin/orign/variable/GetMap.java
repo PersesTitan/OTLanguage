c554898bde9cdb7fccbc0a58e6e708312df876d5
PersesTitan <torampersesTitan@gmail.com>
2022-11-01 20:21:13 +0900

    Create GetMap.java

---
+++ src/main/java/bin/orign/variable/GetMap.java
@@ -0,0 +1,108 @@
+package bin.orign.variable;
+
+import bin.exception.VariableException;
+
+import java.util.*;
+
+import static bin.check.VariableCheck.*;
+import static bin.token.Token.COMMA;
+
+public interface GetMap {
+    default LinkedHashMap<String, String> setBoolMap(LinkedHashMap<String, String> map, String line) {
+        if (!isMapBoolean(line)) throw VariableException.typeMatch();
+        else {
+            StringTokenizer t = new StringTokenizer(line.substring(1, line.length()-1), COMMA);
+            while (t.hasMoreTokens()) {
+                String[] tokens = t.nextToken()
+                        .strip()
+                        .split("=", 2);
+                map.put(tokens[0], tokens[1]);
+            }
+        }
+        return map;
+    }
+
+    default LinkedHashMap<String, Character> setCharacterMap(LinkedHashMap<String, Character> map, String line) {
+        if (!isMapCharacter(line)) throw VariableException.typeMatch();
+        else {
+            StringTokenizer t = new StringTokenizer(line.substring(1, line.length()-1), COMMA);
+            while (t.hasMoreTokens()) {
+                String[] tokens = t.nextToken()
+                        .strip()
+                        .split("=", 2);
+                map.put(tokens[0], tokens[1].charAt(0));
+            }
+        }
+        return map;
+    }
+
+    default LinkedHashMap<String, Double> setDoubleMap(LinkedHashMap<String, Double> map, String line) {
+        if (!isMapDouble(line)) throw VariableException.typeMatch();
+        else {
+            StringTokenizer t = new StringTokenizer(line.substring(1, line.length()-1), COMMA);
+            while (t.hasMoreTokens()) {
+                String[] tokens = t.nextToken()
+                        .strip()
+                        .split("=", 2);
+                map.put(tokens[0], Double.parseDouble(tokens[1]));
+            }
+        }
+        return map;
+    }
+
+    default LinkedHashMap<String, Float> setFlotMap(LinkedHashMap<String, Float> map, String line) {
+        if (!isMapFloat(line)) throw VariableException.typeMatch();
+        else {
+            StringTokenizer t = new StringTokenizer(line.substring(1, line.length()-1), COMMA);
+            while (t.hasMoreTokens()) {
+                String[] tokens = t.nextToken()
+                        .strip()
+                        .split("=", 2);
+                map.put(tokens[0], Float.parseFloat(tokens[1]));
+            }
+        }
+        return map;
+    }
+
+    default LinkedHashMap<String, Integer> setIntegerMap(LinkedHashMap<String, Integer> map, String line) {
+        if (!isMapInteger(line)) throw VariableException.typeMatch();
+        else {
+            StringTokenizer t = new StringTokenizer(line.substring(1, line.length()-1), COMMA);
+            while (t.hasMoreTokens()) {
+                String[] tokens = t.nextToken()
+                        .strip()
+                        .split("=", 2);
+                map.put(tokens[0], Integer.parseInt(tokens[1]));
+            }
+        }
+        return map;
+    }
+
+    default LinkedHashMap<String, Long> setLongMap(LinkedHashMap<String, Long> map, String line) {
+        if (!isMapLong(line)) throw VariableException.typeMatch();
+        else {
+            StringTokenizer t = new StringTokenizer(line.substring(1, line.length()-1), COMMA);
+            while (t.hasMoreTokens()) {
+                String[] tokens = t.nextToken()
+                        .strip()
+                        .split("=", 2);
+                map.put(tokens[0], Long.parseLong(tokens[1]));
+            }
+        }
+        return map;
+    }
+
+    default LinkedHashMap<String, String> setStringMap(LinkedHashMap<String, String> map, String line) {
+        if (!isMapString(line)) throw VariableException.typeMatch();
+        else {
+            StringTokenizer t = new StringTokenizer(line.substring(1, line.length()-1), COMMA);
+            while (t.hasMoreTokens()) {
+                String[] tokens = t.nextToken()
+                        .strip()
+                        .split("=", 2);
+                map.put(tokens[0], tokens[1]);
+            }
+        }
+        return map;
+    }
+}
