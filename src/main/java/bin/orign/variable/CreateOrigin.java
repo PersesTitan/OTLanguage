a6411a202ef777e6ee5e4f5ccaf19be72549e7c9
PersesTitan <torampersesTitan@gmail.com>
2022-11-01 20:21:27 +0900

    Update CreateOrigin.java

--- src/main/java/bin/orign/variable/CreateOrigin.java
+++ src/main/java/bin/orign/variable/CreateOrigin.java
@@ -1,33 +1,25 @@
 package bin.orign.variable;
 
-import bin.apply.Setting;
-import bin.token.LoopToken;
 import bin.token.MergeToken;
-import work.StartWork;
+import work.v3.StartWorkV3;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Map;
 
-public class CreateOrigin implements StartWork, LoopToken {
+import static bin.token.Token.BLANKS;
+import static bin.token.VariableToken.VARIABLE_PUT;
 
-    @Override
-    public boolean check(String line) {
-        return false;
+public class CreateOrigin extends StartWorkV3 implements MergeToken {
+    public CreateOrigin(int... counts) {
+        super(counts);
     }
 
     @Override
-    public void start(String line, String origen,
+    public void start(String line, String[] params,
                       LinkedList<Map<String, Map<String, Object>>> repositoryArray) {
-        // 변수명:1234  => 변수명, 1234
         String[] values = matchSplitError(line, BLANKS, 2);
         String[] tokens = values[1].split(VARIABLE_PUT, 2);
         variableDefineError(tokens[0], repositoryArray.get(0));
         repositoryArray.get(0).get(values[0]).put(tokens[0], tokens.length == 2 ? tokens[1] : "");
-    }
-
-    @Override
-    public void first() {
-
     }
 }
