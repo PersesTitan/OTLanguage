83965336789609a369ebe189921aeb8613db33ab
PersesTitan <torampersesTitan@gmail.com>
2022-11-02 01:31:28 +0900

    Create RandomDouble.java

---
+++ src/main/java/bin/orign/math/random/RandomDouble.java
@@ -0,0 +1,35 @@
+package bin.orign.math.random;
+
+import work.v3.ReturnWorkV3;
+
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Random;
+
+import static bin.check.VariableCheck.isDouble;
+
+public class RandomDouble extends ReturnWorkV3 {
+    public RandomDouble(int... counts) {
+        super(counts);
+    }
+
+    @Override
+    public String start(String line, String[] params,
+                        LinkedList<Map<String, Map<String, Object>>> repositoryArray) {
+        Random random = new Random();
+        if (params.length == 1) {
+            if (params[0].isBlank()) return Double.toString(random.nextDouble());
+            else {
+                if (!isDouble(params[0])) return null;
+                double num = Double.parseDouble(params[0]);
+                return Double.toString(random.nextDouble(num));
+            }
+        } else {
+            if (!isDouble(params[0])) return null;
+            else if (!isDouble(params[1])) return null;
+            double num1 = Double.parseDouble(params[0]);
+            double num2 = Double.parseDouble(params[1]);
+            return Double.toString(random.nextDouble(num1, num2));
+        }
+    }
+}
