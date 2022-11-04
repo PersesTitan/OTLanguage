740fc354a4ced61d23c78456ae93f3121b00ce02
PersesTitan <torampersesTitan@gmail.com>
2022-11-05 01:10:26 +0900

    Update FilePath.java

--- src/main/java/bin/apply/sys/run/FilePath.java
+++ src/main/java/bin/apply/sys/run/FilePath.java
@@ -6,6 +6,7 @@ import bin.apply.sys.make.StartLine;
 import bin.exception.FileException;
 import bin.token.LoopToken;
 import work.StartWork;
+import work.v3.StartWorkV3;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
@@ -20,29 +21,19 @@ import static bin.apply.sys.item.Separator.SEPARATOR_LINE;
 import static bin.apply.sys.item.SystemSetting.extension;
 import static bin.apply.sys.item.SystemSetting.extensionCheck;
 import static bin.apply.sys.make.StartLine.errorPath;
+import static bin.token.Token.ACCESS;
 
-public class FilePath implements LoopToken, StartWork {
-    private final Matcher matcher;
-
-    public FilePath() {
-        String patternText = startEndMerge(FILE, BLANKS, FILE_TYPE, "(", ACCESS, FILE_TYPE, ")*");
-        this.matcher = Pattern.compile(patternText).matcher("");
-    }
-
-    public boolean check(String line) {
-        return matcher.reset(line).find();
+public class FilePath extends StartWorkV3 {
+    // 1
+    public FilePath(int... counts) {
+        super(counts);
     }
 
     @Override
-    public void start(String line, String origen,
+    public void start(String line, String[] params,
                       LinkedList<Map<String, Map<String, Object>>> repositoryArray) {
-        StringTokenizer tokenizer = new StringTokenizer(line.strip());
-        tokenizer.nextToken();                  // ㅍㅅㅍ
         // 디렉토리~파일명
-        String path = Setting.path + SEPARATOR_FILE + tokenizer
-                        .nextToken()
-                        .replace(ACCESS, SEPARATOR_FILE);
-
+        String path = Setting.path + SEPARATOR_FILE + params[0].replace(ACCESS, SEPARATOR_FILE);
         for (String ext : extension) {
             File file = new File(path + ext);
             if (file.isFile()) {
@@ -50,27 +41,22 @@ public class FilePath implements LoopToken, StartWork {
                 return;
             }
         }
-        throw FileException.pathNoHaveError();
-    }
-
-    @Override
-    public void first() {
-
+        throw new FileException().pathNoHaveError();
     }
 
     private void importFile(File file, LinkedList<Map<String, Map<String, Object>>> repositoryArray) {
-        if (!file.exists()) throw FileException.pathNoHaveError();
-        else if (!file.isFile()) throw FileException.isNotFileError();
-        else if (!file.canRead()) throw FileException.noReadError();
-        else if (!extensionCheck(file.getName())) throw FileException.rightExtension();
+        if (!file.exists()) throw new FileException().pathNoHaveError();
+        else if (!file.isFile()) throw new FileException().isNotFileError();
+        else if (!file.canRead()) throw new FileException().noReadError();
+        else if (!extensionCheck(file.getName())) throw new FileException().rightExtension();
 
         StringBuilder builder = new StringBuilder();
         try (FileReader fileReader = new FileReader(file, StandardCharsets.UTF_8);
-            BufferedReader reader = new BufferedReader(fileReader)) {
+             BufferedReader reader = new BufferedReader(fileReader)) {
             for (int i=1;;i++) {
                 String line = reader.readLine();
                 if (line == null) break;
-                builder.append(i).append(" ").append(line).append(SEPARATOR_LINE);
+                builder.append(i).append(" ").append(line.stripLeading()).append(SEPARATOR_LINE);
             }
         } catch (IOException ignored) {}
         String path = errorPath.get();
