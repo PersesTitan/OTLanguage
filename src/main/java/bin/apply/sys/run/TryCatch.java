71ce1c015dc99e73f0af1090218abcb71455e3d5
PersesTitan <torampersesTitan@gmail.com>
2022-11-05 01:10:21 +0900

    Update TryCatch.java

--- src/main/java/bin/apply/sys/run/TryCatch.java
+++ src/main/java/bin/apply/sys/run/TryCatch.java
@@ -1,50 +1,27 @@
 package bin.apply.sys.run;
 
-import bin.apply.sys.make.StartLine;
-import bin.exception.MatchException;
-import bin.token.LoopToken;
-import work.StartWork;
+import bin.token.MergeToken;
+import work.v3.StartWorkV3;
 
 import java.util.LinkedList;
 import java.util.Map;
-import java.util.StringTokenizer;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import static bin.apply.sys.make.StartLine.getFinalTotal;
 import static bin.apply.sys.make.StartLine.startStartLine;
 
-public class TryCatch implements StartWork, LoopToken {
-    private final Matcher matcher;
-
-    public TryCatch(String type) {
-        this.matcher = Pattern
-                .compile(startEndMerge(type, BLANKS, BRACE_STYLE))
-                .matcher("");
+public class TryCatch extends StartWorkV3 implements MergeToken {
+    // 1
+    public TryCatch(int... counts) {
+        super(counts);
     }
 
     @Override
-    public boolean check(String line) {
-        return matcher.reset(line).find();
-    }
-
-    @Override
-    public void start(String line, String origen,
+    public void start(String line, String[] params,
                       LinkedList<Map<String, Map<String, Object>>> repositoryArray) {
-        // ㅠㅅㅠ (test,1,10)
-        StringTokenizer tokenizer = new StringTokenizer(line);
-        tokenizer.nextToken();
-        String tokens = tokenizer.nextToken();
-        if (tokens == null) throw MatchException.grammarError();
-        String[] loopTotals = getLoopTotal(tokens); // test, total
+        String[] loopTotals = getLoopTotal(params[0].strip()); // test, total
         try {
             String finalTotal = getFinalTotal(false, loopTotals[1], loopTotals[0]);
             startStartLine(finalTotal, loopTotals[0], repositoryArray);
         } catch (Exception ignored) {}
-    }
-
-    @Override
-    public void first() {
-
     }
 }
