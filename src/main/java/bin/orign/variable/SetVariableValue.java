12674e33e4162506176463d0ef82d4e54688e8ed
PersesTitan <torampersesTitan@gmail.com>
2022-11-09 00:12:25 +0900

    Update SetVariableValue.java

--- src/main/java/bin/orign/variable/SetVariableValue.java
+++ src/main/java/bin/orign/variable/SetVariableValue.java
@@ -10,7 +10,7 @@ import static bin.apply.Controller.getSetVariable;
 public interface SetVariableValue extends LoopToken {
     default void set(String variableType, String variableName, String value,
                        Map<String, Map<String, Object>> repository) {
-        if (!getSetVariable.getType(variableType, value)) throw VariableException.typeMatch();
+        if (!getSetVariable.getType(variableType, value)) throw new VariableException().typeMatch();
         if (ORIGIN_LIST.contains(variableType)) repository.get(variableType).put(variableName, value);
         else if (SET_LIST.contains(variableType)) {
             Object ob = repository.get(variableType).get(variableName);
