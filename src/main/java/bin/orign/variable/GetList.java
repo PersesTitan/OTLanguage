ebe684b94678368bf4e9f87d4ca40f89346f8636
PersesTitan <torampersesTitan@gmail.com>
2022-11-09 00:12:50 +0900

    Update GetList.java

--- src/main/java/bin/orign/variable/GetList.java
+++ src/main/java/bin/orign/variable/GetList.java
@@ -16,7 +16,7 @@ public interface GetList {
 
     default LinkedList<String> setBoolList(LinkedList<String> list, String line) {
         if (listCheck(line) && isBoolean(line)) list.add(line);
-        else if (isListBoolean(line)) throw VariableException.typeMatch();
+        else if (isListBoolean(line)) throw new VariableException().typeMatch();
         else {
             StringTokenizer t = new StringTokenizer(line.substring(1, line.length()-1), SING);
             while (t.hasMoreTokens()) list.add(t.nextToken());
@@ -26,7 +26,7 @@ public interface GetList {
 
     default LinkedList<Character> setCharacterList(LinkedList<Character> set, String line) {
         if (listCheck(line) && isCharacter(line)) set.add(line.charAt(0));
-        else if (isListCharacter(line)) throw VariableException.typeMatch();
+        else if (isListCharacter(line)) throw new VariableException().typeMatch();
         else {
             StringTokenizer t = new StringTokenizer(line.substring(1, line.length()-1), SING);
             while (t.hasMoreTokens()) set.add(t.nextToken().charAt(0));
@@ -36,7 +36,7 @@ public interface GetList {
 
     default LinkedList<Double> setDoubleList(LinkedList<Double> set, String line) {
         if (listCheck(line) && isDouble(line)) set.add(Double.parseDouble(line));
-        else if (isListDouble(line)) throw VariableException.typeMatch();
+        else if (isListDouble(line)) throw new VariableException().typeMatch();
         else {
             StringTokenizer t = new StringTokenizer(line.substring(1, line.length()-1), SING);
             while (t.hasMoreTokens()) set.add(Double.parseDouble(t.nextToken()));
@@ -46,7 +46,7 @@ public interface GetList {
 
     default LinkedList<Float> setFlotList(LinkedList<Float> set, String line) {
         if (listCheck(line) && isFloat(line)) set.add(Float.parseFloat(line));
-        else if (isListFloat(line)) throw VariableException.typeMatch();
+        else if (isListFloat(line)) throw new VariableException().typeMatch();
         else {
             StringTokenizer t = new StringTokenizer(line.substring(1, line.length()-1), SING);
             while (t.hasMoreTokens()) set.add(Float.parseFloat(t.nextToken()));
@@ -56,7 +56,7 @@ public interface GetList {
 
     default LinkedList<Integer> setIntegerList(LinkedList<Integer> set, String line) {
         if (listCheck(line) && isInteger(line)) set.add(Integer.parseInt(line));
-        else if (isListInteger(line)) throw VariableException.typeMatch();
+        else if (isListInteger(line)) throw new VariableException().typeMatch();
         else {
             StringTokenizer t = new StringTokenizer(line.substring(1, line.length()-1), SING);
             while (t.hasMoreTokens()) set.add(Integer.parseInt(t.nextToken()));
@@ -66,7 +66,7 @@ public interface GetList {
 
     default LinkedList<Long> setLongList(LinkedList<Long> set, String line) {
         if (listCheck(line) && isLong(line)) set.add(Long.parseLong(line));
-        else if (isListLong(line)) throw VariableException.typeMatch();
+        else if (isListLong(line)) throw new VariableException().typeMatch();
         else {
             StringTokenizer t = new StringTokenizer(line.substring(1, line.length()-1), SING);
             while (t.hasMoreTokens()) set.add(Long.parseLong(t.nextToken()));
@@ -76,7 +76,7 @@ public interface GetList {
 
     default LinkedList<String> setStringList(LinkedList<String> set, String line) {
         if (listCheck(line)) set.add(line);
-        else if (!isListString(line)) throw VariableException.typeMatch();
+        else if (!isListString(line)) throw new VariableException().typeMatch();
         else {
             StringTokenizer t = new StringTokenizer(line.substring(1, line.length()-1), COMMA);
             while (t.hasMoreTokens()) set.add(t.nextToken().strip());
