ab8348875878dbac09586b6b9969273055829dab
PersesTitan <torampersesTitan@gmail.com>
2022-11-10 00:40:35 +0900

    Create PrintSpace.java

---
+++ src/main/java/bin/orign/console/normal/PrintSpace.java
@@ -0,0 +1,18 @@
+package bin.orign.console.normal;
+
+import work.v3.StartWorkV3;
+
+import java.util.LinkedList;
+import java.util.Map;
+
+public class PrintSpace extends StartWorkV3 {
+    public PrintSpace(int... counts) {
+        super(counts);
+    }
+
+    @Override
+    public void start(String line, String[] params,
+                      LinkedList<Map<String, Map<String, Object>>> repositoryArray) {
+        System.out.print(params[0] + " ");
+    }
+}
