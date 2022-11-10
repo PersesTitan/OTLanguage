d446b78240c298af3fda8bf59655b15b01e3ca54
PersesTitan <torampersesTitan@gmail.com>
2022-11-11 00:05:55 +0900

    Update BoolV3Test.java

--- src/test/java/etc/calculator/bool/BoolV3Test.java
+++ src/test/java/etc/calculator/bool/BoolV3Test.java
@@ -1,54 +1,45 @@
 package etc.calculator.bool;
 
-import bin.CreateReturnWorks;
 import bin.exception.MatchException;
-import etc.calculator.token.CalToken;
 
-import java.util.*;
-import java.util.function.BiFunction;
+import java.util.Collections;
+import java.util.Stack;
 
-import static bin.token.Token.NO_TOKEN;
-import static bin.token.Token.TOKEN;
 import static bin.token.cal.BoolToken.*;
 
-public class BoolV3Test implements CalToken {
-    public static void main(String[] args) {
-
-    }
-
-    private final Stack<String> stack = new Stack<>();
-    public Stack<String> start(String line, LinkedList<Map<String, Map<String, Object>>> ra) {
-        stack.clear();
-        StringTokenizer tokenizer = new StringTokenizer(line, "ㅇㄸㄲㄴ", true);
-        while (tokenizer.hasMoreTokens()) {
-            String token = tokenizer.nextToken();
-            if (token.isBlank()) continue;
-            if (token.equals(TOKEN)) {
-                String a = tokenizer.nextToken();
-                if (a.equals(TOKEN)) stack.add(TRUE);
-                else if (a.equals(NO_TOKEN)) stack.add(NOT);
-                else {
-                    if (tokenizer.nextToken().equals(TOKEN)) stack.add(a);
-                    else throw new MatchException().grammarTypeError();
-                }
-            } else if (token.equals(NO_TOKEN)) {
-                if (tokenizer.nextToken().equals(NO_TOKEN)) stack.add(FALSE);
-                else throw new MatchException().grammarTypeError();
-            } else stack.add(token.strip());
+public class BoolV3Test {
+    public boolean start(Stack<String> stack) {
+        Collections.reverse(stack);
+        boolean bool1 = getBoolean(stack);
+        while (!stack.isEmpty()) {
+            bool1 = changeBoolean(
+                    bool1,
+                    stack.pop(),
+                    getBoolean(stack));
         }
-
-        compare.keySet().forEach(v -> search(stack, v, ra));
-        return stack;
+        return bool1;
     }
 
-    private void search(Stack<String> stack, String token, LinkedList<Map<String, Map<String, Object>>> ra) {
-        int i;
-        while ((i = stack.indexOf(token)) != -1) {
-            double d1 = getDouble(stack.get(i-1), ra);
-            double d2 = getDouble(stack.get(i+1), ra);
-            stack.set(i ,  compare.get(stack.get(i)).apply(d1, d2));
-            stack.remove(i+1);
-            stack.remove(i-1);
+    private boolean changeBoolean(boolean bool1, String sing, boolean bool2) {
+        return switch (sing) {
+            case AND -> bool1 && bool2;
+            case OR -> bool1 || bool2;
+            default -> throw new MatchException().grammarTypeError();
+        };
+    }
+
+    private boolean getBoolean(Stack<String> stack) {
+        String line = stack.pop();
+        if (line.equals(NOT)) {
+            line = stack.pop();
+            if (line.equals(TRUE)) return false;
+            else if (line.equals(FALSE)) return true;
+            else throw new MatchException().grammarTypeError();
+        } else {
+            if (line.equals(TRUE)) return true;
+            else if (line.equals(FALSE)) return false;
+            else throw new MatchException().grammarTypeError();
         }
     }
+
 }
