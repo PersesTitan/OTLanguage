0c19b895a1f5be180c69eec87381c160b05ab307
PersesTitan <torampersesTitan@gmail.com>
2022-11-12 12:20:13 +0900

    Update MainTest.java

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,7 +1,9 @@
+import bin.apply.Repository;
 import bin.apply.Setting;
 import bin.apply.sys.make.ChangeHangle;
 import bin.exception.FileException;
 import bin.token.LoopToken;
+import etc.loop.WhileTest;
 import module.download.MakeGitTest;
 
 import javax.swing.*;
@@ -12,6 +14,7 @@ import java.util.regex.Pattern;
 
 import static bin.apply.Controller.br;
 import static bin.apply.Controller.bw;
+import static bin.apply.Repository.startWorksV3;
 import static bin.apply.sys.item.Separator.SEPARATOR_HOME;
 
 public class MainTest implements LoopToken, ChangeHangle {
@@ -23,8 +26,14 @@ public class MainTest implements LoopToken, ChangeHangle {
 //        JFrame frame = new JFrame("test");
 
 //        new MakeGitTest();
+
+        new Setting();
+        Repository.createStartWorks(WHITE.replace("\\", ""), "", new WhileTest(1));
         try {
             new Main(new String[]{SEPARATOR_HOME, "hello.otl"});
+//            new Main(new String[]{SEPARATOR_HOME, "test/set_test.otl"});
+//            new Main(new String[]{SEPARATOR_HOME, "test/list_test.otl"});
+//            new Main(new String[]{SEPARATOR_HOME, "test/map_test.otl"});
         } catch (FileException e) {
             new FileException().printErrorMessage(e, Setting.mainPath);
         } finally {try {br.close(); bw.close();} catch (IOException ignored) {}}
