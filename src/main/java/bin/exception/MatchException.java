5f701eb1b194ef2694f6512c4856eca46581368d
PersesTitan <torampersesTitan@gmail.com>
2022-11-13 11:10:03 +0900

    Update MatchException.java

--- src/main/java/bin/exception/MatchException.java
+++ src/main/java/bin/exception/MatchException.java
@@ -1,6 +1,11 @@
 package bin.exception;
 
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.regex.Pattern;
+
+import static bin.calculator.tool.CalculatorTool.compare;
+import static bin.token.LoopToken.ELSE_IF;
+import static bin.token.cal.BoolToken.*;
 
 public class MatchException extends RuntimeException {
     private final String grammarError = "문법이 일치하지 않습니다.";
@@ -11,6 +16,8 @@ public class MatchException extends RuntimeException {
     private final String dateFormatError = "Date Format 형식에 일치하지 않습니다.";
     private final String timeFormatError = "Time Format 형식에 일치하지 않습니다.";
     private final String grammarTypeError = "타입 또는 문법이 유효하지 않습니다.";
+    private String grammarTypeToken = null;
+    private GrammarTypeClass grammarTypeClass;
 
     public MatchException() {}
 
@@ -25,9 +32,8 @@ public class MatchException extends RuntimeException {
             case loopStyleError -> "The grammar must have '{' at the end.\nPlease check the grammar again.";
             case dateFormatError -> "Date Format does not match the format.\nPlease check the format.";
             case timeFormatError -> "Time Format does not match the format.\nPlease check the format.";
-            case grammarTypeError -> "The type or grammar is invalid.\nPlease check the price.";
-            default -> (e.getMessage().equals(patternMatchError2)
-                    ? ""
+            case grammarTypeError -> grammarTypeErrorMessage(grammarTypeClass);
+            default -> (e.getMessage().equals(patternMatchError2) ? ""
                     : "Error Pattern (" + e.getMessage().replaceFirst(Pattern.quote(patternMatchError1.substring("%s".length())), "") + ")\n")
                         + "Hint: Cannot use special characters (url에서는 특수문자는 사용할 수 없습니다.)\n"
                         + "It cannot be used because it already exists or does not match the rule.\n"
@@ -43,8 +49,33 @@ public class MatchException extends RuntimeException {
                 : String.format(patternMatchError1, error));
     }
 
-    public MatchException grammarTypeError() {
+    public MatchException grammarTypeError(String token, GrammarTypeClass grammarTypeClass) {
+        this.grammarTypeToken = token;
+        this.grammarTypeClass = grammarTypeClass;
         return new MatchException(grammarTypeError);
+    }
+
+    private String grammarTypeErrorMessage(GrammarTypeClass grammarTypeClass) {
+        String message = "The type or grammar is invalid.\nPlease check the price.";
+        if (grammarTypeToken != null) {
+            message = message
+                    .concat("\nDoesn't match (" + grammarTypeToken + ")\n")
+                    .concat(switch (grammarTypeClass) {
+                        case OR_AND -> String.format("Does not match token (%s, %s).", OR, AND);
+                        case TRUE_FALSE -> String.format("Does not match token (%s, %s).", TRUE, FALSE);
+                        case COMPARE -> String.format("Does not match token (%s).", String.join(", ", compare.keySet()));
+                        case NO_ELSE_IF -> String.format("Does not match token (%s).", ELSE_IF);
+                        case TOKEN -> "Incomprehensible token";
+                        case NUMBER -> "It's not a number.";
+                        case VALID -> "The form is not valid.";
+                    });
+            grammarTypeToken = null;
+        }
+        return message;
+    }
+
+    public enum GrammarTypeClass {
+        OR_AND, NUMBER, TRUE_FALSE, COMPARE, TOKEN, VALID, NO_ELSE_IF
     }
 
     public MatchException dateFormatError() {
