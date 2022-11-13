a17e7e6f95ada073bcead511d25ccf73cf55a7d1
PersesTitan <torampersesTitan@gmail.com>
2022-11-14 00:31:29 +0900

    Update MainTest.java

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -6,6 +6,7 @@ import bin.exception.FileException;
 import bin.token.LoopToken;
 import etc.loop.IfTest;
 import etc.loop.WhileTest;
+import etc.method.MethodTest;
 import module.download.MakeGitTest;
 
 import java.awt.*;
@@ -13,6 +14,8 @@ import java.io.IOException;
 
 import static bin.apply.Controller.br;
 import static bin.apply.Controller.bw;
+import static bin.apply.Repository.repository;
+import static bin.apply.Repository.startWorksV3;
 import static bin.apply.sys.item.Separator.SEPARATOR_HOME;
 
 public class MainTest implements LoopToken, ChangeHangle {
@@ -23,10 +26,13 @@ public class MainTest implements LoopToken, ChangeHangle {
 //        Robot robot = new Robot();
 //        JFrame frame = new JFrame("test");
 
-        new MakeGitTest();
+//        new MakeGitTest();
+        Repository.createStartWorks(METHOD, "", new MethodTest(1));
+        new Setting();
         try {
             StartLine.developmentMode = true;
             new Main(new String[]{SEPARATOR_HOME, "hello.otl"});
+
 //            new Main(new String[]{SEPARATOR_HOME, "test/set_test.otl"});
 //            new Main(new String[]{SEPARATOR_HOME, "test/list_test.otl"});
 //            new Main(new String[]{SEPARATOR_HOME, "test/map_test.otl"});
