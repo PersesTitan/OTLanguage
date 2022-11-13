708ab627fcf5d7b2f15905cb4dc68780b25c50a4
PersesTitan <torampersesTitan@gmail.com>
2022-11-13 11:09:19 +0900

    Update BlankV3Test.java

--- src/test/java/etc/calculator/token/BlankV3Test.java
+++ src/test/java/etc/calculator/token/BlankV3Test.java
@@ -1,6 +1,7 @@
 package etc.calculator.token;
 
 import bin.exception.MatchException;
+import bin.exception.MatchException.GrammarTypeClass;
 import etc.calculator.bool.BoolV3Test;
 import etc.calculator.bool.CompareV3Test;
 
@@ -8,6 +9,7 @@ import java.util.LinkedList;
 import java.util.Map;
 import java.util.Stack;
 
+import static bin.exception.MatchException.GrammarTypeClass.*;
 import static bin.token.cal.BoolToken.FALSE;
 import static bin.token.cal.BoolToken.TRUE;
 
@@ -22,11 +24,6 @@ public interface BlankV3Test {
             }
         }
         return builder.toString();
-    }
-
-    // line : 123 ㅇ+ㅇ 343
-    private String calculator(String line) {
-        return line;
     }
 
     BoolV3Test bool = new BoolV3Test();
@@ -56,12 +53,12 @@ public interface BlankV3Test {
             while ((start = builder.lastIndexOf("(")) != -1) {
                 int end = builder.indexOf(")", start) + 1;
                 Stack<String> stack = number.start(builder.substring(start+1, end-1), ra);
-                if (stack.size() != 1) throw new MatchException().grammarTypeError();
+                if (stack.size() != 1) throw new MatchException().grammarTypeError(stack.toString(), VALID);
                 builder.replace(start, end, stack.pop());
             }
         }
         Stack<String> stack = number.start(builder.toString(), ra);
-        if (stack.size() != 1) throw new MatchException().grammarTypeError();
+        if (stack.size() != 1) throw new MatchException().grammarTypeError(stack.toString(), VALID);
         return stack.pop();
     }
 }
