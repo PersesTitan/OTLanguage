94dc253699ad6786cb268f74c494e74424002cb6
PersesTitan <torampersesTitan@gmail.com>
2022-11-15 08:25:11 +0900

    Create MethodStart.java

---
+++ src/test/java/etc/v3/method/start/MethodStart.java
@@ -0,0 +1,33 @@
+package etc.v3.method.start;
+
+import bin.define.item.MethodItemVoid;
+import bin.exception.VariableException;
+import work.v3.StartWorkV3;
+
+import java.util.LinkedList;
+import java.util.Map;
+
+import static bin.token.LoopToken.METHOD;
+
+public class MethodStart extends StartWorkV3 {
+    public MethodStart(int... counts) {
+        super(counts);
+    }
+
+    @Override
+    public void start(String line, String[] params,
+                      LinkedList<Map<String, Map<String, Object>>> repositoryArray) {
+        int position = line.indexOf('[');
+        String variableType = line.substring(0, position);
+        Object startItem = repositoryArray.get(0).get(METHOD).get(variableType);
+
+        if (startItem == null) throw new VariableException().noDefineMethod();
+        else if (startItem instanceof MethodItemVoid methodItemTest) {
+            if (methodItemTest.getParams() == 0) {
+                if (!(params.length == 1 && params[0].isEmpty())) throw new VariableException().methodParamsError();
+            } else if (methodItemTest.getParams() != params.length)
+                throw new VariableException().methodParamsError();
+            methodItemTest.start(params, repositoryArray);
+        } else throw new VariableException().methodTypeMatch();
+    }
+}
