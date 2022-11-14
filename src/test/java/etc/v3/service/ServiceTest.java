9555f4075e225a8975ea1d2569f05843b2b06bc7
PersesTitan <torampersesTitan@gmail.com>
2022-11-15 08:22:27 +0900

    Create ServiceTest.java

---
+++ src/test/java/etc/v3/service/ServiceTest.java
@@ -0,0 +1,18 @@
+package etc.v3.service;
+
+import bin.apply.Setting;
+import work.StartWork;
+import work.v3.StartWorkV3;
+
+import java.util.ServiceLoader;
+
+public class ServiceTest {
+    public static void main(String[] args) {
+        ServiceLoader<StartWorkV3> test = ServiceLoader.load(StartWorkV3.class);
+        ServiceLoader<StartWork> servce = ServiceLoader.load(StartWork.class);
+        System.out.println(servce);
+
+        test.forEach(System.out::println);
+        servce.forEach(System.out::println);
+    }
+}
