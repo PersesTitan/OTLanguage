01ecfea065dbd8c422bc5d5a96b3fd48c11813ec
PersesTitan <torampersesTitan@gmail.com>
2022-11-16 01:15:06 +0900

    Create ReturnListTest.java

---
+++ src/test/java/etc/v3/list/ReturnListTest.java
@@ -0,0 +1,11 @@
+package etc.v3.list;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReturnListTest {
+    public static void main(String[] args) {
+
+    }
+}
