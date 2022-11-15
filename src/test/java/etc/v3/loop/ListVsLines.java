62f8ddb29fe06c23de970f7b0620914d011b5898
PersesTitan <torampersesTitan@gmail.com>
2022-11-16 01:14:44 +0900

    Create ListVsLines.java

---
+++ src/test/java/etc/v3/loop/ListVsLines.java
@@ -0,0 +1,45 @@
+package etc.v3.loop;
+
+import java.util.regex.Matcher;
+
+import static bin.apply.sys.item.Separator.SEPARATOR_LINE;
+
+public class ListVsLines {
+    public static void main(String[] args) {
+        String test = """
+                aaa
+                aaa
+                aaa
+                
+                
+                
+                safdfs
+                s
+                fs
+                f
+                s
+                fs
+                df
+                """;
+
+        int count = 10000000;
+
+        long s = System.currentTimeMillis();
+        for (int i = 0; i<count; i++) {
+            for (var a : test.lines().toList()) {
+
+            }
+        }
+        System.out.println(System.currentTimeMillis() - s);
+
+        s = System.currentTimeMillis();
+        for (int i = 0; i<count; i++) {
+            for (var a : test.split(SEPARATOR_LINE)) {
+
+            }
+        }
+        System.out.println(System.currentTimeMillis() - s);
+    }
+
+    // lines.toList() < split
+}
