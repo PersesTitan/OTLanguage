65a364fb163249a2396951a7fe5af12838654d66
PersesTitan <torampersesTitan@gmail.com>
2022-11-17 17:24:18 +0900

    update break point

--- src/main/java/bin/apply/sys/make/StartLine.java
+++ src/main/java/bin/apply/sys/make/StartLine.java
@@ -1,11 +1,10 @@
 package bin.apply.sys.make;
 
+import bin.apply.Repository;
 import bin.apply.Setting;
 import bin.apply.sys.item.RunType;
-import bin.exception.ConsoleException;
-import bin.exception.MatchException;
-import bin.exception.ServerException;
-import bin.exception.VariableException;
+import bin.calculator.tool.Calculator;
+import bin.exception.*;
 import bin.token.LoopToken;
 
 import java.io.File;
@@ -19,9 +18,10 @@ import static bin.apply.Controller.bracket;
 import static bin.apply.Controller.loopController;
 import static bin.apply.Setting.start;
 import static bin.apply.sys.item.SystemSetting.extensionCheck;
+import static bin.calculator.tool.Calculator.*;
 
-public class StartLine implements LoopToken {
-
+public class StartLine implements LoopToken, Calculator {
+    public static boolean developmentMode = false;
     public static void startLine(String total, String path,
                                  LinkedList<Map<String, Map<String, Object>>> repositoryArray) {
         boolean extensionCheck = extensionCheck(path);
@@ -30,22 +30,25 @@ public class StartLine implements LoopToken {
             String finalTotal = getFinalTotal(extensionCheck, total, path);
             startStartLine(finalTotal, total, repositoryArray);
         } catch (VariableException e) {
-            e.printStackTrace();
             new VariableException().variableErrorMessage(e, errorPath.get(), errorLine.get(), errorCount.get());
-            setLine();
+            setLine(e);
         } catch (MatchException e) {
             new MatchException().matchErrorMessage(e, errorPath.get(), errorLine.get(), errorCount.get());
-            setLine();
+            setLine(e);
         } catch (ServerException e) {
             new ServerException().serverErrorMessage(e, errorPath.get(), errorLine.get(), errorCount.get());
-            setLine();
+            setLine(e);
         } catch (ConsoleException e) {
             new ConsoleException().consoleErrorMessage(e, errorPath.get(), errorLine.get(), errorCount.get());
-            setLine();
+            setLine(e);
+        } catch (CosException e) {
+            new CosException().cosErrorMessage(e, errorPath.get(), errorLine.get(), errorCount.get());
+            setLine(e);
         }
     }
 
-    private static void setLine() {
+    private static void setLine(RuntimeException e) {
+        if (developmentMode) e.printStackTrace();
         if (Setting.runType.equals(RunType.Normal)) System.exit(0);
     }
 
@@ -67,10 +70,34 @@ public class StartLine implements LoopToken {
                                    LinkedList<Map<String, Map<String, Object>>> repositoryArray) {
         for (var line : bracket.bracket(total, fileName, false).lines().toList()) {
             line = loopController.check(setError(line, total).strip());
-            if (line.equals(BREAK) || line.equals(CONTINUE)) return line;
-            else Setting.start(line, errorLine.get(), repositoryArray);
+            if (line.endsWith(BREAK) || line.endsWith(CONTINUE)) {
+                // FINISH, CONTINUE, BREAK
+                String lines = returnLoop(line.strip(), repositoryArray);
+                if (line.equals(CONTINUE)) continue;
+                if (!lines.equals(finish)) return lines;
+            } else Setting.start(line, errorLine.get(), repositoryArray);
         }
-        return "FINISH";
+        return finish;
+    }
+
+    private final static String finish = "FINISH";
+    private static String returnLoop(String line, LinkedList<Map<String, Map<String, Object>>> repositoryArray) {
+        if (line.equals(BREAK)) return BREAK;
+        else if (line.equals(CONTINUE)) return CONTINUE;
+        else if (line.endsWith(BREAK)) {
+            int position = line.lastIndexOf('?');
+            return line.substring(position + 1).strip().equals(BREAK)
+                    && getBool(line.substring(0, position), repositoryArray)
+                    ? BREAK
+                    : finish;
+        } else if (line.endsWith(CONTINUE)) {
+            int position = line.lastIndexOf('?');
+            return line.substring(position + 1).strip().equals(CONTINUE)
+                    && getBool(line.substring(0, position), repositoryArray)
+                    ? CONTINUE
+                    : finish;
+        }
+        return "";
     }
 
     public static void startPoison(String total, String fileName,
@@ -86,6 +113,7 @@ public class StartLine implements LoopToken {
     public static final AtomicReference<String> errorPath = new AtomicReference<>();
     public static String setError(String line, String total) {
         if (line.isBlank()) return "";
+        else if (LOOP_TOKEN.containsKey(total)) total = LOOP_TOKEN.get(total);
         String[] tokens = line.split(" ", 2);
         String lineCount = tokens[0];
         errorCount.set(Integer.parseInt(lineCount));
