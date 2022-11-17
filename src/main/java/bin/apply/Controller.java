bcc86a608a19c2c4c38dae0468da4e7b14cb1b77
PersesTitan <torampersesTitan@gmail.com>
2022-11-17 17:22:04 +0900

    add For

--- src/main/java/bin/apply/Controller.java
+++ src/main/java/bin/apply/Controller.java
@@ -1,27 +1,35 @@
 package bin.apply;
 
 import bin.apply.sys.make.Bracket;
-import bin.calculator.NumberCalculator;
-import bin.calculator.bool.BoolCalculator;
-import bin.calculator.bool.CompareCalculator;
+import bin.calculator.number.AllNumberCalculator;
+import bin.calculator.bool.all.AllBoolCalculator;
+import bin.calculator.bool.all.AllCompareCalculator;
 import bin.check.VariableTypeCheck;
 import bin.exception.ConsoleException;
 import bin.orign.GetSetVariable;
+import bin.orign.loop.For;
 import bin.orign.loop.tool.LoopController;
+import bin.string.list.AggregationList;
 
 import java.io.*;
 
 public interface Controller {
-    NumberCalculator numberCalculator = new NumberCalculator();
-    BoolCalculator boolCalculator = new BoolCalculator();
-    CompareCalculator compareCalculator = new CompareCalculator();
+    AllNumberCalculator allNumberCalculator = new AllNumberCalculator();
+    AllBoolCalculator allBoolCalculator = new AllBoolCalculator();
+    AllCompareCalculator allCompareCalculator = new AllCompareCalculator();
+
     LoopController loopController = new LoopController();
     Bracket bracket = new Bracket();
     GetSetVariable getSetVariable = new GetSetVariable();
     VariableTypeCheck variableTypeCheck = new VariableTypeCheck();
+    For forStart = new For();
+
+
+    AggregationList aggregationList = new AggregationList();
     // 입력, 출력
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
 
     static String scanner() {
         try {
