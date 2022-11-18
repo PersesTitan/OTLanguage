001ff7614f6be9f1ae134f4af644b1d140f899eb
PersesTitan <torampersesTitan@gmail.com>
2022-11-18 12:01:36 +0900

    Update StringToken.java

--- src/main/java/bin/token/StringToken.java
+++ src/main/java/bin/token/StringToken.java
@@ -10,11 +10,21 @@ public interface StringToken {
     String CONTAINS = QUESTION;
     String EQUALS = "=" + QUESTION;
 
-    String CONTAINS_S = QUESTION_S;
-    String EQUALS_S = "=" + QUESTION_S;
+    String CONTAINS_S = QUESTION_S;         // ?
+    String EQUALS_S = "=" + QUESTION_S;     // =?
+    String PATTERN = "ㅍㅌㅍ";
 
-    String ADD_ALL = "";        // 합집합
-    String RETAIN_ALL = "";     // 교집합
-    String REMOVE_ALL = "";     // 차집합
-    String CONTAINS_ALL = "";   // 부분 집합
+    String TO_LOWER = "ㅅㅁㅅ";
+    String TO_UPPER = "ㄷㅁㄷ";
+
+    String ADD_ALL = "ㅎㅈㅎ";        // 합집합
+    String RETAIN_ALL = "ㄱㅈㅎ";     // 교집합
+    String REMOVE_ALL = "ㅊㅈㅎ";     // 차집합
+    String CONTAINS_ALL = "ㅂㅈㅎ";   // 부분 집합
+
+    // MATH
+    String ROUND = "ㅂㅇㅂ";      // 반올림
+    String CEIL = "ㅇㄹㅇ";       // 올림
+    String FLOOR = "ㄴㄹㄴ";      // 내림
+    String ABS = "ㅈㄷㅈ";        // 절댓값
 }
