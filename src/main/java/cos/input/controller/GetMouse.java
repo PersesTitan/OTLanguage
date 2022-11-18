8801c87f13dfdd753e115f5d6a01b5153264e4b0
PersesTitan <torampersesTitan@gmail.com>
2022-11-18 12:00:24 +0900

    Create GetMouse.java

---
+++ src/main/java/cos/input/controller/GetMouse.java
@@ -0,0 +1,22 @@
+package cos.input.controller;
+
+import bin.token.LoopToken;
+import work.v3.ReturnWorkV3;
+
+import java.awt.*;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class GetMouse extends ReturnWorkV3 implements LoopToken {
+    // 0
+    public GetMouse(int... counts) {
+        super(counts);
+    }
+
+    @Override
+    public String start(String line, String[] params,
+                        LinkedList<Map<String, Map<String, Object>>> repositoryArray) {
+        Point local = MouseInfo.getPointerInfo().getLocation();
+        return String.format("[%d, %d]", local.x, local.y);
+    }
+}
