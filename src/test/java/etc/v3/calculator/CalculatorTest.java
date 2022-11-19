f0e6f0a6d9728e20538f6b7feb0de33aca62d105
PersesTitan <torampersesTitan@gmail.com>
2022-11-19 13:59:39 +0900

    Create CalculatorTest.java

---
+++ src/test/java/etc/v3/calculator/CalculatorTest.java
@@ -0,0 +1,18 @@
+package etc.v3.calculator;
+
+import org.joda.time.LocalTime;
+
+import java.math.BigInteger;
+import java.util.HashSet;
+import java.util.Set;
+
+public class CalculatorTest {
+
+    public static void main(String[] args) {
+        System.out.println(new LocalTime());
+    }
+
+    private void sum() {
+//        BigInteger bigInteger = new BigInteger();
+    }
+}
