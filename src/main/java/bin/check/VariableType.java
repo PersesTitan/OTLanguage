c9301480f8b338237d0c4b034e2816bcc310a558
PersesTitan <torampersesTitan@gmail.com>
2022-11-20 10:22:43 +0900

    Update VariableType.java

--- src/main/java/bin/check/VariableType.java
+++ src/main/java/bin/check/VariableType.java
@@ -4,5 +4,6 @@ public enum VariableType {
     Boolean, Character, Double, Float, Integer, Long, String,
     ListBoolean, ListCharacter, ListDouble, ListFloat, ListInteger, ListLong, ListString,
     MapBoolean, MapCharacter, MapDouble, MapFloat, MapInteger, MapLong, MapString,
-    SetBoolean, SetCharacter, SetDouble, SetFloat, SetInteger, SetLong, SetString
+    SetBoolean, SetCharacter, SetDouble, SetFloat, SetInteger, SetLong, SetString,
+    Method
 }
