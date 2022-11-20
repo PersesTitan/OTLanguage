2238d3e741adfe242e519a27c1cb982d59bf5013
PersesTitan <torampersesTitan@gmail.com>
2022-11-20 10:22:16 +0900

    Update DefineKlass.java

--- src/main/java/bin/define/klass/DefineKlass.java
+++ src/main/java/bin/define/klass/DefineKlass.java
@@ -1,75 +1,158 @@
 package bin.define.klass;
 
-import bin.exception.VariableException;
-import bin.token.LoopToken;
-import work.StartWork;
+import bin.apply.sys.item.HpMap;
+import bin.apply.sys.item.SystemSetting;
+import bin.apply.sys.make.StartLine;
+import bin.define.item.MethodItemReturn;
+import bin.define.item.MethodItemVoid;
+import bin.exception.MatchException;
+import bin.token.MergeToken;
+import work.v3.ReturnWorkV3;
+import work.v3.StartWorkV3;
 
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.*;
 
-public class DefineKlass implements LoopToken, StartWork {
-    private final String type;
-    private final Matcher matcher;
+import static bin.apply.Repository.*;
+import static bin.apply.sys.make.StartLine.startStartLine;
+import static bin.define.item.MethodItemTool.*;
+import static bin.token.LoopToken.METHOD;
+import static bin.token.Token.BL;
+import static bin.token.Token.BR;
 
-    public DefineKlass(String type) {
-        this.type = type;
-        String params = orMerge(TOTAL_LIST) + BLANKS + VARIABLE_HTML;
-        String patternText = startEndMerge(
-                type, BLANKS, VARIABLE_HTML,
-                "((", BL, params, BR, ")+|", BL, BR, ")",
-                BLANKS, BRACE_STYLE);
-        this.matcher = Pattern.compile(patternText).matcher("");
+public class DefineKlass extends StartWorkV3 implements MergeToken {
+    // 1
+    public DefineKlass(int... counts) {
+        super(counts);
     }
 
     @Override
-    public boolean check(String line) {
-        return this.matcher.reset(line).find();
-    }
-
-    @Override
-    public void start(String line, String origen,
+    public void start(String line, String[] params,
                       LinkedList<Map<String, Map<String, Object>>> repositoryArray) {
-        // ㅋㅅㅋ 클래스명[] (test,1,2)    // 클래스명[] (test,1,2)
-        line = line.strip().substring(type.length()).strip();
-        // 클래스명, ] (test,1,2)
-        String[] tokens = matchSplitError(line, BL, 2);
-        // ㅇㅈㅇ 변수명, (test,1,2)
-        String[] klassParams = matchSplitError(tokens[1], BR + BLANKS, 2);
-
-        String[][] params = klassParams[0].isEmpty()
-            ? new String[0][0]
-            : getParams(klassParams[0].split(BR + BL));
-        // test, 1, 10
-        String[] fileInformation = matchSplitError(bothEndCut(klassParams[1]), ",", 3);
-        String fileName = fileInformation[0];
-        String total = LOOP_TOKEN.get(fileName);
-        int start = total.indexOf("\n" + fileInformation[1] + " ");
-        int end = total.indexOf("\n" + fileInformation[2] + " ");
-
-        var repository = repositoryArray.get(0).get(this.type);
-
+        // params = 클래스명[ㅇㅈㅇ 변수명1][ㅇㅁㅇ 변수명2] (test,1,2)
+        String tokens = params[0].strip();
+        int start = tokens.indexOf('[');
+        int end = tokens.lastIndexOf(']') + 1;
+        String klassName = tokens.substring(0, start);       // 클래스명
+        String parameter = tokens.substring(start, end);     // [ㅇㅈㅇ 변수명1][ㅇㅁㅇ 변수명2]
+        String loop = tokens.substring(end).strip();         // (test,1,2)
+        if (parameter.startsWith("[") && parameter.endsWith("]")
+                && loop.startsWith("(") && loop.endsWith(")")) {
+            getParameters(klassName, parameter, loop);
+        } else throw new MatchException().grammarError();
     }
 
-    @Override
-    public void first() {
+    private void getParameters(String klassName, String parameter, String loop) {
+        // (test,1,15) => test, 1, 15
+        final String[] loopTokens = getFinal(loop);
+        final String fileName = loopTokens[0];
+        final String finalTotal = loopTokens[1];
 
-    }
-
-    private final Set<String> set = new HashSet<>();
-    private String[][] getParams(String[] params) {
-        set.clear();
-        int count = params.length;
-        String[][] param = new String[count][2];
-        for (int i = 0; i<count; i++) {
-            String[] values = matchSplitError(params[i], BLANKS, 2);
-            if (set.contains(values[1])) throw new VariableException().sameVariable();
-            else set.add(values[1]);
-            param[i] = values;
+        final int paramCount;
+        final String[][] parameters;
+        parameter = bothEndCut(parameter);          // ㅇㅈㅇ 변수명1][ㅇㅁㅇ 변수명2
+        if (parameter.isEmpty()) {                  // [] 일때
+            paramCount = 0;
+            parameters = new String[0][0];
+        } else {
+            // [[ㅇㅈㅇ, 변수명1], [ㅇㅁㅇ, 변수명2]]
+            parameters = getParams(parameter.split(BR + BL));
+            paramCount = parameters.length;
         }
-        return param;
+
+        defineKlass(paramCount, klassName, parameters, finalTotal, fileName);
+    }
+
+    private void defineKlass(int paramCount, String klassName, String[][] parameters,
+                             String finalTotal, String fileName) {
+        SystemSetting.createStartWorks(klassName, "", new StartWorkV3(paramCount) {
+            private final String[][] param = parameters;
+            private final Map<String, Map<String, Object>> repository = new HashMap<>() {{
+                repositoryItems.forEach(v -> put(v, new HpMap(v)));
+            }};
+
+            @Override
+            public void start(String line, String[] params,
+                              LinkedList<Map<String, Map<String, Object>>> repositoryArray) {
+                resetKlassMethod(klassName);
+                // ㅋㅅㅋ 클래스명[] (test,1,14)
+                resetRepository(repository);
+                setParams(this.param, params, repository);
+                repositoryArray.addFirst(repository);
+                try {
+                    startStartLine(finalTotal, fileName, repositoryArray);
+                } finally {
+                    repositoryArray.removeFirst();
+                    var repository = this.repository.get(METHOD);
+                    repository.forEach((methodName, v) -> {
+                        if (v instanceof MethodItemVoid methodItemVoid) {
+                            defineMethodVoid(klassName, methodName, methodItemVoid, this.repository);
+                        } else if (v instanceof MethodItemReturn methodItemReturn) {
+                            defineMethodReturn(klassName, methodName, methodItemReturn, this.repository);
+                        }
+                    });
+                    repository.clear();
+                }
+            }
+        });
+    }
+
+    // (test,1,2) => finalTotal
+    private String[] getFinal(String line) {
+        final String[] loopTokens = getLoopTotal(line);
+        final String fileName = loopTokens[0], total = loopTokens[1];
+        final String finalTotal = StartLine.getFinalTotal(false, total, fileName).strip();
+        return new String[]{fileName, finalTotal};
+    }
+
+    private void resetKlassMethod(String klassName) {
+        var rep1 = startWorksV3.get(klassName);
+        if (rep1 != null) {
+            var v = rep1.remove("");
+            rep1.clear();
+            rep1.put("", v);
+        }
+        var rep2 = returnWorksV3.get(klassName);
+        if (rep2 != null) {
+            var v = rep2.remove("");
+            rep2.clear();
+            rep2.put("", v);
+        }
+    }
+
+    /**
+     * 메소드 정의하는 로직
+     */
+    // 타입 void
+    private void defineMethodVoid(String klassName, String methodName, MethodItemVoid methodItemVoid,
+                                  Map<String, Map<String, Object>> klassRepository) {
+        SystemSetting.createStartWorks(klassName, methodName, new StartWorkV3(methodItemVoid.getParams()) {
+            @Override
+            public void start(String line, String[] params,
+                              LinkedList<Map<String, Map<String, Object>>> repositoryArray) {
+                repositoryArray.addFirst(klassRepository);
+                try {
+                    methodItemVoid.start(params, repositoryArray);
+                } finally {
+                    repositoryArray.removeFirst();
+                }
+            }
+        });
+    }
+
+    // 타입 return
+    private void defineMethodReturn(String klassName, String methodName, MethodItemReturn methodItemReturn,
+                                    Map<String, Map<String, Object>> klassRepository) {
+        SystemSetting.createReturnWorks(klassName, methodName, new ReturnWorkV3(methodItemReturn.getParams()) {
+            @Override
+            public String start(String line, String[] params,
+                                LinkedList<Map<String, Map<String, Object>>> repositoryArray) {
+                repositoryArray.addFirst(klassRepository);
+                try {
+                    return methodItemReturn.start(params, repositoryArray);
+                } finally {
+                    repositoryArray.removeFirst();
+                }
+            }
+        });
     }
 }
