7a2d6acf6eca351882c97f9dd3a6bd88244452b3
PersesTitan <torampersesTitan@gmail.com>
2022-11-21 00:03:00 +0900

    Create CreateIconTest.java

---
+++ src/test/java/etc/v3/input/keyboard/CreateIconTest.java
@@ -0,0 +1,19 @@
+package etc.v3.input.keyboard;
+
+import bin.apply.Setting;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public interface CreateIconTest {
+    static void setIcon() {
+        try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream("icon.otlm"))) {
+            Taskbar.getTaskbar().setIconImage(((ImageIcon) inputStream.readObject()).getImage());
+        } catch (IOException | ClassNotFoundException e) {
+            Setting.warringMessage("아이콘을 불러올 수 없습니다.");
+        }
+    }
+}
