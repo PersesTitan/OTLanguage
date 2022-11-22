fc75d8cf85446c5efa6b913ac15c8fa767deea53
PersesTitan <torampersesTitan@gmail.com>
2022-11-23 00:05:54 +0900

    Update Separator.java

--- src/main/java/bin/apply/sys/item/Separator.java
+++ src/main/java/bin/apply/sys/item/Separator.java
@@ -1,5 +1,10 @@
 package bin.apply.sys.item;
 
+import bin.apply.Setting;
+import bin.apply.sys.make.StartLine;
+
+import java.nio.file.Paths;
+
 public interface Separator {
     String SEPARATOR_FILE = System.getProperty("file.separator");              // /
     String SEPARATOR_PATH = System.getProperty("path.separator");              // :
@@ -12,11 +17,21 @@ public interface Separator {
     String ALTERATION = "alteration";   // 변경
     String OPERATE = "operate";         // 동작
     String MODULE = "module";
-    String INSTALL_PATH = SEPARATOR_HOME + SEPARATOR_FILE + ".otl";        // /User/name/.otl
-    String MODULE_PATH = INSTALL_PATH + SEPARATOR_FILE + MODULE;           // /User/name/.otl/module
-    String COMPULSION_PATH = MODULE_PATH + SEPARATOR_FILE + COMPULSION;    // /User/name/.otl/module/compulsion
-    String ALTERATION_PATH = MODULE_PATH + SEPARATOR_FILE + ALTERATION;    // /User/name/.otl/module/alteration
-    String OPERATE_PATH = MODULE_PATH + SEPARATOR_FILE + OPERATE;          // /User/name/.otl/module/operate
+
+    // /User/name/.otl
+    String INSTALL_PATH = StartLine.developmentMode
+        ? getPath(SEPARATOR_HOME, ".otl")
+        : Paths.get(Setting.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getParent().toString();
+    String MODULE_PATH = getPath(INSTALL_PATH, MODULE);           // /User/name/.otl/module
+    String COMPULSION_PATH = getPath(MODULE_PATH, COMPULSION);    // /User/name/.otl/module/compulsion
+    String ALTERATION_PATH = getPath(MODULE_PATH, ALTERATION);    // /User/name/.otl/module/alteration
+    String OPERATE_PATH = getPath(MODULE_PATH, OPERATE);          // /User/name/.otl/module/operate
+    // /User/name/.otl/module/system.otls
+    String SYSTEM_PATH = getPath(MODULE_PATH, "system" + SYSTEM_EXTENSION);
+
+    static String getPath(String...line) {
+        return String.join(SEPARATOR_FILE, line);
+    }
 
     /*
         - /User/name/.otl/module - bin - system.otls
