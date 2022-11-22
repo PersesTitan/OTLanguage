78f417ad7e92cd2879537625de3a02c737398c60
PersesTitan <torampersesTitan@gmail.com>
2022-11-23 00:04:11 +0900

    Update StringToken.java

--- src/main/java/bin/token/StringToken.java
+++ src/main/java/bin/token/StringToken.java
@@ -17,6 +17,10 @@ public interface StringToken {
     String TO_LOWER = "ㅅㅁㅅ";
     String TO_UPPER = "ㄷㅁㄷ";
 
+    String INDEX = "ㅇㅊㅇ";
+    String LAST_INDEX = "ㅇㅈㅇ";
+    String SUBSTRING = "ㅋㅌㅋ";
+
     String ADD_ALL = "ㅎㅈㅎ";        // 합집합
     String RETAIN_ALL = "ㄱㅈㅎ";     // 교집합
     String REMOVE_ALL = "ㅊㅈㅎ";     // 차집합
@@ -27,4 +31,7 @@ public interface StringToken {
     String CEIL = "ㅇㄹㅇ";       // 올림
     String FLOOR = "ㄴㄹㄴ";      // 내림
     String ABS = "ㅈㄷㅈ";        // 절댓값
+
+    String MAX = "ㅋㄴㅋ";     // 큰 값
+    String MIN = "ㅈㄱㅈ";     // 작은 값
 }
