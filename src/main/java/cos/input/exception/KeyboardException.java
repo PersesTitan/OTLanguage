ced33903be26de5ab45628e246523ee330d3ca10
PersesTitan <torampersesTitan@gmail.com>
2022-11-23 14:30:53 +0900

    Update KeyboardException.java

--- src/main/java/cos/input/exception/KeyboardException.java
+++ src/main/java/cos/input/exception/KeyboardException.java
@@ -11,16 +11,18 @@ public class KeyboardException extends CosException {
     }
 
     public KeyboardException awtError() {
-        String awtError = "화면, 마우스, 키보드 등의 접근이 불가능합니다. `"
-                + "Screen, mouse, keyboard, etc. are not available."
-                + "\nPlease check the screen, mouse, and keyboard permissions and status.";
+        String awtError = """
+                화면, 마우스, 키보드 등의 접근이 불가능합니다.
+                Screen, mouse, keyboard, etc. are not available.
+                Please check the screen, mouse, and keyboard permissions and status.""";
         return new KeyboardException(awtError);
     }
 
     public KeyboardException securityException() {
-        String errorMessage = "생성 권한이 부여되지 않았습니다. `" +
-                "Create permission not granted.\n" +
-                "Please retry after granting permission.";
+        String errorMessage = """
+                생성 권한이 부여되지 않았습니다.
+                Create permission not granted.
+                Please retry after granting permission.""";
         return new KeyboardException(errorMessage);
     }
 }
