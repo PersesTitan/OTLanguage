4f8cc4d884127a912f93a5e7353e47ada6e0253b
PersesTitan <torampersesTitan@gmail.com>
2022-11-25 00:39:10 +0900

    Update MakeTest.java

--- src/test/java/module/MakeTest.java
+++ src/test/java/module/MakeTest.java
@@ -1,34 +1,24 @@
 package module;
 
 import bin.apply.Setting;
+import bin.calculator.tool.CalculatorTool;
+import bin.exception.MatchException;
 import work.v3.ReturnWorkV3;
 import work.v3.StartWorkV3;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
+import java.util.*;
 
 import static bin.apply.Repository.*;
 import static bin.apply.sys.item.Separator.*;
+import static bin.calculator.tool.CalculatorTool.*;
+import static bin.token.Token.NO_TOKEN;
+import static bin.token.Token.TOKEN;
+import static bin.token.cal.BoolToken.*;
 
 public class MakeTest {
-
     public static void main(String[] args) {
-        HashMap<String, Map<String, StartWorkV3>> priorityStartWorksV3 = new HashMap<>();
-
-        new Setting();
-        System.out.println(COMPULSION_PATH);
-        System.out.println(ALTERATION_PATH);
-        System.out.println(OPERATE_PATH);
-
-        new File(COMPULSION_PATH).mkdirs();
-        new File(ALTERATION_PATH).mkdirs();
-        new File(OPERATE_PATH).mkdirs();
-
-        new MakeModule<LinkedList<Map<String, Map<String, Object>>>>().makeWork("list.otlm", repository);
-        new MakeModule<HashMap<String, Map<String, StartWorkV3>>>().makeWork(OPERATE_PATH + SEPARATOR_FILE + COMPULSION + MODULE_EXTENSION, startWorksV3);
-        new MakeModule<HashMap<String, Map<String, StartWorkV3>>>().makeWork(COMPULSION_PATH + SEPARATOR_FILE + ALTERATION + MODULE_EXTENSION, priorityStartWorksV3);
-        new MakeModule<HashMap<String, Map<String, ReturnWorkV3>>>().makeWork(ALTERATION_PATH + SEPARATOR_FILE + OPERATE + MODULE_EXTENSION, returnWorksV3);
+        String a = "asd.af.otl";
+        System.out.println(a.substring(0, a.lastIndexOf('.')));
     }
 }
