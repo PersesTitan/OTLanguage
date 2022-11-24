0215384cd5dd718359f0167327a264430654194c
PersesTitan <torampersesTitan@gmail.com>
2022-11-25 00:39:51 +0900

    Create TypingTest.java

---
+++ src/test/java/etc/v3/input/keyboard/start/TypingTest.java
@@ -0,0 +1,22 @@
+package etc.v3.input.keyboard.start;
+
+import work.v3.StartWorkV3;
+
+import java.awt.*;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class TypingTest extends StartWorkV3 {
+    private final Robot robot;
+
+    public TypingTest(Robot robot, int... counts) {
+        super(counts);
+        this.robot = robot;
+    }
+
+    @Override
+    public void start(String line, String[] params,
+                      LinkedList<Map<String, Map<String, Object>>> repositoryArray) {
+
+    }
+}
