63b3c56b56d67e6cd30ecb7e8914d065b8335749
PersesTitan <torampersesTitan@gmail.com>
2022-11-25 00:39:20 +0900

    Update MakeGitTest.java

--- src/test/java/module/download/MakeGitTest.java
+++ src/test/java/module/download/MakeGitTest.java
@@ -1,11 +1,11 @@
 package module.download;
 
-import bin.apply.Repository;
 import bin.apply.sys.item.SystemSetting;
 import bin.apply.sys.run.FilePath;
 import bin.apply.sys.run.ForceQuit;
 import bin.apply.sys.run.Sleep;
 import bin.apply.sys.run.TryCatch;
+import bin.define.klass.DefineKlass;
 import bin.define.method.DefineMethod;
 import bin.orign.console.normal.*;
 import bin.orign.console.priority.PriorityPrint;
@@ -14,7 +14,13 @@ import bin.orign.console.priority.PriorityPrintTap;
 import bin.orign.console.priority.PriorityPrintln;
 import bin.orign.loop.If;
 import bin.orign.loop.While;
+import bin.orign.math.Max;
+import bin.orign.math.Min;
 import bin.orign.math.random.*;
+import bin.orign.math.round.Abs;
+import bin.orign.math.round.Ceil;
+import bin.orign.math.round.Floor;
+import bin.orign.math.round.Round;
 import bin.orign.variable.CreateList;
 import bin.orign.variable.CreateMap;
 import bin.orign.variable.CreateOrigin;
@@ -24,15 +30,32 @@ import bin.string.list.ListAdd;
 import bin.string.list.ListContains;
 import bin.string.list.ListRemove;
 import bin.string.list.ListRetain;
+import bin.string.pattern.MatcherFind;
+import bin.string.position.Index;
+import bin.string.position.LastIndex;
+import bin.string.position.SubString;
+import bin.string.tocase.ToLower;
+import bin.string.tocase.ToUpper;
 import bin.token.LoopToken;
+import cos.music.replace.GetLoop;
+import cos.music.replace.GetPitch;
+import cos.music.replace.GetSpeed;
+import cos.music.replace.GetVolume;
+import cos.music.start.*;
 import cos.poison.controller.HttpMethod;
 import cos.poison.setting.PoisonCreate;
 import cos.poison.setting.PoisonMethod;
 import cos.poison.setting.PoisonStart;
+import etc.gui.OGUITest;
+import etc.gui.item.*;
+import etc.gui.replace.*;
+import etc.gui.start.*;
+import etc.music.start.*;
 
 import java.io.*;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -45,26 +68,34 @@ import static bin.token.StringToken.*;
 import static bin.token.VariableToken.FILE;
 import static bin.token.VariableToken.STRING_VARIABLE;
 import static bin.token.cal.NumberToken.*;
+import static etc.gui.setting.RepositoryTest.*;
+import static etc.music.paly.MusicRepository.*;
 import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
 public class MakeGitTest {
     public MakeGitTest() {
-        clear();
-        start1();
+        clear(); start1();
         new MakeGitTest("origin", "src/main/java/bin");
-        clear();
-        start2();
+        clear(); start2();
         new MakeGitTest("poison", "src/main/java/cos/poison");
+        clear(); start3();
+        new MakeGitTest("gui", "src/main/java/cos/gui");
+        clear(); start4();
+        new MakeGitTest("music", "src/main/java/cos/music");
     }
 
     private static void clear() {
+        system.clear();
         list.clear();
         priorityStartWorksV3.clear();
         startWorksV3.clear();
         returnWorksV3.clear();
+        files.clear();
     }
 
+    private static final List<String> system = new ArrayList<>();
     private static final List<File> list = new ArrayList<>();
+    private static final List<String> files = new ArrayList<>();
     private void listFile(File file) {
         for (File cpFile : Objects.requireNonNull(file.listFiles())) {
             if (cpFile.isFile()) list.add(cpFile);
@@ -72,18 +103,57 @@ public class MakeGitTest {
         }
     }
 
+    public static String SYSTEM = "system:";
+    public static String MODULE = "module:";
+    public static String CLASS  = "class:";
+    public static String FILE = "file:";
+
     String COMPULSION = "compulsion";   // 강제
     String ALTERATION = "alteration";   // 변경
     String OPERATE = "operate";         // 동작
 
     // name : poison
     private MakeGitTest(String name, String fileName) {
+        // 기본 세팅 확인
+        boolean bool = name.equals("origin");
         listFile(new File(fileName));
         // User/name/Documents/GitHub/module/poison
         String namePath = SEPARATOR_HOME + "/Documents/GitHub/module/" + name;
+        String namePath2 = null;
+        if (bool) namePath2 = SEPARATOR_HOME + "/Documents/GitHub/.otl/module/";
         File file = new File(namePath);
         file.mkdirs();
+
+        // 기본 값 추가
+        if (bool) {
+            File file1 = new File(namePath2);
+            if (!file1.isFile()) file1.mkdirs();
+            try (BufferedWriter bw = new BufferedWriter(
+                    new FileWriter(namePath2 + "/system.otls", false))) {
+                system.forEach(v -> {
+                    try {
+                        bw.write(v.replace("\\", ""));
+                        bw.newLine();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
         try (BufferedWriter br = new BufferedWriter(new FileWriter(namePath + "/system.otls", false))) {
+            br.write(SYSTEM);
+            br.newLine();
+            for (String systemItem : system) {
+                br.write("   " + systemItem.replace("\\", ""));
+                br.newLine();
+            }
+            br.newLine();
+
+            br.write(MODULE);
+            br.newLine();
             if (!priorityStartWorksV3.isEmpty()) {
                 try (var output = new ObjectOutputStream(new FileOutputStream(namePath + "/" + COMPULSION + MODULE_EXTENSION))) {
                     output.writeObject(priorityStartWorksV3);
@@ -91,6 +161,14 @@ public class MakeGitTest {
                 try (var output = new ObjectOutputStream(new FileOutputStream(COMPULSION_PATH + "/" + name + ".otlm"))) {
                     output.writeObject(priorityStartWorksV3);
                 } catch (IOException ignored) {}
+                if (bool) {
+                    File file1 = new File(namePath2 + "compulsion/" + name + ".otlm");
+                    file1.getParentFile().mkdirs();
+                    try (var output = new ObjectOutputStream(new FileOutputStream(file1))) {
+                        output.writeObject(priorityStartWorksV3);
+                    } catch (IOException ignored) {}
+                }
+                br.write("   ");
                 br.write(COMPULSION);
                 br.newLine();
             }
@@ -102,7 +180,14 @@ public class MakeGitTest {
                 try (var output = new ObjectOutputStream(new FileOutputStream(ALTERATION_PATH + "/" + name + ".otlm"))) {
                     output.writeObject(returnWorksV3);
                 } catch (IOException ignored) {}
-
+                if (bool) {
+                    File file1 = new File(namePath2 + "alteration/" + name + ".otlm");
+                    file1.getParentFile().mkdirs();
+                    try (var output = new ObjectOutputStream(new FileOutputStream(file1))) {
+                        output.writeObject(priorityStartWorksV3);
+                    } catch (IOException ignored) {}
+                }
+                br.write("   ");
                 br.write(ALTERATION);
                 br.newLine();
             }
@@ -114,20 +199,45 @@ public class MakeGitTest {
                 try (var output = new ObjectOutputStream(new FileOutputStream(OPERATE_PATH + "/" + name + ".otlm"))) {
                     output.writeObject(startWorksV3);
                 } catch (IOException ignored) {}
+                if (bool) {
+                    File file1 = new File(namePath2 + "operate/" + name + ".otlm");
+                    file1.getParentFile().mkdirs();
+                    try (var output = new ObjectOutputStream(new FileOutputStream(file1))) {
+                        output.writeObject(startWorksV3);
+                    } catch (IOException ignored) {}
+                }
+                br.write("   ");
                 br.write(OPERATE);
                 br.newLine();
             }
 
+            br.newLine();
+            br.write(CLASS);
             for (File f : list) {
                 int start = (System.getProperty("user.home") + "/Documents/Java/OTLanguage/src/main/java/").length();
                 int end = f.getAbsolutePath().length() - ".java".length();
                 String value = f.getAbsolutePath().substring(start, end).replace("/", "~");
                 br.newLine();
+                br.write("   ");
                 br.write(value.replace(".", "~"));
 
                 String klassName = file.getAbsolutePath() + "/" + f.getName().substring(0, f.getName().length()-".java".length()) + ".class";
                 copy(value.replace("~", SEPARATOR_FILE), klassName);
+            }
 
+            if (!files.isEmpty()) {
+                br.newLine();
+                br.newLine();
+                br.write(FILE);
+                files.forEach(line -> {
+                    try {
+                        br.newLine();
+                        br.write("   ");
+                        br.write(line.replace("/", ACCESS));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -140,6 +250,14 @@ public class MakeGitTest {
     }
 
     private static void start1() {
+        system.add(IF_);
+        system.add(ELSE_IF_);
+        system.add(ELSE_);
+        system.add(WHITE);
+        system.add(TRY_CATCH);
+        system.add(METHOD);
+        system.add(KLASS);
+
         String WHITE = LoopToken.WHITE.replace("\\", "");
         String IF = LoopToken.IF.replace("\\", "");
 
@@ -167,7 +285,7 @@ public class MakeGitTest {
         createStartWorks(WHITE, "", new While(1));
         createStartWorks(IF, "", new If(1));
         createStartWorks(METHOD, "", new DefineMethod(1));
-
+        createStartWorks(KLASS, "", new DefineKlass(1));
 
         createReturnWorks(RANDOM_BOOL, "", new RandomBoolean(1));
         createReturnWorks(RANDOM_DOUBLE, "", new RandomDouble(1, 2));
@@ -180,18 +298,94 @@ public class MakeGitTest {
         createReturnWorks(STRING_VARIABLE, SPLIT_REGULAR, new SplitRegular(2));
         createReturnWorks(STRING_VARIABLE, CONTAINS_S, new Contains(2));
         createReturnWorks(STRING_VARIABLE, EQUALS_S, new Equals(2));
+        createReturnWorks(STRING_VARIABLE, PATTERN, new MatcherFind(2));
+        createReturnWorks(STRING_VARIABLE, TO_LOWER, new ToLower(1));
+        createReturnWorks(STRING_VARIABLE, TO_UPPER, new ToUpper(1));
+        createReturnWorks(STRING_VARIABLE, INDEX, new Index(2, 3));
+        createReturnWorks(STRING_VARIABLE, LAST_INDEX, new LastIndex(2, 3));
+        createReturnWorks(STRING_VARIABLE, SUBSTRING, new SubString(2, 3));
         LIST_LIST.forEach(v -> {
             createStartWorks(v, ADD_ALL, new ListAdd(v, 2));
             createStartWorks(v, RETAIN_ALL, new ListRetain(v, 2));
             createStartWorks(v, REMOVE_ALL, new ListRemove(v, 2));
             createReturnWorks(v, CONTAINS_ALL, new ListContains(v, 2));
         });
+
+        Round round = new Round(1);
+        Ceil ceil = new Ceil(1);
+        Floor floor = new Floor(1);
+        Abs abs = new Abs(1);
+        SystemSetting.createReturnWorks(INT_VARIABLE, ABS, abs);
+        SystemSetting.createReturnWorks(LONG_VARIABLE, ABS, abs);
+        Arrays.asList(DOUBLE_VARIABLE, FLOAT_VARIABLE).forEach(v -> {
+            SystemSetting.createReturnWorks(v, ROUND, round);
+            SystemSetting.createReturnWorks(v, CEIL, ceil);
+            SystemSetting.createReturnWorks(v, FLOOR, floor);
+            SystemSetting.createReturnWorks(v, ABS, abs);
+        });
+
+        Max max = new Max(2);
+        Min min = new Min(2);
+        Arrays.asList(INT_VARIABLE, LONG_VARIABLE, FLOAT_VARIABLE, DOUBLE_VARIABLE).forEach(v -> {
+            createReturnWorks(v, MAX, max);
+            createReturnWorks(v, MIN, min);
+        });
     }
 
     private static void start2() {
+        system.add(POISON + ACCESS + POISON_POST);
+        system.add(POISON + ACCESS + POISON_GET);
         createStartWorks(POISON, "", new PoisonCreate(1, 2));
         createStartWorks(POISON, POISON_START, new PoisonStart(0));
         createStartWorks(POISON, POISON_POST, new PoisonMethod(HttpMethod.POST));
         createStartWorks(POISON, POISON_GET, new PoisonMethod(HttpMethod.GET));
+    }
+
+    private static void start3() {
+        system.add(GUI + ACCESS + ACTIONS);
+
+        createStartWorks(GUI, "", new OGUITest(0));
+        createStartWorks(GUI, ADD, new OAddTest(2));
+        createStartWorks(GUI, REMOVE, new ORemoveTest(2));
+        createStartWorks(GUI, SET_MOVE, new OSetMoveTest(3));
+        createStartWorks(GUI, SET_SIZE, new OSetSizeTest(3));
+        createStartWorks(GUI, SET_TEXT, new OSetTextTest(2));
+        createStartWorks(GUI, SET_VISIBLE, new OSetVisibleTest(2));
+        createStartWorks(GUI, ACTIONS, new OAddActionsTest(1));
+
+        createStartWorks(GUI, BUTTON, new OButtonTest(1, 2));
+        createStartWorks(GUI, LABEL, new OLabelTest(1, 2));
+        createStartWorks(GUI, PASSWORD_FIELD, new OPasswordFieldTest(1, 2));
+        createStartWorks(GUI, TEXT_FIELD, new OTextFieldTest(1, 2));
+        createStartWorks(GUI, PANEL, new OPanelTest(1));
+        createStartWorks(GUI, FRAME, new OFrameTest(0, 1));
+
+        createReturnWorks(GUI, GET_CONTAINS, new OGetContainsTest(3));
+        createReturnWorks(GUI, GET_HEIGHT, new OGetHeightTest(1));
+        createReturnWorks(GUI, GET_POSITION, new OGetPositionTest(1));
+        createReturnWorks(GUI, GET_TEXT, new OGetTextTest(1));
+        createReturnWorks(GUI, GET_WIDTH, new OGetWidthTest(1));
+        createReturnWorks(GUI, GET_X, new OGetXTest(1));
+        createReturnWorks(GUI, GET_Y, new OGetYTest(1));
+    }
+
+    private static void start4() {
+        for (File file : Objects.requireNonNull(new File("music").listFiles())) {
+            String name = file.getName();
+            if (name.endsWith(".wav")) files.add(name);
+        }
+
+        CreateMusic createMusic = new CreateMusic(1);
+        Arrays.asList(도, 레, 미, 파, 솔, 라, 시).forEach(v -> createStartWorks(MUSIC, v, createMusic));
+        createStartWorks(MUSIC, PITCH, new SetPitch(2));
+        createStartWorks(MUSIC, VOLUME, new SetVolume(2));
+        createStartWorks(MUSIC, SPEED, new SetSpeed(2));
+        createStartWorks(MUSIC, LOOP, new SetLoop(2));
+        createStartWorks(MUSIC, PLAY, new PlaySound());
+
+        createReturnWorks(MUSIC, VOLUME, new GetVolume(1));
+        createReturnWorks(MUSIC, PITCH, new GetPitch(1));
+        createReturnWorks(MUSIC, SPEED, new GetSpeed(1));
+        createReturnWorks(MUSIC, LOOP, new GetLoop(1));
     }
 }
