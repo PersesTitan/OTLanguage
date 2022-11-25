1c7a3db7211e8b8c6b0894c96e394a865839a967
PersesTitan <torampersesTitan@gmail.com>
2022-11-26 01:06:21 +0900

    Update MainTest.java

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -3,23 +3,32 @@ import bin.apply.sys.make.ChangeHangle;
 import bin.apply.sys.make.StartLine;
 import bin.exception.FileException;
 import bin.token.LoopToken;
+import etc.groovy.variable.SetVariableTest;
 import etc.gui.replace.*;
 import etc.gui.start.*;
 import module.download.MakeGitTest;
 
 import java.awt.*;
 import java.io.IOException;
+import java.util.Arrays;
 
 import static bin.apply.Controller.br;
 import static bin.apply.Controller.bw;
 import static bin.apply.sys.item.Separator.*;
-import static bin.apply.sys.item.SystemSetting.createReturnWorks;
-import static bin.apply.sys.item.SystemSetting.createStartWorks;
+import static etc.groovy.items.GroovyRepositoryTest.JAVA;
+import static etc.groovy.items.GroovyRepositoryTest.JAVA_START;
 import static etc.gui.setting.RepositoryTest.*;
 
 public class MainTest implements LoopToken, ChangeHangle {
     public static void main(String[] args) throws AWTException, IOException {
         StartLine.developmentMode = true;
+        LOOP_SET.addAll(Arrays.asList(IF_, ELSE_IF_, ELSE_, WHITE_, TRY_CATCH, METHOD, KLASS));
+        LOOP_SET.add(JAVA + ACCESS + JAVA_START);
+        LOOP_SET.addAll(Arrays.asList(
+                GUI + ACCESS + ACTIONS,
+                POISON + ACCESS + POISON_POST,
+                POISON + ACCESS + POISON_GET));
+
 //        Taskbar.getTaskbar().setIconImage(Toolkit.getDefaultToolkit().getImage(sb.toString()));
 //        Taskbar.getTaskbar().setIconImage(Toolkit.getDefaultToolkit().getImage("image/icon.png"));
 //        Robot robot = new Robot();
@@ -27,10 +36,6 @@ public class MainTest implements LoopToken, ChangeHangle {
 
         new MakeGitTest();
         new Setting();
-
-        createStartWorks(GUI, SET_COLUMNS, new OSetColumnsTest(2));
-
-        createReturnWorks(GUI, GET_COLUMNS, new OGetColumnsTest(1));
 
         try {
             new Main(new String[]{SEPARATOR_HOME, "hello.otl"});
