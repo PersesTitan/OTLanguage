e07724c9041f7f98eccac169745335670008a5a1
PersesTitan <torampersesTitan@gmail.com>
2022-11-28 00:03:15 +0900

    Update ReturnShell.java

--- src/main/java/cos/shell/ReturnShell.java
+++ src/main/java/cos/shell/ReturnShell.java
@@ -1,17 +1,15 @@
 package cos.shell;
 
+import cos.shell.tool.ShellTool;
 import work.v3.ReturnWorkV3;
 
 import java.util.LinkedList;
 import java.util.Map;
 
-public class ReturnShell extends ReturnWorkV3 {
-    public ReturnShell(int... counts) {
-        super(counts);
-    }
-
+public class ReturnShell extends ReturnWorkV3 implements ShellTool {
     @Override
-    public String start(String line, String[] params, LinkedList<Map<String, Map<String, Object>>> repositoryArray) {
-        return null;
+    public String start(String line, String[] params,
+                        LinkedList<Map<String, Map<String, Object>>> repositoryArray) {
+        return getValue(line, repositoryArray).toString();
     }
 }
