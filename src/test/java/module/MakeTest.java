30e367702a934acaec1aba745fa3b98e78fafb3c
PersesTitan <torampersesTitan@gmail.com>
2022-11-28 00:02:48 +0900

    Update MakeTest.java

--- src/test/java/module/MakeTest.java
+++ src/test/java/module/MakeTest.java
@@ -1,42 +1,56 @@
 package module;
 
-import bin.apply.Setting;
-import bin.calculator.tool.CalculatorTool;
-import bin.exception.MatchException;
-import work.v3.ReturnWorkV3;
-import work.v3.StartWorkV3;
-
-import java.io.File;
 import java.util.*;
-
-import static bin.apply.Repository.*;
-import static bin.apply.sys.item.Separator.*;
-import static bin.calculator.tool.CalculatorTool.*;
-import static bin.token.Token.NO_TOKEN;
-import static bin.token.Token.TOKEN;
-import static bin.token.cal.BoolToken.*;
 
 public class MakeTest {
     public static void main(String[] args) {
-        int hangle_start = '\uAC00';
-        int hangle_end = '\uD7A3' + 1;
-        Random random = new Random();
-        System.out.println(hangle_start);
-        System.out.println(hangle_end);
-        System.out.println((char) random.nextInt(hangle_start, hangle_end));
-        System.out.println((char) random.nextInt(hangle_start, hangle_end));
-        System.out.println((char) random.nextInt(hangle_start, hangle_end));
-        System.out.println((char) random.nextInt(hangle_start, hangle_end));
-        System.out.println(UUID.randomUUID());
-        System.out.println(UUID.randomUUID());
-        System.out.println(UUID.randomUUID());
-        System.out.println(UUID.randomUUID());
-        System.out.println(UUID.randomUUID());
+        String total = """
+                                
+                ㅉㅂㅉ
+                ㅉㅂㅉ~ㅅㅌㅅ {
+                    리스트 << 5;
+                    리스트.add(15);
+                    블린3=false
+                    System.out.println(리스트);
+                    System.out.println("안녕");
+                    if (true) {
+                        System.out.println("안녕1");
+                    }
+                    else if (false) {
+                        System.out.println("안녕2");
+                    }
+                }
+                                
+                                
+                ㅆㅁㅆ :블린1_
+                ㅆㅁㅆ :블린2_
+                ㅆㅁㅆ :블린3_
+                """;
+
+        start(total, 1);
+    }
+
+    private static int start(String total, int position) {
+        boolean strBool = true;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = position; i < total.length(); i++) {
+            char c = total.charAt(i);
+            switch (c) {
+                case '\"', '\'' -> strBool = !strBool;
+                case '{' -> {
+                    if (strBool) stack.add(i);
+                }
+                case '}' -> {
+                    if (strBool) {
+                        if (stack.isEmpty()) return i;
+                        stack.pop();
+                    }
+                }
+            }
+        }
+        return position;
     }
 
     private final static int START_VALUE = 44032;
     private final static int END_VALUE = 55204;
-    private static String getRandom() {
-
-    }
 }
