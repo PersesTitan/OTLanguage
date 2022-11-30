bfec4112025001f2b65d3e17f44a25d898e50a89
PersesTitan <torampersesTitan@gmail.com>
2022-12-01 07:32:11 +0900

    Update HttpMethod.java

--- src/main/java/cos/poison/controller/HttpMethod.java
+++ src/main/java/cos/poison/controller/HttpMethod.java
@@ -1,5 +1,21 @@
 package cos.poison.controller;
 
+import bin.apply.sys.item.Color;
+
 public enum HttpMethod {
-    POST, GET
+    POST(Color.POST_PRINT),
+    GET(Color.GET_PRINT);
+
+    private static final int maxLen = 4;
+    private final String name;
+    HttpMethod(String color) {
+        this.name = color.concat(" ")
+                .concat(this.name())
+                .concat(" ".repeat(maxLen - this.name().length() + 1))
+                .concat(Color.RESET);
+    }
+
+    public String getName() {
+        return this.name;
+    }
 }
