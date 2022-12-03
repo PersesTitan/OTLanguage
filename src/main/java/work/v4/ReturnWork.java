12fe18522a072bb43258ca373f1bddbe859c8e7f
PersesTitan <torampersesTitan@gmail.com>
2022-12-03 13:29:50 +0900

    Create ReturnWork.java

---
+++ src/main/java/work/v4/ReturnWork.java
@@ -0,0 +1,26 @@
+package work.v4;
+
+import bin.exception.MatchException;
+
+import java.io.Serializable;
+
+public abstract class ReturnWork implements Serializable {
+    private final WorkItem[] workItems;
+    public ReturnWork(WorkItem...workItems) {
+        this.workItems = workItems;
+    }
+
+    public abstract void start(String line, Object[] params);
+
+    public Object[] paramsCheck(int size, String[] params) {
+        if (params.length == 1 && params[0].isEmpty() && size == 0) return new Object[0];
+        return check(size, params);
+    }
+
+    private Object[] check(int size, String[] params) {
+        for (WorkItem item : workItems) {
+            if (size == item.count()) return item.getParams(params);
+        }
+        throw new MatchException().grammarError();
+    }
+}
