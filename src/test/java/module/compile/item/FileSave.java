e491f7aa74ec15420618a8604f0b392f43137248
PersesTitan <torampersesTitan@gmail.com>
2022-12-06 07:47:14 +0900

    Create FileSave.java

---
+++ src/test/java/module/compile/item/FileSave.java
@@ -0,0 +1,75 @@
+package module.compile.item;
+
+import bin.apply.sys.make.Bracket;
+import bin.apply.sys.make.StartLine;
+import bin.exception.FileException;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+import static bin.apply.sys.item.Separator.EXT_REP;
+import static bin.apply.sys.item.Separator.SEPARATOR_LINE;
+import static bin.token.LoopToken.LOOP_TOKEN;
+
+public class FileSave implements Serializable {
+    private String total;
+    private String finalTotal;
+
+    private String fileName;      // test
+    private String separator;     // .otl
+
+    public FileSave(File file) {
+        // set EXT_REP
+        String fileName = file.getName();
+        int count = fileName.indexOf('.');
+        this.fileName  = fileName.substring(0, count);
+        this.separator = fileName.substring(count+1);
+
+        this.finalTotal = Bracket.getInstance().bracket(getTotal(file), file);
+        this.total = LOOP_TOKEN.get(this.fileName);
+    }
+
+    private String getTotal(File file) {
+        StringBuilder total = new StringBuilder();
+        try (FileReader fr = new FileReader(file, StandardCharsets.UTF_8);
+             BufferedReader reader = new BufferedReader(fr)) {
+            for (int i = 1;;i++) {
+                String line = reader.readLine();
+                if (line == null) break;
+                total.append(i).append(" ").append(line.stripLeading()).append(SEPARATOR_LINE);
+            }
+        } catch (IOException e) {
+            if (StartLine.developmentMode) e.printStackTrace();
+            throw new FileException().noReadError();
+        }
+        return total.toString();
+    }
+
+    @Serial
+    private void readObject(ObjectInputStream oi) throws IOException, ClassNotFoundException {
+        this.fileName = (String) oi.readObject();
+        this.finalTotal = (String) oi.readObject();
+        this.separator = (String) oi.readObject();
+        this.total = (String) oi.readObject();
+
+        EXT_REP.put(fileName, separator);
+        LOOP_TOKEN.put(fileName, total);
+        oi.close();
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public String getFinalTotal() {
+        return finalTotal;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public String getSeparator() {
+        return separator;
+    }
+}
