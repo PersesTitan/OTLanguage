2e0a0b7899bf2b4403ac900a505c24d4e1a666ec
PersesTitan <torampersesTitan@gmail.com>
2022-12-09 00:03:38 +0900

    Update PoisonStart.java

--- src/main/java/cos/poison/setting/PoisonStart.java
+++ src/main/java/cos/poison/setting/PoisonStart.java
@@ -5,6 +5,8 @@ import work.v3.StartWorkV3;
 import java.util.LinkedList;
 import java.util.Map;
 
+import static bin.apply.Controller.allLoop;
+import static bin.apply.Setting.debugMode;
 import static cos.poison.setting.PoisonCreate.httpServerManager;
 
 public class PoisonStart extends StartWorkV3 {
@@ -16,6 +18,7 @@ public class PoisonStart extends StartWorkV3 {
     public void start(String line, String[] params,
                       LinkedList<Map<String, Map<String, Object>>> repositoryArray) {
         httpServerManager.start();
-        while (true) {}
+        if (debugMode.isCompile()) httpServerManager.stop();
+        else allLoop();
     }
 }
