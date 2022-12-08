5b0b6a613cfae43b05e7849b4e15872797eddf61
PersesTitan <torampersesTitan@gmail.com>
2022-12-09 00:03:26 +0900

    Update CompileTest.java

--- src/test/java/module/compile/CompileTest.java
+++ src/test/java/module/compile/CompileTest.java
@@ -1,33 +1,57 @@
 package module.compile;
 
-import bin.apply.Repository;
 import bin.apply.Setting;
+import bin.apply.sys.item.DebugMode;
 import bin.apply.sys.make.StartLine;
 import bin.exception.FileException;
+import module.compile.item.CountMap;
 import module.compile.item.FileSave;
 
 import java.io.*;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
-import static bin.apply.sys.item.Separator.EXT_REP;
+import static bin.apply.Repository.*;
+import static bin.apply.Setting.debugMode;
 
 public class CompileTest {
+    public static Set<String> usedModel = new HashSet<>();
+
     public static void main(String[] args) {
 //        new MakeGitTest();
 //        new Setting();
+        debugMode = DebugMode.COMPILE;
+        StartLine.developmentMode = true;
+
+        new Setting();
+        replaceMap(priorityStartWorksV3);
+        replaceMap(startWorksV3);
+        replaceMap(returnWorksV3);
 
         new CompileTest(new File("web.otl"));
         FileSave fileSave = input("item.otli");
-        System.out.println(fileSave);
+        System.out.println(usedModel);
+
+//        System.out.println(fileSave);
 
         Setting.mainPath = new File("web.otl").getAbsolutePath();
         Setting.path = new File("web.otl").getAbsoluteFile().getParent();
 
         assert fileSave != null;
-        StartLine.startStartLine(fileSave.getFinalTotal(), fileSave.getTotal(), Repository.repository);
+//        StartLine.startStartLine(fileSave.getFinalTotal(), fileSave.getTotal(), Repository.repository);
+    }
+
+    private static <V> void replaceMap(Map<String, Map<String, V>> map) {
+        var set = new HashSet<>(map.keySet());
+        for (var key : set) {
+            var m = map.remove(key);
+            map.put(key, new CountMap<>(m, key));
+        }
     }
 
     private CompileTest(File file) {
-        FileSave save = new FileSave(file);
+        FileSave save = new FileSave(file, usedModel);
         output(save);
     }
 
