473657635e7cc004a4bf17565115acf99ec00a15
PersesTitan <torampersesTitan@gmail.com>
2022-12-11 00:16:40 +0900

    Update RunType.java

--- src/main/java/bin/apply/sys/item/RunType.java
+++ src/main/java/bin/apply/sys/item/RunType.java
@@ -1,5 +1,13 @@
 package bin.apply.sys.item;
 
 public enum RunType {
-    Normal, Shell
+    Normal, Shell;
+
+    public boolean isNormal() {
+        return this.equals(Normal);
+    }
+
+    public boolean isShell() {
+        return this.equals(Shell);
+    }
 }
