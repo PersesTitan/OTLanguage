7063d268740e2a68463d61f61b2c0c0aa67e4d58
PersesTitan <torampersesTitan@gmail.com>
2022-12-12 00:09:25 +0900

    Update DebugMode.java

--- src/main/java/bin/apply/sys/item/DebugMode.java
+++ src/main/java/bin/apply/sys/item/DebugMode.java
@@ -2,5 +2,30 @@ package bin.apply.sys.item;
 
 public enum DebugMode {
     // Level = H >>> L
-    INFO, WARRING, ERROR
+    INFO, WARRING, ERROR, COMPILE;
+
+    private final int level;
+    DebugMode() {
+        this.level = this.ordinal();
+    }
+
+    DebugMode(int level) {
+        this.level = level;
+    }
+    /**
+     * ex) INFO.check(DebugMode.WARRING) <br>
+     *               => 1 < 2            <br>
+     *               => true             <br>
+     */
+     public boolean check(DebugMode debugMode) {
+        return this.level < debugMode.level;
+    }
+
+    public boolean isCompile() {
+         return this.equals(COMPILE);
+    }
+
+    public boolean isNoCompile() {
+         return !isCompile();
+    }
 }
