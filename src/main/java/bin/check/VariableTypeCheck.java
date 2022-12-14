d2fb3467938ac6a22d58fca75bb6746f33553210
PersesTitan <torampersesTitan@gmail.com>
2022-12-15 08:43:01 +0900

    Update VariableTypeCheck.java

--- src/main/java/bin/check/VariableTypeCheck.java
+++ src/main/java/bin/check/VariableTypeCheck.java
@@ -123,227 +123,209 @@ public class VariableTypeCheck implements VariableToken, GetSet, GetList, GetMap
     public Object getObject(VariableType variableType,
                             Object values, Object object) {
         String value = values.toString();
-        switch (variableType) {
+        return switch (variableType) {
             case Method -> {
-                if (values instanceof MethodItemReturn || values instanceof MethodItemVoid) return values;
+                if (values instanceof MethodItemReturn || values instanceof MethodItemVoid) yield values;
                 else throw new VariableException().methodTypeMatch();
             }
-            case Integer -> {
-                if (!isInteger(value)) throw new VariableException().typeMatch();
-                else return Integer.parseInt(value);
-            }
-            case Long -> {
-                if (!isLong(value)) throw new VariableException().typeMatch();
-                else return Long.parseLong(value);
-            }
-            case Boolean -> {
-                if (!isBoolean(value)) throw new VariableException().typeMatch();
-                else return value;
-            }
-            case Character -> {
-                if (!isCharacter(value)) throw new VariableException().typeMatch();
-                else return value.charAt(0);
-            }
-            case Float -> {
-                if (!isFloat(value)) throw new VariableException().typeMatch();
-                else return Float.parseFloat(value);
-            }
-            case Double -> {
-                if (!isDouble(value)) throw new VariableException().typeMatch();
-                else return Double.parseDouble(value);
-            }
+            case Integer -> getInteger(value);
+            case Long -> getLong(value);
+            case Boolean -> getBoolean(value);
+            case Character -> getCharacter(value);
+            case Float -> getFloat(value);
+            case Double -> getDouble(value);
             case SetInteger -> {
-                if (object == null) return value.isBlank()
+                if (object == null) yield value.isBlank()
                         ? new LinkedHashSet<Integer>()
                         : setIntegerSet(new LinkedHashSet<>(), getValueSet(value));
                 else if (object instanceof LinkedHashSet<?>) {
-                    return setIntegerSet((LinkedHashSet<Integer>) object, getValueSet(value));
+                    yield setIntegerSet((LinkedHashSet<Integer>) object, getValueSet(value));
                 } else {
                     LinkedHashSet<Integer> set = (LinkedHashSet<Integer>) object;
-                    return setIntegerSet(set, getSetValue(set, value));
+                    yield setIntegerSet(set, getSetValue(set, value));
                 }
             }
             case SetLong -> {
-                if (object == null) return value.isBlank()
+                if (object == null) yield value.isBlank()
                         ? new LinkedHashSet<Long>()
                         : setLongSet(new LinkedHashSet<>(), getValueSet(value));
                 else {
                     LinkedHashSet<Long> set = (LinkedHashSet<Long>) object;
-                    return setLongSet(set, getSetValue(set, value));
+                    yield setLongSet(set, getSetValue(set, value));
                 }
             }
             case SetBoolean -> {
-                if (object == null) return value.isBlank()
+                if (object == null) yield value.isBlank()
                         ? new LinkedHashSet<String>()
                         : setBoolSet(new LinkedHashSet<>(), getValueSet(value));
                 else {
                     LinkedHashSet<String> set = (LinkedHashSet<String>) object;
-                    return setBoolSet(set, getSetValue(set, value));
+                    yield setBoolSet(set, getSetValue(set, value));
                 }
             }
             case SetString -> {
-                if (object == null) return value.isBlank()
+                if (object == null) yield value.isBlank()
                         ? new LinkedHashSet<String>()
                         : setStringSet(new LinkedHashSet<>(), getValueSet(value));
                 else {
                     LinkedHashSet<String> set = (LinkedHashSet<String>) object;
-                    return setStringSet(set, getSetValue(set, value));
+                    yield setStringSet(set, getSetValue(set, value));
                 }
             }
             case SetCharacter -> {
-                if (object == null) return value.isBlank()
+                if (object == null) yield value.isBlank()
                         ? new LinkedHashSet<Character>()
                         : setCharacterSet(new LinkedHashSet<>(), getValueSet(value));
                 else {
                     LinkedHashSet<Character> set = (LinkedHashSet<Character>) object;
-                    return setCharacterSet(set, getSetValue(set, value));
+                    yield setCharacterSet(set, getSetValue(set, value));
                 }
             }
             case SetFloat -> {
-                if (object == null) return value.isBlank()
+                if (object == null) yield value.isBlank()
                         ? new LinkedHashSet<Float>()
                         : setFlotSet(new LinkedHashSet<>(), getValueSet(value));
                 else {
                     LinkedHashSet<Float> set = (LinkedHashSet<Float>) object;
-                    return setFlotSet(set, getSetValue(set, value));
+                    yield setFlotSet(set, getSetValue(set, value));
                 }
             }
             case SetDouble -> {
-                if (object == null) return value.isBlank()
+                if (object == null) yield value.isBlank()
                         ? new LinkedHashSet<Double>()
                         : setDoubleSet(new LinkedHashSet<>(), getValueSet(value));
                 else {
                     LinkedHashSet<Double> set = (LinkedHashSet<Double>) object;
-                    return setDoubleSet(set, getSetValue(set, value));
+                    yield setDoubleSet(set, getSetValue(set, value));
                 }
             }
             case ListInteger -> {
-                if (object == null) return value.isBlank()
+                if (object == null) yield value.isBlank()
                         ? new LinkedList<Integer>()
                         : setIntegerList(new LinkedList<>(), getValueList(value));
                 else {
                     LinkedList<Integer> list = (LinkedList<Integer>) object;
-                    return setIntegerList(list, getListValue(list, value));
+                    yield setIntegerList(list, getListValue(list, value));
                 }
             }
             case ListLong -> {
-                if (object == null) return value.isBlank()
+                if (object == null) yield value.isBlank()
                         ? new LinkedList<Long>()
                         : setLongList(new LinkedList<>(), getValueList(value));
                 else {
                     LinkedList<Long> list = (LinkedList<Long>) object;
-                    return setLongList(list, getListValue(list, value));
+                    yield setLongList(list, getListValue(list, value));
                 }
             }
             case ListBoolean -> {
-                if (object == null) return value.isBlank()
+                if (object == null) yield value.isBlank()
                         ? new LinkedList<String>()
                         : setBoolList(new LinkedList<>(), getValueList(value));
                 else {
                     LinkedList<String> list = (LinkedList<String>) object;
-                    return setBoolList(list, getListValue(list, value));
+                    yield setBoolList(list, getListValue(list, value));
                 }
             }
             case ListString -> {
-                if (object == null) return value.isBlank()
+                if (object == null) yield value.isBlank()
                         ? new LinkedList<String>()
                         : setStringList(new LinkedList<>(), getValueList(value));
                 else {
                     LinkedList<String> list = (LinkedList<String>) object;
-                    return setStringList(list, getListValue(list, value));
+                    yield setStringList(list, getListValue(list, value));
                 }
             }
             case ListCharacter -> {
-                if (object == null) return value.isBlank()
+                if (object == null) yield value.isBlank()
                         ? new LinkedList<Character>()
                         : setCharacterList(new LinkedList<>(), getValueList(value));
                 else {
                     LinkedList<Character> list = (LinkedList<Character>) object;
-                    return setCharacterList(list, getListValue(list, value));
+                    yield setCharacterList(list, getListValue(list, value));
                 }
             }
             case ListFloat -> {
-                if (object == null) return value.isBlank()
+                if (object == null) yield value.isBlank()
                         ? new LinkedList<Float>()
                         : setFlotList(new LinkedList<>(), getValueList(value));
                 else {
                     LinkedList<Float> list = (LinkedList<Float>) object;
-                    return setFlotList(list, getListValue(list, value));
+                    yield setFlotList(list, getListValue(list, value));
                 }
             }
             case ListDouble -> {
-                if (object == null) return value.isBlank()
+                if (object == null) yield value.isBlank()
                         ? new LinkedList<Double>()
                         : setDoubleList(new LinkedList<>(), getValueList(value));
                 else {
                     LinkedList<Double> list = (LinkedList<Double>) object;
-                    return setDoubleList(list, getListValue(list, value));
+                    yield setDoubleList(list, getListValue(list, value));
                 }
             }
             case MapInteger -> {
-                if (object == null) return value.isBlank()
+                if (object == null) yield value.isBlank()
                         ? new LinkedHashMap<String, Integer>()
                         : setIntegerMap(new LinkedHashMap<>(), getValueMap(value));
                 else {
                     LinkedHashMap<String, Integer> map = (LinkedHashMap<String, Integer>) object;
-                    return setIntegerMap(map, getMapValue(map, value));
+                    yield setIntegerMap(map, getMapValue(map, value));
                 }
             }
             case MapLong -> {
-                if (object == null) return value.isBlank()
+                if (object == null) yield value.isBlank()
                         ? new LinkedHashMap<String, Long>()
                         : setLongMap(new LinkedHashMap<>(), getValueMap(value));
                 else {
                     LinkedHashMap<String, Long> map = (LinkedHashMap<String, Long>) object;
-                    return setLongMap(map, getMapValue(map, value));
+                    yield setLongMap(map, getMapValue(map, value));
                 }
             }
             case MapBoolean -> {
-                if (object == null) return value.isBlank()
+                if (object == null) yield value.isBlank()
                         ? new LinkedHashMap<String, String>()
                         : setBoolMap(new LinkedHashMap<>(), getValueMap(value));
                 else {
                     LinkedHashMap<String, String> map = (LinkedHashMap<String, String>) object;
-                    return setBoolMap(map, getMapValue(map, value));
+                    yield setBoolMap(map, getMapValue(map, value));
                 }
             }
             case MapString -> {
-                if (object == null) return value.isBlank()
+                if (object == null) yield value.isBlank()
                         ? new LinkedHashMap<String, String>()
                         : setStringMap(new LinkedHashMap<>(), getValueMap(value));
                 else {
                     LinkedHashMap<String, String> map = (LinkedHashMap<String, String>) object;
-                    return setStringMap(map, getMapValue(map, value));
+                    yield setStringMap(map, getMapValue(map, value));
                 }
             }
             case MapCharacter -> {
-                if (object == null) return value.isBlank()
+                if (object == null) yield value.isBlank()
                         ? new LinkedHashMap<String, Character>()
                         : setCharacterMap(new LinkedHashMap<>(), getValueMap(value));
                 else {
                     LinkedHashMap<String, Character> map = (LinkedHashMap<String, Character>) object;
-                    return setCharacterMap(map, getMapValue(map, value));
+                    yield setCharacterMap(map, getMapValue(map, value));
                 }
             }
             case MapFloat -> {
-                if (object == null) return value.isBlank()
+                if (object == null) yield value.isBlank()
                         ? new LinkedHashMap<String, Float>()
                         : setFlotMap(new LinkedHashMap<>(), getValueMap(value));
                 else {
                     LinkedHashMap<String, Float> map = (LinkedHashMap<String, Float>) object;
-                    return setFlotMap(map, getMapValue(map, value));
+                    yield setFlotMap(map, getMapValue(map, value));
                 }
             }
             case MapDouble -> {
-                if (object == null) return value.isBlank()
+                if (object == null) yield value.isBlank()
                         ? new LinkedHashMap<String, Double>()
                         : setDoubleMap(new LinkedHashMap<>(), getValueMap(value));
                 else {
                     LinkedHashMap<String, Double> map = (LinkedHashMap<String, Double>) object;
-                    return setDoubleMap(map, getMapValue(map, value));
+                    yield setDoubleMap(map, getMapValue(map, value));
                 }
             }
-            default -> {return value;}
-        }
+            default -> value;
+        };
     }
 }
