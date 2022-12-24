989acd98267b9d576748806ede19d90f2ff48b8b
PersesTitan <torampersesTitan@gmail.com>
2022-12-24 10:18:48 +0900

    Update StartWork.java

--- src/main/java/work/v4/StartWork.java
+++ src/main/java/work/v4/StartWork.java
@@ -3,6 +3,8 @@ package work.v4;
 import bin.exception.MatchException;
 
 import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.Map;
 
 public abstract class StartWork implements Serializable {
     private final WorkItem[] workItems;
@@ -10,7 +12,7 @@ public abstract class StartWork implements Serializable {
         this.workItems = workItems;
     }
 
-    public abstract void start(String line, Object[] params, String loop);
+    public abstract void start(String line, Object[] params, String loop, LinkedList<Map<String, Map<String, Object>>> repositoryArray);
 
     public Object[] paramsCheck(int size, String[] params) {
         if (params.length == 1 && params[0].isEmpty() && size == 0) return new Object[0];
