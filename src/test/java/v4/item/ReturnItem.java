1f6da667a7fc760bc9ac759dfc9a3e64789510d1
PersesTitan <torampersesTitan@gmail.com>
2022-12-27 07:36:27 +0900

    Create ReturnItem.java

---
+++ src/test/java/v4/item/ReturnItem.java
@@ -0,0 +1,20 @@
+package v4.item;
+
+import v4.bin.apply.sys.item.Type;
+import v4.bin.apply.sys.item.TypeMap;
+
+import java.util.LinkedList;
+
+public record ReturnItem(String...types) {
+    public Object[] checkType(String[] params, LinkedList<TypeMap> repositoryArray) {
+        return getValue(params, types, repositoryArray);
+    }
+
+    private Object[] getValue(String[] params, String[] types, LinkedList<TypeMap> repositoryArray) {
+        Object[] values = new Object[params.length];
+        for (int i = 0; i<params.length; i++) {
+            values[i] = Type.getType(types[i]).getValue(types[i], params[i], repositoryArray);
+        }
+        return values;
+    }
+}
