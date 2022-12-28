c1998d610514ae4e3aa3738bd82b733ca643bbc3
PersesTitan <torampersesTitan@gmail.com>
2022-12-29 00:45:45 +0900

    Create PrintSpace.java

---
+++ src/test/java/v4/bin/origin/console/PrintSpace.java
@@ -0,0 +1,19 @@
+package v4.bin.origin.console;
+
+import v4.bin.apply.sys.item.TypeMap;
+import v4.item.StartItem;
+import v4.work.StartWork;
+
+import java.util.LinkedList;
+
+public class PrintSpace extends StartWork {
+    public PrintSpace(StartItem... workItems) {
+        super(workItems);
+    }
+
+    @Override
+    public void start(String line, String loop, Object[] params,
+                      LinkedList<TypeMap> repositoryArray) {
+        System.out.print(params[0] + " ");
+    }
+}
