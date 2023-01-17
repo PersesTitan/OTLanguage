036450fb0cec6c5f7897460887ab292958e754c3
PersesTitan <torampersesTitan@gmail.com>
2023-01-18 00:21:47 +0900

    Update RepositoryTest.java

--- src/test/java/etc/v4/RepositoryTest.java
+++ src/test/java/etc/v4/RepositoryTest.java
@@ -1,8 +1,5 @@
 package etc.v4;
 
-import bin.define.method.MethodReturn;
-import bin.define.method.MethodVoid;
-import etc.v4.variable.StartVariable;
 import work.v4.ReturnWork;
 import work.v4.StartWork;
 
@@ -13,7 +10,4 @@ public interface RepositoryTest {
     HashMap<String, Map<String, StartWork>> priorityStartWorks = new HashMap<>();
     HashMap<String, Map<String, StartWork>> startWorks = new HashMap<>();
     HashMap<String, Map<String, ReturnWork>> returnWorks = new HashMap<>();
-    StartVariable startVariable = new StartVariable();
-    MethodVoid methodVoid = new MethodVoid();
-    MethodReturn methodReturn = new MethodReturn();
 }
