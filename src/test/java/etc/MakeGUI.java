959bceac61c0209099687b489155b77cbefd93aa
PersesTitan <torampersesTitan@gmail.com>
2023-01-18 00:21:49 +0900

    Create MakeGUI.java

---
+++ src/test/java/etc/MakeGUI.java
@@ -0,0 +1,29 @@
+package etc;
+
+import javax.swing.*;
+import javax.swing.text.JTextComponent;
+import java.awt.*;
+
+public class MakeGUI {
+    JComponent component;
+
+    JButton button;
+    Canvas canvas;
+    Checkbox checkbox;
+    Choice choice;
+    Container container;
+    /////////
+    JPanel panel;
+    JApplet applet;
+    ///////////
+    JWindow window;
+    JDialog dialog;
+    FileDialog fileDialog;
+    JFrame frame;
+    JScrollPane scrollPane;
+
+    JLabel label;
+    JList list;
+    Scrollbar scrollbar;
+    JTextComponent textComponent;
+}
