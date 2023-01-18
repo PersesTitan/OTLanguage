37baafeeee8fc1ea62e09cef2b43e5877f13197b
PersesTitan <torampersesTitan@gmail.com>
2023-01-19 00:49:21 +0900

    Update MethodVoid.java

--- src/main/java/bin/define/method/MethodVoid.java
+++ src/main/java/bin/define/method/MethodVoid.java
@@ -2,20 +2,40 @@ package bin.define.method;
 
 import bin.define.item.MethodItemVoid;
 import bin.exception.VariableException;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.apache.hadoop.yarn.webapp.hamlet.Hamlet;
 import work.v3.StartWorkV3;
 
 import java.util.LinkedList;
 import java.util.Map;
 
 import static bin.token.LoopToken.METHOD;
+import static bin.token.MergeToken.access;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class MethodVoid extends StartWorkV3 {
+    private static MethodVoid methodVoid;
+    public static MethodVoid getInstance() {
+        if (methodVoid == null) {
+            synchronized (MethodVoid.class) {
+                methodVoid = new MethodVoid();
+            }
+        }
+        return methodVoid;
+    }
+
     @Override
     public void start(String line, String[] params,
                       LinkedList<Map<String, Map<String, Object>>> repositoryArray) {
         int position = line.indexOf('[');
         String variableType = line.substring(0, position);
-        Object startItem = repositoryArray.get(0).get(METHOD).get(variableType);
+
+        int count = access(variableType, repositoryArray.size());
+        if (count == -1) throw new VariableException().localNoVariable();
+        variableType = variableType.substring(count);
+
+        Object startItem = repositoryArray.get(count).get(METHOD).get(variableType);
 
         if (startItem == null) throw new VariableException().noDefineMethod();
         else if (startItem instanceof MethodItemVoid methodItemTest) {
