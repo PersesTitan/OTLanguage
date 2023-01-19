b5177f278723464267828ac25239fdf9713c248a
PersesTitan <torampersesTitan@gmail.com>
2023-01-20 00:04:06 +0900

    Update MethodReturn.java

--- src/main/java/bin/define/method/MethodReturn.java
+++ src/main/java/bin/define/method/MethodReturn.java
@@ -2,20 +2,40 @@ package bin.define.method;
 
 import bin.define.item.MethodItemReturn;
 import bin.exception.VariableException;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import work.v3.ReturnWorkV3;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Map;
 
 import static bin.token.LoopToken.METHOD;
+import static bin.token.MergeToken.access;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class MethodReturn extends ReturnWorkV3 {
+    private static MethodReturn methodReturn;
+    public static MethodReturn getInstance() {
+        if (methodReturn == null) {
+            synchronized (MethodReturn.class) {
+                methodReturn = new MethodReturn();
+            }
+        }
+        return methodReturn;
+    }
+
     @Override
     public String start(String line, String[] params,
                         LinkedList<Map<String, Map<String, Object>>> repositoryArray) {
         int position = line.indexOf('[');
         String variableType = line.substring(0, position);
-        Object startItem = repositoryArray.get(0).get(METHOD).get(variableType);
+
+        int count = access(variableType, repositoryArray.size());
+        if (count == -1) return null;
+        variableType = variableType.substring(count);
+
+        Object startItem = repositoryArray.get(count).get(METHOD).get(variableType);
 
         if (startItem == null) throw new VariableException().noDefineMethod();
         else if (startItem instanceof MethodItemReturn methodItemReturn) {
