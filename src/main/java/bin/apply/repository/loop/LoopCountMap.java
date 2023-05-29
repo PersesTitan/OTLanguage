8b5cd58655f8398e08113ce2b6add87826aec026
PersesTitan <torampersesTitan@gmail.com>
2023-05-29 17:35:51 +0900

    Create LoopCountMap.java

---
+++ src/main/java/bin/apply/repository/loop/LoopCountMap.java
@@ -0,0 +1,37 @@
+package bin.apply.repository.loop;
+
+import bin.exception.MatchException;
+import bin.exception.VariableException;
+import bin.token.check.CheckToken;
+import work.LoopWork;
+
+import java.util.HashMap;
+
+public class LoopCountMap extends HashMap<Integer, LoopWork> {
+    public LoopCountMap(LoopWork work) {
+        this.put(work);
+    }
+
+    public void put(LoopWork work) {
+        int size = work.getSIZE();
+        if (size == 1 && CheckToken.isBlankType(work.getPARAMS()[0])) put(0, work);
+        this.put(size, work);
+    }
+
+    private void put(int i, LoopWork work) {
+        if (super.containsKey(i)) throw VariableException.DEFINE_NAME.getThrow(null);
+        else super.put(i, work);
+    }
+
+    public LoopWork get(int size) {
+        if (super.containsKey(size)) return super.get(size);
+        else throw MatchException.PARAM_COUNT_ERROR.getThrow(size);
+    }
+
+    public boolean contains(LoopWork work) {
+        int size = work.getSIZE();
+        if (size == 1 && CheckToken.isBlankType(work.getPARAMS()[0])) {
+            return super.containsKey(0) || super.containsKey(1);
+        } else return super.containsKey(size);
+    }
+}
