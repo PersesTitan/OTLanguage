f6c9be33471492e5dd75a901abb4d3dcd17bf8b3
PersesTitan <torampersesTitan@gmail.com>
2023-05-29 17:35:48 +0900

    Create LoopMap.java

---
+++ src/main/java/bin/apply/repository/loop/LoopMap.java
@@ -0,0 +1,27 @@
+package bin.apply.repository.loop;
+
+import bin.exception.MatchException;
+import work.LoopWork;
+
+import java.io.Serial;
+import java.util.HashMap;
+
+public class LoopMap extends HashMap<String, MethodMap> {
+    @Serial
+    private static final long serialVersionUID = -4460826311801706685L;
+
+    public <T extends LoopCountMap> void put(String klass, String method, LoopWork work, T map) {
+        if (super.containsKey(klass)) super.get(klass).put(method, map.get());
+        else super.put(klass, new MethodMap(method, map));
+    }
+
+    public void put(String klass, String method, LoopWork work) {
+        if (super.containsKey(klass)) super.get(klass).put(method, work);
+        else super.put(klass, new MethodMap(method, work));
+    }
+
+    public LoopWork get(String klass, String method, int size) {
+        if (super.containsKey(klass)) return super.get(klass).get(method, size);
+        else throw MatchException.PARAM_COUNT_ERROR.getThrow(klass);
+    }
+}
