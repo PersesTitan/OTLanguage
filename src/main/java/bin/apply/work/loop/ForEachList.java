34c85996d535858194cbf1c1c517eb11d513d188
PersesTitan <torampersesTitan@gmail.com>
2023-06-01 22:59:28 +0900

    Create ForEachList.java

---
+++ src/main/java/bin/apply/work/loop/ForEachList.java
@@ -0,0 +1,17 @@
+package bin.apply.work.loop;
+
+import bin.apply.mode.TypeMode;
+import bin.apply.repository.function.OSConsumer;
+import bin.variable.custom.CustomList;
+import work.LoopWork;
+
+public class ForEachList extends LoopWork {
+    public ForEachList(TypeMode mode) {
+        super(mode.getList(), false, 1, new String[]{mode.getType()});
+    }
+
+    @Override
+    protected void loopItem(Object klass, Object[] params, OSConsumer consumer) {
+        ((CustomList<?>) klass).forEach(consumer::accept);
+    }
+}
