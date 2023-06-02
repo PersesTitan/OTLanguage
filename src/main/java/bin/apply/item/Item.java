9db41150f50b39ff413e9a8c54156ff30a3c068b
PersesTitan <torampersesTitan@gmail.com>
2023-06-02 14:35:17 +0900

    Create Item.java

---
+++ src/main/java/bin/apply/item/Item.java
@@ -0,0 +1,17 @@
+package bin.apply.item;
+
+import bin.token.EditToken;
+import bin.token.Token;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public interface Item {
+    default String toString(String klass, Object... values) {
+        return klass + Token.PARAM_S +
+                Arrays.stream(values)
+                        .map(EditToken::toString)
+                        .collect(Collectors.joining(Token.PARAM)) +
+                Token.PARAM_E;
+    }
+}
