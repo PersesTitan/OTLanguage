5f6da0d567ebb5867f929af807c7d42134451534
PersesTitan <torampersesTitan@gmail.com>
2023-06-11 14:31:06 +0900

    Create ArrayTool.java

---
+++ src/main/java/bin/parser/param/variable/ArrayTool.java
@@ -0,0 +1,16 @@
+package bin.parser.param.variable;
+
+import bin.parser.param.ParamToken;
+
+public class ArrayTool extends ParamToken {
+
+    @Override
+    public Object replace() {
+        return null;
+    }
+
+    @Override
+    public String getReplace() {
+        return null;
+    }
+}
