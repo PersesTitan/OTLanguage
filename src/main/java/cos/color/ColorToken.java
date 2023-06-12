115c4aa9aabc6e25dbcf110b46681016dc93d41a
PersesTitan <torampersesTitan@gmail.com>
2023-06-13 08:28:52 +0900

    Create ColorToken.java

---
+++ src/main/java/cos/color/ColorToken.java
@@ -0,0 +1,17 @@
+package cos.color;
+
+public interface ColorToken {
+    String COLOR = "ㅅㄲㄱ";
+
+    String SET_R = "<ㅃ";
+    String SET_G = "<ㅊ";
+    String SET_B = "<ㅍ";
+    String SET_A = "<ㅇ";
+    String SET_COLOR = "<";
+
+    String GET_R = ">ㅃ";
+    String GET_G = ">ㅊ";
+    String GET_B = ">ㅍ";
+    String GET_A = ">ㅇ";
+    String GET_COLOR = ">";
+}
