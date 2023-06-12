b379d184e98e7429f6c717d176fe602fed592f48
PersesTitan <torampersesTitan@gmail.com>
2023-06-13 08:29:03 +0900

    Create FileToken.java

---
+++ src/main/java/cos/file/FileToken.java
@@ -0,0 +1,17 @@
+package cos.file;
+
+public interface FileToken {
+    String FILE = "ㅍㅅㅍ";
+
+    String WRITHE = "<ㄹㄷㄹ";
+
+    String FILE_LIST = ">ㄹㅅㄹ";
+    String FILE_READ = ">ㄹㄷㄹ";
+
+    String GET_NAME = ">ㅇㄹㅇ";
+    String GET_PATH = ">ㄱㄹㄱ";
+
+    String IS_FILE = "?ㅍ";
+    String IS_DIR = "?ㄷ";
+    String EXISTS = "?";
+}
