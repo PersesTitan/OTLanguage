29f6ccb53ddfaaf48a1595aaa75c7ec5fc8e6e2c
PersesTitan <torampersesTitan@gmail.com>
2023-06-14 00:22:41 +0900

    Create OCRToken.java

---
+++ src/main/java/cos/ocr/OCRToken.java
@@ -0,0 +1,12 @@
+package cos.ocr;
+
+interface OCRToken {
+    String OCR = "ㅌㅆㅌ";
+
+    String ADD = "<ㅇㄴㅇ";
+    String GET = ">ㅇㄴㅇ";
+    String REMOVE = "-ㅇㄴㅇ";
+    String INIT = "!ㅇㄴㅇ";
+
+    String START = "ㅅㅌㅅ";
+}
