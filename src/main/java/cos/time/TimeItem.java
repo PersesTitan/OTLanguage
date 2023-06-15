9a45931bdca2a5dd2852c8f0f94dcf4c9077c419
PersesTitan <torampersesTitan@gmail.com>
2023-06-15 10:57:09 +0900

    Create TimeItem.java

---
+++ src/main/java/cos/time/TimeItem.java
@@ -0,0 +1,69 @@
+package cos.time;
+
+import bin.apply.item.Item;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+
+@Getter
+@RequiredArgsConstructor
+class TimeItem implements Item {
+    private final LocalTime time;
+
+    TimeItem(LocalDateTime dateTime) {
+        int hour = dateTime.getHour();
+        int minute = dateTime.getMinute();
+        int second = dateTime.getSecond();
+        int nano = dateTime.getNano();
+        this.time = LocalTime.of(hour, minute, second, nano);
+    }
+
+    public TimeItem() {
+        this.time = LocalTime.now();
+    }
+
+    public TimeItem(int hour, int minute) {
+        TimeToken.checkValue(hour, 0, 23);
+        TimeToken.checkValue(minute, 0, 59);
+        this.time = LocalTime.of(hour, minute);
+    }
+
+    public TimeItem(int hour, int minute, int second) {
+        TimeToken.checkValue(hour, 0, 23);
+        TimeToken.checkValue(minute, 0, 59);
+        TimeToken.checkValue(second, 0, 59);
+        this.time = LocalTime.of(hour, minute, second);
+    }
+
+    public TimeItem(int hour, int minute, int second, int nano) {
+        TimeToken.checkValue(hour, 0, 23);
+        TimeToken.checkValue(minute, 0, 59);
+        TimeToken.checkValue(second, 0, 59);
+        this.time = LocalTime.of(hour, minute, second, nano);
+    }
+
+    public TimeItem plus(ChronoUnit unit, long time) {
+        return new TimeItem(this.time.plus(time, unit));
+    }
+
+    public TimeItem minus(ChronoUnit unit, long time) {
+        return new TimeItem(this.time.minus(time, unit));
+    }
+
+    public int get(ChronoField chronoField) {
+        return this.time.get(chronoField);
+    }
+
+    @Override
+    public String toString() {
+        int hour = time.getHour();
+        int minute = time.getMinute();
+        int second = time.getSecond();
+        int nano = time.getNano();
+        return this.toString(TimeToken.TIME, hour, minute, second, nano);
+    }
+}
