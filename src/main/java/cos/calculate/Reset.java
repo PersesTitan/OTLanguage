0ed0cc21b197c3d6732e26b9380de50c0b046a7e
PersesTitan <torampersesTitan@gmail.com>
2023-06-20 00:31:19 +0900

    Update Reset.java

--- src/main/java/cos/calculate/Reset.java
+++ src/main/java/cos/calculate/Reset.java
@@ -7,6 +7,11 @@ import java.math.BigInteger;
 
 public class Reset implements ResetWork, CalculateToken {
     @Override
+    public String version() {
+        return "1.0.0";
+    }
+
+    @Override
     public void reset() {
         create(CALCULATE, CalculateItem.class, v -> new CalculateItem((String) v[0]), s);
     }
