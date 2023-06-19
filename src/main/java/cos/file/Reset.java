c86827c6f3bc2aa9462e590a2cd1a5ebd39b995b
PersesTitan <torampersesTitan@gmail.com>
2023-06-20 00:31:50 +0900

    Update Reset.java

--- src/main/java/cos/file/Reset.java
+++ src/main/java/cos/file/Reset.java
@@ -6,6 +6,11 @@ import work.ResetWork;
 
 public class Reset implements ResetWork, FileToken, Repository {
     @Override
+    public String version() {
+        return "1.0.0";
+    }
+
+    @Override
     public void reset() {
         create(FILE, FileItem.class, v -> new FileItem((String) v[0]), s);
 
