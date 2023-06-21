e3e235b2e0aab369bf9ec1a9279b1811ca8c376e
PersesTitan <torampersesTitan@gmail.com>
2023-06-22 01:28:56 +0900

    Update GuiException.java

--- src/main/java/cos/gui/GuiException.java
+++ src/main/java/cos/gui/GuiException.java
@@ -9,7 +9,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 enum GuiException implements ErrorTool {
     DO_NOT_SUPPORT_TYPE("지원하지 않는 종류입니다."),
-    DO_NOT_USE_TYPE("해당 타입에서는 지원하지 않습니다.")
+    DO_NOT_USE_TYPE("해당 타입에서는 지원하지 않습니다."),
+    DO_NOT_INPUT_DEVICE("입력 장치 지원에 문제가 있습니다.")
     ;
 
     private final String message;
@@ -26,6 +27,11 @@ enum GuiException implements ErrorTool {
                     """
                     This type does not support it.
                     Please check the item type.
+                    """;
+            case DO_NOT_INPUT_DEVICE ->
+                    """
+                    There is a problem with input device support.
+                    Please check the input device status.
                     """;
         };
     }
