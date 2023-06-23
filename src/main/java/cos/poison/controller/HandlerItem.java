b3bbd7cb830b5c629aae59ed831dfba433c919b9
PersesTitan <torampersesTitan@gmail.com>
2023-06-24 01:39:20 +0900

    Delete HandlerItem.java

--- src/main/java/cos/poison/controller/HandlerItem.java
+++
@@ -1,4 +0,0 @@
-package cos.poison.controller;
-
-public record HandlerItem(String fileName, String startFinalTotal, String[][] params,
-                          String responseValue, String contentType) { }
