64691c1bdd6f3fd5d5eca7686c84302cb19513c7
PersesTitan <torampersesTitan@gmail.com>
2023-06-30 13:49:43 +0900

    Update CodeItem.java

--- src/main/java/bin/apply/item/CodeItem.java
+++ src/main/java/bin/apply/item/CodeItem.java
@@ -1,9 +1,14 @@
 package bin.apply.item;
 
 import bin.apply.line.LineTool;
+import bin.apply.work.system.Import;
 import bin.exception.Error;
+import bin.exception.FileException;
 import bin.exception.SystemException;
+import bin.token.SepToken;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.*;
@@ -13,9 +18,7 @@ public class CodeItem implements Serializable {
     private static final long serialVersionUID = -3491013047653565560L;
     public static final String VERSION = "5.0.0";
     public static CodeItem MAIN = null;
-    public static String RUT_PATH = null;
-//    public static final AtomicReference<CodeItem> MAIN = new AtomicReference<>();
-//    public static final AtomicReference<String> RUN_PATH = new AtomicReference<>();
+    public static String RUN_PATH = null;
 
     private final List<String> IMPORT = new ArrayList<>();
     protected final String PATH;
@@ -33,18 +36,16 @@ public class CodeItem implements Serializable {
         start(0, LINES.length);
     }
 
-    public void start(int s, int e) {
-        for (int i = s; i < e;) {
-            try {
-                i = start(i);
-            } catch (Error error) {
-                throw error.setMessage(PATH, FILES[i], i+1);
-            }
+    public void start(int s, final int e) {
+        try {
+            while (s<e) s=start(s);
+        } catch (Error error) {
+            throw error.setMessage(PATH, FILES[s], s+1);
         }
     }
 
-    public void startTry(int s, int e) {
-        for (int i = s; i < e;) i = start(i);
+    public void startTry(int s, final int e) {
+        while (s<e) s=start(s);
     }
 
     protected int start(int i) {
@@ -57,14 +58,34 @@ public class CodeItem implements Serializable {
 
     // static
     public static void addModule(String module) {
-//        CodeItem item = MAIN.get();
         if (MAIN != null) MAIN.IMPORT.add(module);
         else throw SystemException.CREATE_ERROR.getThrow(module);
     }
 
     public static boolean contains(String module) {
-//        CodeItem item = MAIN.get();
         if (MAIN != null) return MAIN.IMPORT.contains(module);
         else throw SystemException.CREATE_ERROR.getThrow(module);
+    }
+
+    // get fileName
+    public static String getModulePath(String moduleName) {
+        return getModulePath(moduleName, null);
+    }
+
+    public static String getModulePath(String moduleName, String file) {
+        if (RUN_PATH == null) throw SystemException.FILE_ERROR.getThrow(moduleName);
+        else if (file == null) return SepToken.getPath(RUN_PATH, SepToken.MODULE, moduleName);
+        else return SepToken.getPath(RUN_PATH, SepToken.MODULE, moduleName, file);
+    }
+
+    @Serial
+    private void readObject(ObjectInputStream ois) {
+        try {
+            ois.defaultReadObject();
+            MAIN = this;
+            for (String models : IMPORT) new Import(models);
+        } catch (IOException | ClassNotFoundException e) {
+            throw FileException.DO_NOT_INCLUDE.getThrow(e.getMessage());
+        }
     }
 }
