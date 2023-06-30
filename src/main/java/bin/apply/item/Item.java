2a2292b76bc413ad3c4f2906f3838e7a82fb73cb
PersesTitan <torampersesTitan@gmail.com>
2023-06-30 13:49:48 +0900

    Update Item.java

--- src/main/java/bin/apply/item/Item.java
+++ src/main/java/bin/apply/item/Item.java
@@ -7,11 +7,9 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public interface Item {
-    default String toString(String klass, Object... values) {
+    default String itemToString(String klass, Object... values) {
         return klass + Token.PARAM_S +
-                Arrays.stream(values)
-                        .map(EditToken::toString)
-                        .collect(Collectors.joining(Token.PARAM)) +
+                Arrays.stream(values).map(EditToken::toString).collect(Collectors.joining(Token.PARAM)) +
                 Token.PARAM_E;
     }
 }
