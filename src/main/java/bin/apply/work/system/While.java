7b9e9d0e72de07f310498153dd6adb7e55352d40
PersesTitan <torampersesTitan@gmail.com>
2023-07-03 07:15:09 +0900

    Update While.java

--- src/main/java/bin/apply/work/system/While.java
+++ src/main/java/bin/apply/work/system/While.java
@@ -2,13 +2,11 @@ package bin.apply.work.system;
 
 import bin.apply.item.CodesItem;
 import bin.apply.item.ParamItem;
-import bin.apply.repository.function.OSConsumer;
+import bin.apply.OSConsumer;
 import bin.parser.param.ParamToken;
 import bin.token.CastingToken;
 import bin.token.KlassToken;
 import work.LoopWork;
-
-import java.util.function.Consumer;
 
 public class While extends LoopWork {
     public While() {
