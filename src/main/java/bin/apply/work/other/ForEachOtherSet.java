8ee2b26e7cadff8bde9a99f7073fed46ecf99ee1
PersesTitan <torampersesTitan@gmail.com>
2023-07-03 07:15:06 +0900

    Update ForEachOtherSet.java

--- src/main/java/bin/apply/work/other/ForEachOtherSet.java
+++ src/main/java/bin/apply/work/other/ForEachOtherSet.java
@@ -3,7 +3,7 @@ package bin.apply.work.other;
 import bin.apply.Repository;
 import bin.apply.item.CodesItem;
 import bin.apply.item.ParamItem;
-import bin.apply.repository.function.OSConsumer;
+import bin.apply.OSConsumer;
 import bin.exception.VariableException;
 import bin.parser.param.ParamToken;
 import bin.token.KlassToken;
