71820e8e8dc0cf8a2d9367418f4d320208bfd6e5
PersesTitan <torampersesTitan@gmail.com>
2023-10-01 09:10:49 +0900

    Delete VariableType.java

--- src/main/java/bin/check/VariableType.java
+++
@@ -1,9 +0,0 @@
-package bin.check;
-
-public enum VariableType {
-    Boolean, Character, Double, Float, Integer, Long, String,
-    ListBoolean, ListCharacter, ListDouble, ListFloat, ListInteger, ListLong, ListString,
-    MapBoolean, MapCharacter, MapDouble, MapFloat, MapInteger, MapLong, MapString,
-    SetBoolean, SetCharacter, SetDouble, SetFloat, SetInteger, SetLong, SetString,
-    Method
-}
