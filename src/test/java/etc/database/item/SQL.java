2019dada34dcf7a4b4cb3420f47a53bcb23bea17
PersesTitan <torampersesTitan@gmail.com>
2023-10-05 09:47:23 +0900

    Delete SQL.java

--- src/test/java/etc/database/item/SQL.java
+++
@@ -1,7 +0,0 @@
-package etc.database.item;
-
-public enum SQL {
-    CREATE, ALTER, DROP, TRUNCATE,  // DCL
-    SELECT, INSERT, UPDATE, DELETE, // DML
-    COMMIT, ROLLBACK, GRANT, REVOKE // DCL
-}
